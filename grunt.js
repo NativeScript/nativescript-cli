afa2d0937019c575309888abef123a7e590b2dfe
Mark van Seventer <mark@vseventer.com>
2012-04-03 22:04:46 +0000

    Added replace plugin to grunt, cleaned up Kinvey.js.
    git-svn-id: https://subversion.assembla.com/svn/kinvey/js-library/trunk@1697 868178e2-d38b-4ba8-a603-825f91c87007

--- grunt.js
+++ grunt.js
@@ -14,29 +14,34 @@ module.exports = function(grunt) {
     sdk: '<%= pkg.name %>-<%= pkg.version %>',//dist base filename
     meta: {
       banner: [//sdk will be prefixed with this banner
-        '/*!', ' * <%= sdk %>', ' *',
-        ' * Copyright (c) <%= grunt.template.today("yyyy") %> Kinvey, Inc. All rights reserved.', ' *',
+        '/*!', ' * <%= sdk %>',
+        ' *',
+        ' * Copyright (c) <%= grunt.template.today("yyyy") %> Kinvey, Inc. All rights reserved.',
+        ' *',
         ' * Licensed to Kinvey, Inc. under one or more contributor',
         ' * license agreements.  See the NOTICE file distributed with',
         ' * this work for additional information regarding copyright',
         ' * ownership.  Kinvey, Inc. licenses this file to you under the',
         ' * Apache License, Version 2.0 (the "License"); you may not',
         ' * use this file except in compliance with the License.  You',
-        ' * may obtain a copy of the License at', ' *',
-        ' *         http://www.apache.org/licenses/LICENSE-2.0', ' *',
+        ' * may obtain a copy of the License at',
+        ' *',
+        ' *         http://www.apache.org/licenses/LICENSE-2.0',
+        ' *',
         ' * Unless required by applicable law or agreed to in writing,',
         ' * software distributed under the License is distributed on an',
         ' * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY',
         ' * KIND, either express or implied.  See the License for the',
         ' * specific language governing permissions and limitations',
-        ' * under the License.', ' */'
+        ' * under the License.',
+        ' */'
       ].join("\n")
     },
 
     // JSHint task
     lint: {
-//      beforeconcat: ['grunt.js', '<%= dir.src %>/**/*.js', '<%= dir.test %>/**/*.js' ],
-      beforeconcat: ['grunt.js', '<%= dir.src %>/**/*.js' ],
+//      beforeconcat: ['grunt.js', 'lib/grunt/**/*.js', '<%= dir.src %>/**/*.js', '<%= dir.test %>/**/*.js' ],
+      beforeconcat: ['grunt.js', 'lib/grunt/**/*.js', '<%= dir.src %>/**/*.js' ],
       afterconcat: [ '<%= dir.dist %>/<%= sdk %>.js' ]
     },
     jshint: {//http://www.jshint.com/options/
@@ -63,20 +68,46 @@ module.exports = function(grunt) {
           '<banner>',
           '<%= dir.src %>/intro.txt',
           '<%= dir.src %>/Kinvey.js',
-          '<%= dir.src %>/query/Query.js',
-          '<%= dir.src %>/query/JsonQueryBuilder.js',
-          '<%= dir.src %>/query/SimpleQuery.js',
-          '<%= dir.src %>/net/Net.js',
-          '<%= dir.src %>/net/Http.js',
-          '<%= dir.src %>/Entity.js',
-          '<%= dir.src %>/Collection.js',
+//          '<%= dir.src %>/query/Query.js',
+//          '<%= dir.src %>/query/JsonQueryBuilder.js',
+//          '<%= dir.src %>/query/SimpleQuery.js',
+//          '<%= dir.src %>/net/Net.js',
+//          '<%= dir.src %>/net/Http.js',
+//          '<%= dir.src %>/Entity.js',
+//          '<%= dir.src %>/Collection.js',
           '<%= dir.src %>/User.js',
-          '<%= dir.src %>/UserCollection.js',
+//          '<%= dir.src %>/UserCollection.js',
           '<%= dir.src %>/outro.txt'
         ],
         dest: '<%= dir.dist %>/<%= sdk %>.js'
       }
     },
+
+    // Replace task
+    replace: {
+      firstPass: {//remove IIFE from source files
+        src: '<%= dir.dist %>/<%= sdk %>.js',
+        find: /^\(function\(Kinvey\) \{|\}\(Kinvey\)\);$/gm
+      },
+      secondPass: {//remove gaps in newlines due to firstPass
+        src: '<%= dir.dist %>/<%= sdk %>.js',
+        find: /\n{3,}/g,
+        replace: '\n\n'
+      },
+      thirdPass: {//remove newline below <banner>
+        src: '<%= dir.dist %>/<%= sdk %>.js',
+        find: /\n(\(function\(undefined\) \{)/g,
+        replace: '$1'
+      },
+
+      reset: {//minification damages undefined argument, undo here 
+        src: '<%= dir.dist %>/<%= sdk %>.min.js',
+        find: /\(function\(a\)\{/g,
+        replace: '(function(undefined){'
+      }
+    },
+
+    // TODO create testing task
 
     // API generation task
     apidoc: {
@@ -88,7 +119,10 @@ module.exports = function(grunt) {
 
     // Minification task
     min: {
-      '<%= dir.dist %>/<%= sdk %>.min.js': [ '<banner>', '<%= dir.dist %>/<%= sdk %>.js' ],
+      dist: {
+        src: [ '<banner>', '<%= dir.dist %>/<%= sdk %>.js' ],
+        dest: '<%= dir.dist %>/<%= sdk %>.min.js'
+      }
     },
 
     // Watch files
@@ -107,6 +141,6 @@ module.exports = function(grunt) {
   grunt.loadTasks('lib/grunt/tasks');
 
   // Register tasks
-  grunt.registerTask('default', 'lint:beforeconcat concat lint:afterconcat apidoc min');
+  grunt.registerTask('default', 'lint:beforeconcat concat replace lint:afterconcat apidoc min');
 
 };
--- src/Kinvey.js
+++ src/Kinvey.js
@@ -5,21 +5,21 @@
 
   /**
    * API version
-   * 
+   *
    * @constant
    */
   Kinvey.API_VERSION = 0;
 
   /**
    * SDK version
-   * 
+   *
    * @constant
    */
   Kinvey.SDK_VERSION = '0.1.0dev';
 
   /**
    * Returns current user, or null if not set
-   * 
+   *
    * @return {Kinvey.User} current user
    */
   Kinvey.getCurrentUser = function() {
@@ -28,14 +28,14 @@
 
   /**
    * Initializes library for use with Kinvey services
-   * 
+   *
    * @example <code>
    * Kinvey.init({
    *   appKey: '<your-app-key>',
    *   appSecret: '<your-app-secret>'
    * });
    * </code>
-   * 
+   *
    * @param {Object} options
    * @throws {Error} on empty appKey, empty appSecret
    */
@@ -54,30 +54,26 @@
 
   /**
    * Round trips a request to the server and back, helps ensure connectivity
-   * 
+   *
    * @example <code>
-   * Kinvey.ping(function() {
-   *   alert('ping succeed');
+   * Kinvey.ping(function(response) {
+   *   console.log('Ping successful', response);
    * }, function(error) {
-   *   alert('ping failed: ' + error.error);
+   *   console.log('Ping failed', error);
    * });
    * </code>
-   * 
-   * @param {function()} [success] success callback
+   *
+   * @param {function(Object)} [success] success callback
    * @param {function(Object)} [failure] failure callback
    */
   Kinvey.ping = function(success, failure) {
     var net = Kinvey.Net.factory(Kinvey.Net.APPDATA_API, '');
-    net.send(Kinvey.Net.READ, function() {
-      success && success();
-    }, function(error) {
-      failure && failure(error);
-    });
+    net.send(Kinvey.Net.READ, success, failure);
   };
 
   /**
    * Sets current user
-   * 
+   *
    * @param {Kinvey.User} user user instance
    */
   Kinvey.setCurrentUser = function(user) {
