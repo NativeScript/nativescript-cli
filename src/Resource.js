9599e93ee1dcfce40bf016d6975494134030dfe9
Mark van Seventer <mark@vseventer.com>
2012-12-06 11:39:47 +0000

    Added character check for file name prior to uploading.
    git-svn-id: https://subversion.assembla.com/svn/kinvey/js-library/trunk@4531 868178e2-d38b-4ba8-a603-825f91c87007

--- src/Resource.js
+++ src/Resource.js
@@ -33,13 +33,27 @@
      * 
      * @param {Object} file File.
      * @param {Object} [options] Options.
-     * @throws {Error} On invalid file.
+     * @throws {Error}
+     *           <ul>
+     *           <li>On invalid file.</li>
+     *           <li>On invalid file name.</li>
+     *           </ul>
      */
     upload: function(file, options) {
       // Validate file.
       if(null == file || null == file.name || null == file.data) {
         throw new Error('File should be an object containing name and data');
       }
+
+      // Validate characters in file name.
+      // This is required because XMLHttpRequest internally uses encodeURI. Percent-encoded
+      // characters don't seem to play well with the underlying blob storage.
+      // @link https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/encodeURI
+      if(file.name !== encodeURI(file.name) || file.name.match(/\#|\?|\//)) {
+        throw new Error('File name contains invalid characters');
+      }
+
+      // Upload.
       Kinvey.Resource._store || (Kinvey.Resource._store = Kinvey.Store.factory(Kinvey.Store.BLOB));
       Kinvey.Resource._store.save(file, options);
     },
