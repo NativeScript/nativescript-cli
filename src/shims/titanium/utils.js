5836b92fd494f7114c73501bf3f14e967259c240
Mark van Seventer <mark@vseventer.com>
2013-07-15 21:02:23 +0000

    Updated storing the active user in Titanium.
    git-svn-id: https://subversion.assembla.com/svn/kinvey/js-library/trunk@7276 868178e2-d38b-4ba8-a603-825f91c87007

--- src/shims/titanium/utils.js
+++ src/shims/titanium/utils.js
@@ -1,27 +1,48 @@
 // `Storage` adapter for Titanium.
+
+// The storage methods are executed in the background. Therefore, implement a
+// queue to force the background processes to execute serially.
+var storagePromise = Kinvey.Defer.resolve(null);
+
+/**
+ * @private
+ * @namespace
+ */
 var TiAppStorage = {
   /**
    * @augments {Storage._destroy}
    */
   _destroy: function(key) {
-    Titanium.App.Properties.removeProperty(key);
-    return Kinvey.Defer.resolve(null);
+    // Remove the item on our turn.
+    storagePromise = storagePromise.then(function() {
+      Titanium.App.Properties.removeProperty(key);
+      return Kinvey.Defer.resolve(null);
+    });
+    return storagePromise;
   },
 
   /**
    * @augments {Storage._get}
    */
   _get: function(key) {
-    var value = Titanium.App.Properties.getObject(key, null);
-    return Kinvey.Defer.resolve(value);
+    // Retrieve the item on our turn.
+    storagePromise = storagePromise.then(function() {
+      var value = Titanium.App.Properties.getObject(key, null);
+      return Kinvey.Defer.resolve(value);
+    });
+    return storagePromise;
   },
 
   /**
    * @augments {Storage._save}
    */
   _save: function(key, value) {
-    Titanium.App.Properties.setObject(key, value);
-    return Kinvey.Defer.resolve(null);
+    // Save the item on our turn.
+    storagePromise = storagePromise.then(function() {
+      Titanium.App.Properties.setObject(key, value);
+      return Kinvey.Defer.resolve(null);
+    });
+    return storagePromise;
   }
 };
 
