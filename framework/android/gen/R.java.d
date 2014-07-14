e5b34ff2b6acb6fb438992610ad3ef3c750356d6
Fatme <hfatme@gmail.com>
2014-07-14 15:14:43 +0300

    Add android framework files

---
+++ framework/android/gen/R.java.d
@@ -0,0 +1,8 @@
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\gen\telerik\kimera\kimeraskeleton\R.java \
+ : C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\res\drawable-hdpi\ic_launcher.png \
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\res\drawable-mdpi\ic_launcher.png \
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\res\drawable-xhdpi\ic_launcher.png \
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\res\drawable-xxhdpi\ic_launcher.png \
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\res\values\strings.xml \
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\res\values\styles.xml \
+C:\Work\icenium-cli\scratch\nativescript-cli\NativescriptExample\bin\AndroidManifest.xml \
