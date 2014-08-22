bca68cad5d3b70b83ee2faf94b274733b61b4a65
Erjan Gavalji <erjan.gavalji@gmail.com>
2014-08-22 12:22:41 +0300

    Add Enable/Disable scripts to Gruntfile to workaround the run of the scripts when `npm install` called

--- BuildPackage.cmd
+++ BuildPackage.cmd
@@ -2,7 +2,9 @@ call "c:\Program Files (x86)\nodejs\nodevars.bat"
 call npm.cmd install -g grunt-cli
 
 set NATIVESCRIPT_SKIP_POSTINSTALL_TASKS=1
+call grunt.cmd enableScripts:false
 call npm.cmd install
+call grunt.cmd enableScripts:true
 set NATIVESCRIPT_SKIP_POSTINSTALL_TASKS=
 
 call grunt.cmd pack --no-color
