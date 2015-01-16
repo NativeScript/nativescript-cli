91c29af1902e0022ea95446d2f8a3115ebaab8ff
Todor Totev <teobugslayer@gmail.com>
2015-01-16 14:25:58 +0200

    Allow installing with sudo node
    See https://github.com/NativeScript/nativescript-cli/issues/171

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -28,7 +28,6 @@ $injector.requireCommand("run", "./commands/run");
 $injector.requireCommand("prepare", "./commands/prepare");
 $injector.requireCommand("build", "./commands/build");
 $injector.requireCommand("deploy", "./commands/deploy");
-$injector.requireCommand("dev-post-install", "./commands/post-install");
 $injector.requireCommand("emulate", "./commands/emulate");
 
 $injector.require("npm", "./node-package-manager");
