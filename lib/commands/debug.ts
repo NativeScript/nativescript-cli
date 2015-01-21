a11ebd175df8bc9fb2767a3f63660cc30a458799
Mihail Slavchev <mihail.slavchev@telerik.com>
2015-01-21 13:59:58 +0200

    implement debug command

---
+++ lib/commands/debug.ts
@@ -0,0 +1,18 @@
+﻿///<reference path="../.d.ts"/>
+"use strict";
+
+import helpers = require("./../common/helpers");
+import util = require("util")
+
+export class DebugCommand implements ICommand {
+    constructor(private $platformService: IPlatformService,
+        private $platformCommandParameter: ICommandParameter) { }
+
+    execute(args: string[]): IFuture<void> {
+        return this.$platformService.debugPlatform(args[0]);
+    }
+
+    allowedParameters = [this.$platformCommandParameter];
+}
+$injector.registerCommand("debug", DebugCommand);
+ 
