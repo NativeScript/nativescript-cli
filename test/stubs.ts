1e30cb86440bc6e2f3d9c7de8b37fd16fb525c95
Yavor Georgiev <fealebenpae@gmail.com>
2015-02-12 19:58:19 +0200

    Update the IPlatformProjectService stub

--- test/stubs.ts
+++ test/stubs.ts
@@ -254,7 +254,7 @@ export class PlatformProjectServiceStub implements IPlatformProjectService {
 	isPlatformPrepared(projectRoot: string): IFuture<boolean> {
 		return Future.fromResult(false);
 	}
-    addLibrary(projectRoot: string, libraryPath: string): IFuture<void> {
+    addLibrary(platformData: IPlatformData, libraryPath: string): IFuture<void> {
         return Future.fromResult();
     }
 }
