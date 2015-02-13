a947306c17d75b975fa93bd128bbb704635fadb4
Fatme Havaluova <Fatme.Havaluova@telerik.com>
2015-02-13 11:23:03 +0200

    Implement build command as hierarchical

--- lib/commands/build.ts
+++ lib/commands/build.ts
@@ -1,14 +1,39 @@
 ///<reference path="../.d.ts"/>
 "use strict";
 
-export class BuildCommand implements ICommand {
-	constructor(private $platformService: IPlatformService,
-		private $platformCommandParameter: ICommandParameter) { }
+export class BuildCommandBase {
+	constructor(private $platformService: IPlatformService) { }
 
-	execute(args: string[]): IFuture<void> {
-		return	this.$platformService.buildPlatform(args[0]);
+	executeCore(args: string[]): IFuture<void> {
+		return this.$platformService.buildPlatform(args[0]);
+	}
+}
+
+export class BuildIosCommand extends BuildCommandBase implements  ICommand {
+	constructor($platformService: IPlatformService,
+		private $platformsData: IPlatformsData) {
+		super($platformService);
 	}
 
-	allowedParameters = [this.$platformCommandParameter];
+	public allowedParameters: ICommandParameter[] = [];
+
+	public execute(args: string[]): IFuture<void> {
+		return this.executeCore([this.$platformsData.availablePlatforms.iOS]);
+	}
 }
-$injector.registerCommand("build", BuildCommand);
+$injector.registerCommand("build|ios", BuildIosCommand);
+
+
+export class BuildAndroidCommand extends BuildCommandBase implements  ICommand {
+	constructor($platformService: IPlatformService,
+				private $platformsData: IPlatformsData) {
+		super($platformService);
+	}
+
+	public allowedParameters: ICommandParameter[] = [];
+
+	public execute(args: string[]): IFuture<void> {
+		return this.executeCore([this.$platformsData.availablePlatforms.Android]);
+	}
+}
+$injector.registerCommand("build|android", BuildAndroidCommand);
--- lib/services/platform-service.ts
+++ lib/services/platform-service.ts
@@ -165,7 +165,7 @@ export class PlatformService implements IPlatformService {
 	public buildPlatform(platform: string): IFuture<void> {
 		return (() => {
 			platform = platform.toLowerCase();
-			this.preparePlatform(platform);
+			this.preparePlatform(platform).wait();
 
 			var platformData = this.$platformsData.getPlatformData(platform);
 			platformData.platformProjectService.buildProject(platformData.projectRoot).wait();
