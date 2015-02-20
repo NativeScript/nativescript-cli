c59a5fad0404b0bbcf9fbd6ea243c8316d8bad34
Yavor Georgiev <fealebenpae@gmail.com>
2015-02-20 19:39:52 +0200

    Remove isRelativeDir stub

--- test/stubs.ts
+++ test/stubs.ts
@@ -113,10 +113,6 @@ export class FileSystemStub implements IFileSystem {
 		return Future.fromResult(true);
 	}
 
-    isRelativePath(path: string): boolean {
-        return true;
-    }
-
 	ensureDirectoryExists(directoryPath: string): IFuture<void> {
 		return Future.fromResult();
 	}
