a96ef904d69dea42d930e6ad0ab78b2550d6ffc2
Thomas Conner <thomas.conner@me.com>
2015-03-16 11:18:23 -0400

    MLIBZ-188 Add App Version header to requests
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- src/core/persistence/net.js
+++ src/core/persistence/net.js
@@ -237,10 +237,13 @@ Kinvey.Persistence.Net = /** @lends Kinvey.Persistence.Net */{
     var headers = {
       Accept                        : 'application/json',
       'X-Kinvey-API-Version'        : Kinvey.API_VERSION,
-      'X-Kinvey-Device-Information' : deviceInformationHeader
+      'X-Kinvey-Device-Information' : deviceInformationHeader,
     };
 
     // Append optional headers.
+    if (options.appVersion != null || Kinvey.APP_VERSION != null) {
+      headers['X-Kinvey-Customer-App-Version'] = options.AppVersion || Kinvey.APP_VERSION;
+    }
     if(null != request.data) {
       headers['Content-Type'] = 'application/json; charset=utf-8';
     }
