f6503411a35aad9fc0f9acfbaf1218189c837f49
Todor Totev <teobugslayer@gmail.com>
2015-03-18 17:48:29 +0200

    At post-install, ask the user to turn on autocompletion Introduce the "autocomplete" command to allow user to turn it on when they need it.

--- postinstall.js
+++ postinstall.js
@@ -1,14 +1,3 @@
 "use strict";
 var child_process = require("child_process");
-var command = process.argv[0] + ' bin/nativescript.js dev-post-install';
-child_process.exec(command, function(error, stdout, stderr) {
-	if (stdout !== null && stdout.toString() !== "") {
-		console.log('stdout: ' + stdout);
-	}
-	if (stderr !== null && stderr.toString() !== "") {
-		console.log('stderr: ' + stderr);
-	}
-	if (error !== null) {
-		console.log('exec error: ' + error);
-	}
-});
+child_process.spawn(process.argv[0], ["bin/nativescript.js", "dev-post-install"], {stdio: "inherit"});
