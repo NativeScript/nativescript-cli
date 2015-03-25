b59bfaaf694f910e053dc05ae4bd48d28c480782
Thomas Conner <thomas.conner@me.com>
2015-03-25 09:46:16 -0400

    MLIBZ-162 Make sure client app version header is a string
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- src/core/persistence/net.js
+++ src/core/persistence/net.js
@@ -243,7 +243,7 @@ Kinvey.Persistence.Net = /** @lends Kinvey.Persistence.Net */{
     // Append optional headers.
     options.clientAppVersion = options.clientAppVersion || Kinvey.ClientAppVersion.stringValue();
     if (options.clientAppVersion != null) {
-      headers['X-Kinvey-Client-App-Version'] = options.clientAppVersion;
+      headers['X-Kinvey-Client-App-Version'] = (options.clientAppVersion + '');
     }
     if(null != request.data) {
       headers['Content-Type'] = 'application/json; charset=utf-8';
