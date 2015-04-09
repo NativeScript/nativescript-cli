544961d9d9047c5221861dc6d10f65c5cbf95640
Thomas Conner <thomas.conner@me.com>
2015-04-09 14:44:41 -0400

    MLIBZ-192 Wrote sample a sample test using nock
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- test/example.spec.js
+++ test/example.spec.js
@@ -16,17 +16,11 @@
     });
 
     describe('Network Request', function() {
-      //var api = null;
-      var user = null;
-
-      before(function() {
-        nock.enableNetConnect();
-      });
-
       before(function() {
         var promise = Kinvey.init({
-          appKey    : config.test.appKey,
-          appSecret : config.test.appSecret
+          apiHostName: 'https://v3yk1n-kcs.kinvey.com',
+          appKey    : 'kid_byW23pLgR',
+          appSecret : '15e67b69e76540c2984cbcb77d8de763'
         });
         return promise.then(null, function(error) {
           // Do not fail if the active user was deleted via the console.
@@ -39,41 +33,68 @@
       });
 
       afterEach(function() {
-        Kinvey.appKey = config.test.appKey;
-        Kinvey.appSecret = config.test.appSecret;
+        Kinvey.appKey = 'kid_byW23pLgR';
+        Kinvey.appSecret = '15e67b69e76540c2984cbcb77d8de763';
       });
 
       before(function() {
-        return Kinvey.User.create({}, { state: false }).then(function(_user) {
-          user = _user;
+        var api = nock('https://v3yk1n-kcs.kinvey.com')
+          .filteringPath(/(&)?_=([^&]*)/, '')
+          .filteringRequestBody(function() {
+            return '*';
+          })
+          .post('/user/kid_byW23pLgR/login/?', '*')
+          .reply(200, {
+            _id: '5526bacf3f82f0a517c4f024',
+            username: 'test',
+            _kmd: {
+              lmt: '2015-04-09T17:45:51.780Z',
+              ect: '2015-04-09T17:45:51.780Z',
+              authtoken: '66e82c49-e71f-4074-97c8-17f77d8e2ce4.2O5DPC/r8XzyYEQs7KOP9d0faQnHPkVN1e3KKVNhqxE='
+            },
+            _acl: {
+              creator: '5526bacf3f82f0a517c4f024'
+            }
+          }, {
+            server: 'ngx_openresty',
+            date: 'Thu, 09 Apr 2015 17:47:01 GMT',
+            'content-type': 'application/json; charset=utf-8',
+            'content-length': '269',
+            connection: 'close',
+            'x-powered-by': 'Express',
+            'x-kinvey-request-id': '5205b4751ebe482682253a14ab116371',
+            'x-kinvey-api-version': '3'
+          });
+
+        return Kinvey.User.login('test', 'test').then(function(user) {
+          expect(user._id).to.exist;
+          expect(api.isDone()).to.be.true;
         });
       });
 
-      after(function() {// Delete the user using its credentials.
-        Kinvey.setActiveUser(user);
-        return Kinvey.User.destroy(user._id, { hard: true }).then(function() {
-          Kinvey.setActiveUser(null); // Reset.
-        });
+      afterEach(function() {
+        nock.cleanAll();
       });
 
-      after(function() { // Cleanup.
-        user = null;
-      });
+      it('should respond with an empty array of books', function() {
+        var api = nock('https://v3yk1n-kcs.kinvey.com')
+          .filteringPath(/(&)?_=([^&]*)/, '')
+          .get('/appdata/kid_byW23pLgR/books/?kinveyfile_tls=true')
+          .reply(200, [], {
+            server: 'ngx_openresty',
+            date: 'Thu, 09 Apr 2015 17:22:45 GMT',
+            'content-type': 'application/json; charset=utf-8',
+            'content-length': '2',
+            connection: 'close',
+            'x-powered-by': 'Express',
+            'x-kinvey-request-id': 'a667bf8b4c7747dfa3a8690a5b66602b',
+            'x-kinvey-api-version': '3'
+          });
 
-      before(function() {
-        // api = nock(config.kcs.protocol + '://' + config.kcs.host)
-        //       .get('/example')
-        //       .reply(200, 'Hello World');
-      });
-
-      it('should respond with a 200', function(done) {
-        Kinvey.DataStore.find('books').then(function(books) {
+        return Kinvey.DataStore.find('books').then(function(books) {
           expect(books).to.be.instanceof(Array);
-          expect(books.length).to.equal(0);
-          done();
-        }, function(err) {
-          console.log(err);
-          done(err);
+          expect(books.length).to.be.equal(0);
+          expect(api.isDone()).to.be.true;
         });
       });
     });
