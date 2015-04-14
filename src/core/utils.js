b22cd0cd5877f29c611d3c8aab00fa1d00fea1c9
Thomas Conner <thomas.conner@me.com>
2015-04-14 09:51:18 -0400

    MLIBZ-249 Move parseQueryString to utils
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- src/core/utils.js
+++ src/core/utils.js
@@ -176,6 +176,53 @@ var getByteCount = function(str) {
   return count;
 };
 
+/**
+ * Parse a query string and return an object.
+ *
+ * @example foo=bar&baz=qux -> { foo: "bar", baz: "qux" }
+ * @param {string} string The query string.
+ * @returns {Object} The query string params.
+ */
+var parseQueryString = function(str) {
+  if (typeof str !== 'string') {
+    return {};
+  }
+
+  str = str.trim().replace(/^(\?|#)/, '');
+
+  if (!str) {
+    return {};
+  }
+
+  var index = str.indexOf('#/');
+  if (index === str.length - 2) {
+    str = str.substring(0, index);
+  }
+
+  return str.trim().split('&').reduce(function (ret, param) {
+    var parts = param.replace(/\+/g, ' ').split('=');
+    var key = parts[0];
+    var val = parts[1];
+
+    key = decodeURIComponent(key);
+    // missing `=` should be `null`:
+    // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+    val = val === undefined ? null : decodeURIComponent(val);
+
+    if (!ret.hasOwnProperty(key)) {
+      ret[key] = val;
+    }
+    else if (Array.isArray(ret[key])) {
+      ret[key].push(val);
+    }
+    else {
+      ret[key] = [ret[key], val];
+    }
+
+    return ret;
+  }, {});
+};
+
 // Define the request Option type for documentation purposes.
 
 /**
