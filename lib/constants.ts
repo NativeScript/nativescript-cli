4f62b65c79bdf2adf1639e2c5b1151dd98bd63c7
Fatme Havaluova <Fatme.Havaluova@telerik.com>
2015-04-28 12:10:34 +0300

    Enable npm as package manager for ns applications

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,10 @@
 export var APP_FOLDER_NAME = "app";
 export var APP_RESOURCES_FOLDER_NAME = "App_Resources";
 export var PROJECT_FRAMEWORK_FOLDER_NAME = "framework";
+export var NATIVESCRIPT_KEY_NAME = "nativescript";
+export var NODE_MODULES_FOLDER_NAME = "node_modules";
+export var PACKAGE_JSON_FILE_NAME = "package.json";
+export var NODE_MODULE_CACHE_PATH_KEY_NAME = "node-modules-cache-path";
 
 export class ReleaseType {
 	static MAJOR = "major";
---
+++ lib/tools/broccoli/broccoli-plugin-wrapper.ts
@@ -0,0 +1,59 @@
+///<reference path="../../.d.ts"/>
+"use strict";
+
+import fs = require('fs');
+import path = require('path');
+import {TreeDiffer} from './tree-differ';
+
+export class BroccoliPluginWrapper implements BroccoliTree {
+  treeDiffer: TreeDiffer;
+  wrappedPlugin: IBroccoliPlugin = null;
+  inputTree: any = null;
+  description: string = null;
+  absoluteOutputPath: string = null;
+  treeRootDirName: string = null;
+  projectDir: string = null;
+  $injector: IInjector = null;
+
+  // props monkey-patched by broccoli builder:
+  inputPath: string = null;
+  cachePath: string = null;
+  outputPath: string = null;
+
+  constructor(private pluginClass: any, wrappedPluginArguments: any) { 
+      this.inputTree = wrappedPluginArguments[0];
+      this.description = this.pluginClass.name;
+      this.absoluteOutputPath = wrappedPluginArguments[1];
+      this.treeRootDirName = wrappedPluginArguments[2];
+      this.projectDir = wrappedPluginArguments[3];
+      this.$injector = $injector.resolve("injector");
+  }
+
+  rebuild(): void {
+      try {
+        this.init();
+  
+        let diffResult = this.treeDiffer.diffTree(this.absoluteOutputPath, this.treeRootDirName);        
+        this.wrappedPlugin.rebuild(diffResult);
+  
+      } catch (e) {
+        e.message = `[${this.description}]: ${e.message}`;
+        throw e;
+      }
+  }
+
+  private init() {
+      this.treeDiffer = new TreeDiffer(this.inputPath);
+      this.wrappedPlugin = this.$injector.resolve(this.pluginClass, 
+        { inputPath: this.inputPath, 
+          cachePath: this.cachePath, 
+          outputRoot: this.absoluteOutputPath, 
+          projectDir: this.projectDir });
+  }
+
+  cleanup() {
+    if (this.wrappedPlugin.cleanup) {
+      this.wrappedPlugin.cleanup();
+    }
+  }
+}
