6b72b66d952e2ce2c85fedb22e2a5e7a485e03a4
Fatme <hfatme@gmail.com>
2015-06-15 15:09:16 +0300

    Add test to ensure that npm dependencies will be correctly prepared

--- lib/tools/broccoli/trees/node-modules-tree.ts
+++ lib/tools/broccoli/trees/node-modules-tree.ts
@@ -6,7 +6,7 @@ let Funnel = require('broccoli-funnel');
 import path = require("path");
 import destCopy from '../node-modules-dest-copy';
 
-class NodeModulesTree implements INodeModulesTree {	
+export class NodeModulesTree implements INodeModulesTree {	
 	public makeNodeModulesTree(absoluteOutputPath: string, projectDir: string): any {
 		let nodeModulesFunnel = new Funnel(projectDir, { include: ["node_modules/**"] });
    		let result = destCopy(nodeModulesFunnel, absoluteOutputPath, "node_modules", projectDir);   
