a611ede31759bcdd5724e3683695da868cac6150
Thomas Conner <thomas.conner@me.com>
2015-06-17 17:45:08 -0400

    Add methods to cache.
    Added fulsh(), flushExpired().
    
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- src/core/cache.js
+++ src/core/cache.js
@@ -1,31 +1,108 @@
-import KinveyCache from 'kinvey-cache';
+import CoreObject from './object';
+import lscache from 'lscache';
 import Kinvey from '../kinvey';
+import utils from './utils';
+const cacheSymbol = Symbol();
 
-class Cache extends KinveyCache {
-  static generateKeyHash(key) {
-    return key;
+function generateKeyHash(key) {
+  return key;
+}
+
+class Cache extends CoreObject {
+  constructor(name = `Kinvey.${Kinvey.appKey}`, time = null) {
+    super();
+
+    // Set the name
+    this.name = name;
+    this.defaultTime = time;
+
+    // Flush expired items
+    this.flushExpired();
   }
 
   static get(key) {
-    let keyHash = Cache.generateKeyHash(key);
     let cache = Cache.instance();
-    return cache.get(keyHash);
+    return cache.get(key);
   }
 
-  static set(key, data, time) {
-    let keyHash = Cache.generateKeyHash(key);
+  get(key) {
+    // Generate a key hash
+    let keyHash = generateKeyHash(key);
+
+    // Set the bucket for the cache
+    lscache.setBucket(this.name);
+
+    // Get the response from the cache
+    return lscache.get(keyHash);
+  }
+
+  static set(key, value, time) {
     let cache = Cache.instance();
-    return cache.set(keyHash, data, time);
+    return cache.set(key, value, time);
+  }
+
+  set(key, value, time) {
+    // Generate a key hash
+    let keyHash = generateKeyHash(key);
+
+    // Set the bucket for the cache
+    lscache.setBucket(this.name);
+
+    // Store the response in the cache
+    return lscache.set(keyHash, value, time);
   }
 
   static remove(key) {
-    let keyHash = Cache.generateKeyHash(key);
     let cache = Cache.instance();
-    return cache.remove(keyHash);
+    return cache.remove(key);
+  }
+
+  remove(key) {
+    // Generate a key hash
+    let keyHash = generateKeyHash(key);
+
+    // Set the bucket for the cache
+    lscache.setBucket(this.name);
+
+    // Remove the response from the cache
+    return lscache.remove(keyHash);
+  }
+
+  static flush() {
+    let cache = Cache.instance();
+    return cache.flush();
+  }
+
+  flush() {
+    // Set the bucket for the cache
+    lscache.setBucket(this.name);
+
+    // Flush the cache
+    lscache.flush();
+  }
+
+  static flushExpired() {
+    let cache = Cache.instance();
+    return cache.flushExpired;
+  }
+
+  flushExpired() {
+    // Set the bucket for the cache
+    lscache.setBucket(this.name);
+
+    // Flush the expired items
+    lscache.flushExpired();
   }
 
   static instance() {
-    return super.instance(`Kinvey.${Kinvey.appKey}`);
+    let cache = this[cacheSymbol];
+
+    if (!utils.isDefined(cache)) {
+      cache = new Cache();
+      this[cacheSymbol] = cache;
+    }
+
+    return cache;
   }
 }
 
