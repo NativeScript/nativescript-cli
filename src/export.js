11c7bdb668715e959fe2f594c6cea2eac00c7ff8
Thomas Conner <thomas.conner@me.com>
2015-06-17 17:47:02 -0400

    Export LocalDataStore and Session
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- src/export.js
+++ src/export.js
@@ -1,13 +1,14 @@
 let Kinvey = require('./kinvey');
 
-Kinvey.DataStore = require('./core/datastore');
 Kinvey.Defer = require('./core/defer');
+Kinvey.LocalDataStore = require('./core/localDataStore');
 Kinvey.Middleware = require('./middleware/middleware');
 Kinvey.Middleware.Cache = require('./middleware/cache');
 Kinvey.Middleware.Http = require('./middleware/http');
 Kinvey.Middleware.Parser = require('./middleware/parser');
 Kinvey.Middleware.Serializer = require('./middleware/serializer');
 Kinvey.Rack = require('./core/rack');
+Kinvey.Session = require('./core/session');
 Kinvey.User = require('./core/user');
 
 export default Kinvey;
