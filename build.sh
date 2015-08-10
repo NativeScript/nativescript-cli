8c651b5407682dac9a1067c304c876c5af01edde
Tejas <tejas@kinvey.com>
2015-08-10 15:31:53 -0400

    improving build script to remove hardcoded workspace

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-export WORKSPACE=/Users/Thomas/Development/Kinvey/SDKs/2.x/JavaScript/SDK
+PROJECT_ROOT=$1
 export NODE_VERSION=0.10.40
-cd ${WORKSPACE} && npm i && make all
+cd ${PROJECT_ROOT} && npm i && make all
