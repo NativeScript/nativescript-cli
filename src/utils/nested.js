0a8a895acb52988f6eb18efea0693ff3abec5de1
Thomas Conner <thomas.conner@me.com>
2015-08-20 17:23:19 -0400

    Added nested util function
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

---
+++ src/utils/nested.js
@@ -0,0 +1,23 @@
+export default function nested(document, dotProperty, value) {
+  if (!dotProperty) {// Top-level document.
+    document = typeof value === 'undefined' ? document : value;
+    return document;
+  }
+
+  let obj = document;
+  const parts = dotProperty.split('.');
+
+  // Traverse the document until the nested property is located.
+  let current = parts.shift();
+  while (current && obj && obj.hasOwnProperty(current)) {
+    if (parts.length === 0) {// Return the (new) property value.
+      obj[current] = typeof value === 'undefined' ? obj[current] : value;
+      return obj[current];
+    }
+
+    obj = obj[current];// Continue traversing.
+    current = parts.shift();
+  }
+
+  return null;// Property not found.
+}
