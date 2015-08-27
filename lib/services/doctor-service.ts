37f5a1c650403000cb6c22e5c44a673b3a9cb9b7
Fatme Havaluova <Fatme.Havaluova@telerik.com>
2015-08-27 17:09:50 +0300

    Respect settings.gradle file from template
    Android runtime introduced gradle.settings file in order to produce apk named <projectName>-debug.apk or <projectName>-release.apk.
    Currently the produced .apk files are named android-debug.apk or android-release.apk.

--- lib/services/doctor-service.ts
+++ lib/services/doctor-service.ts
@@ -70,7 +70,7 @@ class DoctorService implements IDoctorService {
 			this.$logger.out("You will not be able to build your projects for Android or run them in the emulator or on a connected device." + EOL
 				+ "To be able to build for Android and run apps in the emulator on on a connected device, verify that you have installed Gradle.");
 		}
-		
+
 		if(sysInfo.gradleVer && helpers.versionCompare(sysInfo.gradleVer, DoctorService.MIN_SUPPORTED_GRADLE_VERSION) === -1) {
 			this.$logger.warn(`WARNING: Gradle version is lower than ${DoctorService.MIN_SUPPORTED_GRADLE_VERSION}.`);
 			this.$logger.out("You will not be able to build your projects for Android or run them in the emulator or on a connected device." + EOL
