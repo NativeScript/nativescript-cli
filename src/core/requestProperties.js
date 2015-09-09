45c81f23beae08c32d9ceeeb912db7dd6e02bf5c
Thomas Conner <thomas.conner@me.com>
2015-09-09 15:59:24 -0400

    MLIBZ-563 Added app version to request properties class.
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

--- src/core/requestProperties.js
+++ src/core/requestProperties.js
@@ -2,7 +2,9 @@ import Cache from './cache';
 import clone from 'lodash/lang/clone';
 import merge from 'lodash/object/merge';
 const privateRequestPropertiesSymbol = Symbol();
+const sharedInstanceSymbol = Symbol();
 const requestPropertiesKey = 'requestProperties';
+const appVersionKey = 'appVersion';
 
 class PrivateRequestProperties {
   get properties() {
@@ -22,9 +24,6 @@ class PrivateRequestProperties {
 
     // Set the properties
     this.properties = properties;
-
-    // Save
-    this.save();
   }
 
   addProperties(properties = {}) {
@@ -39,41 +38,26 @@ class PrivateRequestProperties {
         delete this.properties[key];
       }
     });
-
-    // Save
-    this.save();
   }
 
   clear() {
     // Set properties to an empty object
     this.properties = {};
-
-    // Save
-    this.save();
   }
 
   clearProperty(key) {
     const properties = this.properties;
 
     // Delete the value if the properties contains the key
-    if (name && properties.hasOwnProperty(key)) {
+    if (key && properties.hasOwnProperty(key)) {
       delete properties[key];
     }
-
-    // Save
-    this.save();
-  }
-
-  save() {
-    // Save the properties to the cache
-    const cache = Cache.sharedInstance();
-    return cache.set(requestPropertiesKey, this.properties);
   }
 }
 
 class RequestProperties {
   /**
-   * Returns the request properties that have been set for the application.
+   * Return the request properties.
    *
    * @return {Object} Request properties
    */
@@ -83,7 +67,7 @@ class RequestProperties {
   }
 
   /**
-   * Replaces all request properties with the provided properties.
+   * Set the request properties.
    *
    * @param {Object} properties Request properties
    */
@@ -92,36 +76,72 @@ class RequestProperties {
     privateRequestProperties.properties = properties;
   }
 
+  /**
+   * Return the app version request property.
+   *
+   * @return {String} App version
+   */
+  get appVersion() {
+    return this.getProperty(appVersionKey);
+  }
+
+  /**
+   * Set the app version request property. The app version can be provided
+   * in major.minor.patch format or something specific to your application.
+   *
+   * @param  {Any} version  App version.
+   */
+  set appVersion(...version) {
+    const major = version[0];
+    const minor = version[1];
+    const patch = version[2];
+    let appVersion = '';
+
+    if (major) {
+      appVersion = (major + '').trim();
+    }
+
+    if (minor) {
+      appVersion = ('.' + minor).trim();
+    }
+
+    if (patch) {
+      appVersion = ('.' + patch).trim();
+    }
+
+    this.setProperty(appVersionKey, appVersion);
+  }
+
   constructor(properties = {}) {
     this[privateRequestPropertiesSymbol] = new PrivateRequestProperties(properties);
   }
 
   /**
-   * Returns the request property for the name or `undefined` if
+   * Returns the request property for the key or `undefined` if
    * it has not been set.
    *
-   * @param  {String} name Request property name
-   * @return {*}           Request property value
+   * @param  {String} key Request property key
+   * @return {*}          Request property value
    */
-  property(name) {
+  getProperty(key) {
     const properties = this.properties;
 
-    if (name && properties.hasOwnProperty(name)) {
-      return properties[name];
+    if (key && properties.hasOwnProperty(key)) {
+      return properties[key];
     }
 
     return undefined;
   }
 
   /**
-   * Sets the request property for the name and value.
+   * Sets the request property key to the value.
    *
-   * @param {String} name  Request property name
+   * @param {String} key   Request property key
    * @param {*}      value Request property value
    */
-  setProperty(name, value) {
+  setProperty(key, value) {
     const properties = {};
-    properties[name] = value;
+    properties[key] = value;
     this.addProperties(properties);
   }
 
@@ -147,11 +167,27 @@ class RequestProperties {
   /**
    * Clears the request property.
    *
-   * @param  {String} name Request property name
+   * @param  {String} key Request property key
    */
-  clearProperty(name) {
+  clearProperty(key) {
     const privateRequestProperties = this[PrivateRequestPropertiesSymbol];
-    privateRequestProperties.clearProperty(name);
+    privateRequestProperties.clearProperty(key);
+  }
+
+  /**
+   * Returns the shared instance of request properties.
+   *
+   * @return {RequestProperties} Request properties shared instance.
+   */
+  static sharedInstance() {
+    let requestProperties = RequestProperties[sharedInstanceSymbol];
+
+    if (!requestProperties) {
+      requestProperties = new RequestProperties();
+      RequestProperties[sharedInstanceSymbol] = requestProperties;
+    }
+
+    return requestProperties;
   }
 }
 
