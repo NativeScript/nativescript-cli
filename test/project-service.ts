39a3becf5351d5d729d138a80238c7a60539ad5a
Toma Popov <toma.todorov@gmail.com>
2015-10-15 17:13:56 +0300

    Change import statements. Code cleanup. Use shelljs.

--- test/project-service.ts
+++ test/project-service.ts
@@ -9,18 +9,19 @@ import * as ProjectServiceLib from "../lib/services/project-service";
 import * as ProjectDataServiceLib from "../lib/services/project-data-service";
 import * as ProjectDataLib from "../lib/project-data";
 import * as ProjectHelperLib from "../lib/common/project-helper";
-import * as StaticConfigLib from "../lib/config";
+import {StaticConfig} from "../lib/config";
 import * as NpmLib from "../lib/node-package-manager";
-import * as NpmInstallationManagerLib from "../lib/npm-installation-manager";
+import {NpmInstallationManager} from "../lib/npm-installation-manager";
 import * as HttpClientLib from "../lib/common/http-client";
-import * as fsLib from "../lib/common/file-system";
+import {FileSystem} from "../lib/common/file-system";
 import * as path from "path";
 import temp = require("temp");
 import * as helpers from "../lib/common/helpers";
 import {assert} from "chai";
-import * as optionsLib from "../lib/options";
-import * as hostInfoLib from "../lib/common/host-info";
-import iOSProjectServiceLib = require("../lib/services/ios-project-service");
+import {Options} from "../lib/options";
+import {HostInfo} from "../lib/common/host-info";
+import {IOSProjectService} from "../lib/services/ios-project-service";
+import * as shell from "shelljs";
 
 let mockProjectNameValidator = {
 	validate: () => { return true; }
@@ -111,17 +112,17 @@ class ProjectIntegrationTest {
 		this.testInjector.register("projectTemplatesService", stubs.ProjectTemplatesService);
 		this.testInjector.register("projectNameValidator", mockProjectNameValidator);
 
-		this.testInjector.register("fs", fsLib.FileSystem);
+		this.testInjector.register("fs", FileSystem);
 		this.testInjector.register("projectDataService", ProjectDataServiceLib.ProjectDataService);
-		this.testInjector.register("staticConfig", StaticConfigLib.StaticConfig);
+		this.testInjector.register("staticConfig", StaticConfig);
 
-		this.testInjector.register("npmInstallationManager", NpmInstallationManagerLib.NpmInstallationManager);
+		this.testInjector.register("npmInstallationManager", NpmInstallationManager);
 		this.testInjector.register("npm", NpmLib.NodePackageManager);
 		this.testInjector.register("httpClient", HttpClientLib.HttpClient);
 		this.testInjector.register("lockfile", stubs.LockFile);
 
-		this.testInjector.register("options", optionsLib.Options);
-		this.testInjector.register("hostInfo", hostInfoLib.HostInfo);
+		this.testInjector.register("options", Options);
+		this.testInjector.register("hostInfo", HostInfo);
 	}
 }
 
@@ -158,19 +159,19 @@ describe("Project Service Tests", () => {
 				return;
 			}
 
+			let testDirectoryPath = "/tmp/Podfile";
 			let testInjector = createInjectorForPodsTest();
 
 			let iOSProjectService: IPlatformProjectService = testInjector.resolve("iOSProjectService");
 			let fs: IFileSystem = testInjector.resolve("fs");
-			let childProcess = testInjector.resolve("childProcess");
 			let projectIntegrationTest = new ProjectIntegrationTest();
 			let workingFolderPath = temp.mkdirSync("ios_project");
 
 			let iosTemplatePath = path.join(projectIntegrationTest.getNpmPackagePath("tns-ios").wait(), "framework/");
-			childProcess.exec(`cp -R ${iosTemplatePath} ${workingFolderPath}`, { cwd: workingFolderPath }).wait();
-			fs.writeFile("/tmp/Podfile/testFile.txt", "Test content.").wait();
+			shell.cp("-R", iosTemplatePath, workingFolderPath);
+			fs.writeFile(`${testDirectoryPath}/testFile.txt`, "Test content.").wait();
 
-			let postInstallCommmand = `\`cat /tmp/Podfile/testFile.txt > ${workingFolderPath}/copyTestFile.txt && rm -rf /tmp/Podfile\``;
+			let postInstallCommmand = `\`cat ${testDirectoryPath}/testFile.txt > ${workingFolderPath}/copyTestFile.txt && rm -rf ${testDirectoryPath}\``;
 			let podfileContent = `post_install do |installer_representation| ${postInstallCommmand} end`;
 			fs.writeFile(path.join(workingFolderPath, "Podfile"), podfileContent).wait();
 
@@ -179,18 +180,17 @@ describe("Project Service Tests", () => {
 				get: () => {
 					return { projectRoot: workingFolderPath };
 				}
-    		 });
+			});
 
 			try {
 				iOSProjectService.afterPrepareAllPlugins().wait();
-			} catch(e) {
-				assert.isNotNull(e);
 			} finally {
-				Object.defineProperty(iOSProjectService, "platformData", platformData);	
+				Object.defineProperty(iOSProjectService, "platformData", platformData);
 			}
 
-			assert.isTrue(fs.exists("/tmp/Podfile").wait());
+			assert.isTrue(fs.exists(testDirectoryPath).wait());
 			assert.isTrue(fs.exists(path.join(workingFolderPath, "copyTestFile.txt")).wait());
+			fs.deleteDirectory(testDirectoryPath).wait(); // Clean up 'tmp' after test ends.
 		});
 	});
 });
@@ -205,20 +205,20 @@ function createTestInjector() {
 	testInjector.register("projectTemplatesService", stubs.ProjectTemplatesService);
 	testInjector.register("projectNameValidator", mockProjectNameValidator);
 
-	testInjector.register("fs", fsLib.FileSystem);
+	testInjector.register("fs", FileSystem);
 	testInjector.register("projectDataService", ProjectDataServiceLib.ProjectDataService);
 
-	testInjector.register("staticConfig", StaticConfigLib.StaticConfig);
+	testInjector.register("staticConfig", StaticConfig);
 
-	testInjector.register("npmInstallationManager", NpmInstallationManagerLib.NpmInstallationManager);
+	testInjector.register("npmInstallationManager", NpmInstallationManager);
 	testInjector.register("httpClient", HttpClientLib.HttpClient);
 	testInjector.register("lockfile", stubs.LockFile);
 
 	testInjector.register("childProcess", ChildProcess);
 
 	testInjector.register('projectData', ProjectDataLib.ProjectData);
-	testInjector.register("options", optionsLib.Options);
-	testInjector.register("hostInfo", hostInfoLib.HostInfo);
+	testInjector.register("options", Options);
+	testInjector.register("hostInfo", HostInfo);
 
 	return testInjector;
 }
@@ -239,13 +239,17 @@ function createInjectorForPodsTest() {
 		"USE_POD_SANDBOX": true
 	});
 	testInjector.register("prompter", {});
-	testInjector.register("fs", fsLib.FileSystem);
-	testInjector.register("staticConfig", StaticConfigLib.StaticConfig);
-	testInjector.register("npmInstallationManager", NpmInstallationManagerLib.NpmInstallationManager);
-	testInjector.register("iOSProjectService", iOSProjectServiceLib.IOSProjectService);
+	testInjector.register("fs", FileSystem);
+	testInjector.register("staticConfig", StaticConfig);
+	testInjector.register("npmInstallationManager", NpmInstallationManager);
+	testInjector.register("iOSProjectService", IOSProjectService);
+	testInjector.register("projectService", ProjectServiceLib.ProjectService);
+	testInjector.register("projectHelper", ProjectHelperLib.ProjectHelper);
+	testInjector.register("projectTemplatesService", stubs.ProjectTemplatesService);
+	testInjector.register("projectNameValidator", mockProjectNameValidator);
+	testInjector.register("options", Options);
+	testInjector.register("hostInfo", HostInfo);
 	testInjector.register("childProcess", ChildProcess);
-	testInjector.register("options", optionsLib.Options);
-	testInjector.register("hostInfo", hostInfoLib.HostInfo);
 
 	return testInjector;
 }
