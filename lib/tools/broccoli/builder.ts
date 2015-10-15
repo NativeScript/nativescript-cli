07718828e9bac8d857ad9b2d04a01a37583a90a0
Hristo Deshev <hristo@deshev.com>
2015-10-15 12:11:56 +0300

    Copy node modules even if they are below a NPM scope subdir.
    - Needed mostly by Angular's @reactivex/rxjs module.
    - Chang the broccoli builder node_modules child discovery to
    recursively walk NPM scope subdirs (any subdir starting with a @ char).
    - Change node-modules-dest-copy to copy @myscope/mymodule dependencies to
    the correct subdir.

--- lib/tools/broccoli/builder.ts
+++ lib/tools/broccoli/builder.ts
@@ -11,88 +11,100 @@ let vinylFilterSince = require("vinyl-filter-since");
 let through = require("through2");
 
 export class Builder implements IBroccoliBuilder {
-  constructor(private $fs: IFileSystem,
-    private $nodeModulesTree: INodeModulesTree,
-    private $projectData: IProjectData,
-    private $projectDataService: IProjectDataService,
-    private $injector: IInjector,
-    private $logger: ILogger) { }
+	constructor(
+		private $fs: IFileSystem,
+		private $nodeModulesTree: INodeModulesTree,
+		private $projectData: IProjectData,
+		private $projectDataService: IProjectDataService,
+		private $injector: IInjector,
+		private $logger: ILogger
+	) {
+	}
 
-    public getChangedNodeModules(absoluteOutputPath: string, platform: string, lastModifiedTime?: Date): IFuture<any> {
-        return (() => {
-            let projectDir = this.$projectData.projectDir;
-            let isNodeModulesModified = false;
-            let nodeModulesPath = path.join(projectDir, constants.NODE_MODULES_FOLDER_NAME);
-            let nodeModules: any = {};
+	public getChangedNodeModules(absoluteOutputPath: string, platform: string, lastModifiedTime?: Date): IFuture<any> {
+		return (() => {
+			let projectDir = this.$projectData.projectDir;
+			let isNodeModulesModified = false;
+			let nodeModulesPath = path.join(projectDir, constants.NODE_MODULES_FOLDER_NAME);
+			let nodeModules: any = {};
 
-            if(lastModifiedTime) {
-                let pipeline = gulp.src(path.join(projectDir, "node_modules/**"))
-                .pipe(vinylFilterSince(lastModifiedTime))
-                .pipe(through.obj( (chunk: any, enc: any, cb: Function) => {
-                    if(chunk.path === nodeModulesPath) {
-                        isNodeModulesModified = true;
-                    }
+			if(lastModifiedTime) {
+				let pipeline = gulp.src(path.join(projectDir, "node_modules/**"))
+				.pipe(vinylFilterSince(lastModifiedTime))
+				.pipe(through.obj( (chunk: any, enc: any, cb: Function) => {
+					if(chunk.path === nodeModulesPath) {
+						isNodeModulesModified = true;
+					}
 
-                    if(!isNodeModulesModified) {
-                        let rootModuleName = chunk.path.split(nodeModulesPath)[1].split(path.sep)[1];
-                        let rootModuleFullPath = path.join(nodeModulesPath, rootModuleName);
-                        nodeModules[rootModuleFullPath] = rootModuleFullPath;
-                    }
+					if(!isNodeModulesModified) {
+						let rootModuleName = chunk.path.split(nodeModulesPath)[1].split(path.sep)[1];
+						let rootModuleFullPath = path.join(nodeModulesPath, rootModuleName);
+						nodeModules[rootModuleFullPath] = rootModuleFullPath;
+					}
 
-                    cb(null);
-                }))
-                .pipe(gulp.dest(absoluteOutputPath));
+					cb(null);
+				}))
+				.pipe(gulp.dest(absoluteOutputPath));
 
-                let future = new Future<void>();
+				let future = new Future<void>();
 
-                pipeline.on('end', (err: Error, data: any) => {
-                    if(err) {
-                        future.throw(err);
-                    } else {
-                        future.return();
-                    }
-                });
+				pipeline.on('end', (err: Error, data: any) => {
+					if(err) {
+						future.throw(err);
+					} else {
+						future.return();
+					}
+				});
 
-                future.wait();
-            }
+				future.wait();
+			}
 
-            if(isNodeModulesModified && this.$fs.exists(absoluteOutputPath).wait()) {
-                let currentPreparedTnsModules = this.$fs.readDirectory(absoluteOutputPath).wait();
-                let tnsModulesPath = path.join(projectDir, constants.APP_FOLDER_NAME, constants.TNS_MODULES_FOLDER_NAME);
-                if(!this.$fs.exists(tnsModulesPath).wait()) {
-                    tnsModulesPath = path.join(projectDir, constants.NODE_MODULES_FOLDER_NAME, constants.TNS_CORE_MODULES_NAME);
-                }
-                let tnsModulesInApp = this.$fs.readDirectory(tnsModulesPath).wait();
-                let modulesToDelete = _.difference(currentPreparedTnsModules, tnsModulesInApp);
-                _.each(modulesToDelete, moduleName => this.$fs.deleteDirectory(path.join(absoluteOutputPath, moduleName)).wait());
-            }
+			if(isNodeModulesModified && this.$fs.exists(absoluteOutputPath).wait()) {
+				let currentPreparedTnsModules = this.$fs.readDirectory(absoluteOutputPath).wait();
+				let tnsModulesPath = path.join(projectDir, constants.APP_FOLDER_NAME, constants.TNS_MODULES_FOLDER_NAME);
+				if(!this.$fs.exists(tnsModulesPath).wait()) {
+					tnsModulesPath = path.join(projectDir, constants.NODE_MODULES_FOLDER_NAME, constants.TNS_CORE_MODULES_NAME);
+				}
+				let tnsModulesInApp = this.$fs.readDirectory(tnsModulesPath).wait();
+				let modulesToDelete = _.difference(currentPreparedTnsModules, tnsModulesInApp);
+				_.each(modulesToDelete, moduleName => this.$fs.deleteDirectory(path.join(absoluteOutputPath, moduleName)).wait());
+			}
 
-            if(!lastModifiedTime || isNodeModulesModified) {
-                let nodeModulesDirectories = this.$fs.exists(nodeModulesPath).wait() ? this.$fs.readDirectory(nodeModulesPath).wait() : [];
-                _.each(nodeModulesDirectories, nodeModuleDirectoryName => {
-                    let nodeModuleFullPath = path.join(nodeModulesPath, nodeModuleDirectoryName);
-                    nodeModules[nodeModuleFullPath] = nodeModuleFullPath;
-                });
-            }
+			if(!lastModifiedTime || isNodeModulesModified) {
+				this.listModules(nodeModulesPath, nodeModules);
+			}
 
-            return nodeModules;
-        }).future<any>()();
-    }
+			return nodeModules;
+		}).future<any>()();
+	}
 
-  public prepareNodeModules(absoluteOutputPath: string, platform: string, lastModifiedTime?: Date): IFuture<void> {
-    return (() => {
-        let nodeModules = this.getChangedNodeModules(absoluteOutputPath, platform, lastModifiedTime).wait();
-        let destCopy = this.$injector.resolve(destCopyLib.DestCopy, {
-            inputPath: this.$projectData.projectDir,
-            cachePath: "",
-            outputRoot: absoluteOutputPath,
-            projectDir: this.$projectData.projectDir,
-            platform: platform
-        });
+	private listModules(nodeModulesPath: string, nodeModules: any): void {
+		let nodeModulesDirectories = this.$fs.exists(nodeModulesPath).wait() ? this.$fs.readDirectory(nodeModulesPath).wait() : [];
+		_.each(nodeModulesDirectories, nodeModuleDirectoryName => {
+			let isNpmScope = /^@/.test(nodeModuleDirectoryName);
+			let nodeModuleFullPath = path.join(nodeModulesPath, nodeModuleDirectoryName);
+			if (isNpmScope) {
+				this.listModules(nodeModuleFullPath, nodeModules);
+			} else {
+				nodeModules[nodeModuleFullPath] = nodeModuleFullPath;
+			}
+		});
+	}
 
-        destCopy.rebuildChangedDirectories(_.keys(nodeModules));
+	public prepareNodeModules(absoluteOutputPath: string, platform: string, lastModifiedTime?: Date): IFuture<void> {
+		return (() => {
+			let nodeModules = this.getChangedNodeModules(absoluteOutputPath, platform, lastModifiedTime).wait();
+			let destCopy = this.$injector.resolve(destCopyLib.DestCopy, {
+				inputPath: this.$projectData.projectDir,
+				cachePath: "",
+				outputRoot: absoluteOutputPath,
+				projectDir: this.$projectData.projectDir,
+				platform: platform
+			});
 
-    }).future<void>()();
-  }
+			destCopy.rebuildChangedDirectories(_.keys(nodeModules));
+
+		}).future<void>()();
+	}
 }
 $injector.register("broccoliBuilder", Builder);
