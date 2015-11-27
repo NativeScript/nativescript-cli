c3af46f5e254d3c7c4f1809940b8648265ba9abc
Todor Totev <teobugslayer@gmail.com>
2015-11-27 20:26:12 +0200

    Check XML files for well-formedness

--- lib/commands/prepare.ts
+++ lib/commands/prepare.ts
@@ -6,7 +6,9 @@ export class PrepareCommand implements ICommand {
 		private $platformCommandParameter: ICommandParameter) { }
 
 	execute(args: string[]): IFuture<void> {
-		return this.$platformService.preparePlatform(args[0]);
+		return (() => {
+			this.$platformService.preparePlatform(args[0]);
+		}).future<void>()();
 	}
 
 	allowedParameters = [this.$platformCommandParameter];
