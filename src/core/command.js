d861cdd8e0d80ef7df69a0887b648995186ccfcb
Thomas Conner <thomas@kinvey.com>
2015-12-14 09:35:35 -0500

    Update command class
    Signed-off-by: Thomas Conner <thomas@kinvey.com>

--- src/core/command.js
+++ src/core/command.js
@@ -7,10 +7,26 @@ const assign = require('lodash/object/assign');
 const rpcNamespace = process.env.KINVEY_RPC_NAMESPACE || 'rpc';
 
 class Command {
-  execute(id, args, options = {}) {
+  constructor(id, options = {}) {
     options = assign({
       auth: Auth.default,
       client: Client.sharedInstance()
+    }, options);
+
+    this.id = id;
+    this.auth = options.auth;
+    this.client = options.client;
+  }
+
+  getPathname(client) {
+    client = client || this.client;
+    return `/${rpcNamespace}/${client.appId}/custom/${this.id}`;
+  }
+
+  execute(args, options = {}) {
+    options = assign({
+      auth: this.auth,
+      client: this.client
     }, options);
 
     const request = new Request({
@@ -18,7 +34,7 @@ class Command {
       auth: options.auth,
       client: options.client,
       method: HttpMethod.POST,
-      pathname: `/${rpcNamespace}/${options.client.appId}/custom/${id}`,
+      pathname: this.getPathname(options.client),
       data: args
     });
     const promise = request.execute();
