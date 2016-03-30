2ed581f4a4d2167ddd8cda378aabde6351997ebb
Thomas Conner <thomas@kinvey.com>
2016-03-30 15:41:55 -0400

    Use correct adapter for db persistence

--- src/rack/middleware/cache.js
+++ src/rack/middleware/cache.js
@@ -6,7 +6,7 @@ import { HttpMethod, StatusCode } from '../../enums';
  * @private
  */
 export class CacheMiddleware extends KinveyMiddleware {
-  constructor(adapters = [DBAdapter.Memory]) {
+  constructor(adapters = [DBAdapter.IndexedDB, DBAdapter.WebSQL, DBAdapter.LocalStorage, DBAdapter.Memory]) {
     super('Kinvey Cache Middleware');
     this.adapters = adapters;
   }
