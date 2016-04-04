4066a0491532bca3a7eb96ce36cdaad04e867ee2
Thomas Conner <thomas@kinvey.com>
2016-04-04 14:31:21 -0400

    Build es5

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import { KinveyProvider } from './provider';
 const ngKinvey = angular.module('kinvey', []);
 ngKinvey.provider('$kinvey', KinveyProvider);
 
-const Kinvey = {
+export default {
   name: 'kinvey'
 };
-
-// Export
-module.exports = Kinvey;
