0600f81c901ea9a41bd4366e4eaa71d739d637bc
Thomas Conner <thomas@kinvey.com>
2016-04-15 08:34:04 -0400

    Allow popup to work in browser environments

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+import { isBrowser } from './utils';
 import bind from 'lodash/bind';
 
 /**
@@ -11,13 +12,35 @@ export class PopupAdapter {
     };
 
     const promise = new Promise((resolve, reject) => {
-      this.popup = global.open(this.url, '_blank', 'location=yes');
+      if (isBrowser()) {
+        this.popup = global.open(this.url, '_blank', 'toolbar=no,location=no');
 
-      if (this.popup) {
-        this.popup.addEventListener('loadstart', this.eventListeners.loadHandler);
-        this.popup.addEventListener('exit', this.eventListeners.closeHandler);
+        if (this.popup) {
+          this.interval = setInterval(() => {
+            if (this.popup.closed) {
+              this.closeHandler();
+            } else {
+              try {
+                this.loadHandler({
+                  url: this.popup.location.href
+                });
+              } catch (e) {
+                // catch any errors due to cross domain issues
+              }
+            }
+          }, 100);
+        } else {
+          return reject(new Error('The popup was blocked.'));
+        }
       } else {
-        return reject(new Error('The popup was blocked.'));
+        this.popup = global.open(this.url, '_blank', 'location=yes');
+
+        if (this.popup) {
+          this.popup.addEventListener('loadstart', this.eventListeners.loadHandler);
+          this.popup.addEventListener('exit', this.eventListeners.closeHandler);
+        } else {
+          return reject(new Error('The popup was blocked.'));
+        }
       }
 
       return resolve(this);
@@ -42,6 +65,7 @@ export class PopupAdapter {
   }
 
   closeHandler() {
+    clearTimeout(this.interval);
     this.popup.removeEventListener('loadstart', this.eventListeners.loadHandler);
     this.popup.removeEventListener('exit', this.eventListeners.closeHander);
     this.emit('closed');
--- utils.js
+++ utils.js
@@ -1,3 +1,8 @@
+export function isBrowser() {
+  const platform = global.device.platform;
+  return platform === 'browser';
+}
+
 export function isiOS() {
   const platform = global.device.platform;
   return platform === 'iOS';
