0ae4319a12cdc2cb8c3210a897f80440beed7d71
Thomas Conner <thomas@kinvey.com>
2016-04-15 14:08:31 -0400

    Update build

--- build/client.js
+++ build/client.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.sharedInstance = exports.Client = undefined;
+exports.Client = exports.sharedClientInstance = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -33,7 +33,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var activeUserCollectionName = process.env.KINVEY_ACTIVE_USER_COLLECTION_NAME || 'kinvey_activeUser';
 var activeSocialIdentityTokenCollectionName = process.env.KINVEY_ACTIVE_SOCIAL_IDENTITY_TOKEN_COLLECTION_NAME || 'kinvey_activeSocialIdentityToken';
-var sharedInstanceSymbol = Symbol();
+var sharedClientInstance = exports.sharedClientInstance = null;
 
 /**
  * The Client class stores information regarding your application. You can create mutiple clients
@@ -218,7 +218,7 @@ var Client = exports.Client = function () {
     key: 'init',
     value: function init(options) {
       var client = new Client(options);
-      Client[sharedInstanceSymbol] = client;
+      exports.sharedClientInstance = sharedClientInstance = client;
       return client;
     }
 
@@ -233,17 +233,13 @@ var Client = exports.Client = function () {
   }, {
     key: 'sharedInstance',
     value: function sharedInstance() {
-      var client = Client[sharedInstanceSymbol];
-
-      if (!client) {
+      if (!sharedClientInstance) {
         throw new _errors.KinveyError('You have not initialized the library. ' + 'Please call Kinvey.init() to initialize the library.');
       }
 
-      return client;
+      return sharedClientInstance;
     }
   }]);
 
   return Client;
 }();
-
-var sharedInstance = exports.sharedInstance = Client[sharedInstanceSymbol];
