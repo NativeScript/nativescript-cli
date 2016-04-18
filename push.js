466cae9ebece98e74f6aa080885d6e39962abc05
Thomas Conner <thomas@kinvey.com>
2016-04-18 11:24:42 -0400

    Update build

--- push.js
+++ push.js
@@ -1,45 +1,11 @@
+import { Push as CorePush } from 'kinvey-javascript-sdk-core/build/push';
 import { KinveyError } from 'kinvey-javascript-sdk-core/build/errors';
-import { EventEmitter } from 'events';
-import { DataStore, DataStoreType } from 'kinvey-javascript-sdk-core/build/stores/datastore';
-import { HttpMethod, AuthType } from 'kinvey-javascript-sdk-core/build/enums';
-import { User } from 'kinvey-javascript-sdk-core/build/user';
-import { NetworkRequest } from 'kinvey-javascript-sdk-core/build/requests/network';
-import { Client } from 'kinvey-javascript-sdk-core/build/client';
-import { Query } from 'kinvey-javascript-sdk-core/build/query';
 import { isiOS, isAndroid } from './utils';
 import assign from 'lodash/assign';
-import url from 'url';
-const pushNamespace = process.env.KINVEY_PUSH_NAMESPACE || 'push';
 const notificationEvent = process.env.KINVEY_NOTIFICATION_EVENT || 'notification';
-const deviceCollectionName = process.env.KINVEY_DEVICE_COLLECTION_NAME || 'kinvey_device';
-const emitter = new EventEmitter();
 
-export class Push {
-  static listeners() {
-    return emitter.listeners(notificationEvent);
-  }
-
-  static onNotification(listener) {
-    return emitter.on(notificationEvent, listener);
-  }
-
-  static onceNotification(listener) {
-    return emitter.once(notificationEvent, listener);
-  }
-
-  static removeListener(listener) {
-    return emitter.removeListener(notificationEvent, listener);
-  }
-
-  static removeAllListeners() {
-    return emitter.removeAllListeners(notificationEvent);
-  }
-
-  static isSupported() {
-    return isiOS() || isAndroid();
-  }
-
-  static init(options = {}) {
+export class Push extends CorePush {
+  getDeviceId(options = {}) {
     if (!Push.isSupported()) {
       return Promise.reject(new KinveyError('Kinvey currently only supports ' +
         'push notifications on iOS and Android platforms.'));
@@ -53,8 +19,7 @@ export class Push {
         alert: true,
         badge: true,
         sound: true
-      },
-      force: false
+      }
     }, options);
 
     const promise = new Promise((resolve, reject) => {
@@ -71,7 +36,7 @@ export class Push {
       });
 
       push.on('notification', data => {
-        Push.emit(notificationEvent, data);
+        this.emit(notificationEvent, data);
       });
 
       push.on('error', error => {
@@ -79,87 +44,12 @@ export class Push {
       });
 
       return push;
-    }).then(deviceId => {
-      if (!deviceId) {
-        throw new KinveyError('Unable to retrieve the device id to register this device for push notifications.');
-      }
-
-      const store = DataStore.getInstance(deviceCollectionName, DataStoreType.Sync);
-      store.disableSync();
-      return store.findById(deviceId).then(entity => {
-        if (options.force !== true) {
-          return entity;
-        }
-
-        const user = User.getActiveUser();
-        const client = Client.sharedInstance();
-        const request = new NetworkRequest({
-          method: HttpMethod.POST,
-          url: url.format({
-            protocol: client.protocol,
-            host: client.host,
-            pathname: `/${pushNamespace}/${client.appKey}/register-device`
-          }),
-          properties: options.properties,
-          authType: user ? AuthType.Session : AuthType.Master,
-          data: {
-            platform: global.device.platform,
-            framework: 'phonegap',
-            deviceId: deviceId,
-            userId: user ? null : options.userId
-          },
-          timeout: options.timeout
-        });
-        return request.execute().then(() => store.save({ _id: deviceId, registered: true }));
-      });
     });
 
     return promise;
   }
 
-  static unregister(options = {}) {
-    if (!Push.isSupported()) {
-      return Promise.reject(new KinveyError('Kinvey currently only supports ' +
-        'push notifications on iOS and Android platforms.'));
-    }
-
-    const store = DataStore.getInstance(deviceCollectionName, DataStoreType.Sync);
-    store.disableSync();
-    const query = new Query();
-    query.equalsTo('registered', true);
-    const promise = store.find(query).then(data => {
-      if (data.length === 1) {
-        return data[0]._id;
-      }
-
-      return undefined;
-    }).then(deviceId => {
-      if (!deviceId) {
-        throw new KinveyError('This device has not been registered.');
-      }
-
-      const user = User.getActiveUser();
-      const client = Client.sharedInstance();
-      const request = new NetworkRequest({
-        method: HttpMethod.POST,
-        url: url.format({
-          protocol: client.protocol,
-          host: client.host,
-          pathname: `/${pushNamespace}/${client.appKey}/unregister-device`
-        }),
-        properties: options.properties,
-        authType: user ? AuthType.Session : AuthType.Master,
-        data: {
-          platform: global.device.platform,
-          framework: 'phonegap',
-          deviceId: deviceId,
-          userId: user ? null : options.userId
-        },
-        timeout: options.timeout
-      });
-      return request.execute().then(response => store.removeById(deviceId).then(() => response.data));
-    });
-
-    return promise;
+  static isSupported() {
+    return isiOS() || isAndroid();
   }
 }
