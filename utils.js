9045e5ad56dc3c8f4b286b9abd99a4e935510458
Thomas Conner <thomas@kinvey.com>
2016-04-22 13:32:57 -0400

    Check if global.device is defined

--- utils.js
+++ utils.js
@@ -1,14 +1,14 @@
 export function isBrowser() {
-  const platform = global.device.platform;
+  const platform = !!global.device ? global.device.platform : undefined;
   return platform === 'browser' || !platform;
 }
 
 export function isiOS() {
-  const platform = global.device.platform;
+  const platform = !!global.device ? global.device.platform : undefined;
   return platform === 'iOS';
 }
 
 export function isAndroid() {
-  const platform = global.device.platform;
+  const platform = !!global.device ? global.device.platform : undefined;
   return platform === 'Android';
 }
