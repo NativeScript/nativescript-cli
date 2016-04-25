ea4324d38a612197eb4ae551c175a71627a5bc39
Thomas Conner <thomas@kinvey.com>
2016-04-25 10:09:30 -0400

    MLIBZ-833 Unit Test Custom Endpoint

---
+++ src/endpoint.js
@@ -0,0 +1,61 @@
+import { Client } from './client';
+import { HttpMethod, AuthType } from './enums';
+import { KinveyError } from './errors';
+import { NetworkRequest } from './requests/network';
+import url from 'url';
+import assign from 'lodash/assign';
+import isString from 'lodash/isString';
+const rpcNamespace = process.env.KINVEY_RPC_NAMESPACE || 'rpc';
+
+/**
+ * Executes a custom command.
+ */
+export default class CustomEndpoint {
+  /**
+   * Execute a custom endpoint. A promise will be returned that will be resolved
+   * with the result of the command or rejected with an error.
+   *
+   * @param   {String}          endpoint                          Endpoint to execute.
+   * @param   {Object}          [args]                            Command arguments
+   * @param   {Object}          [options={}]                      Options
+   * @param   {Properties}      [options.properties]              Custom properties to send with
+   *                                                              the request.
+   * @param   {Number}          [options.timeout]                 Timeout for the request.
+   * @return  {Promise}                                           Promise
+   *
+   * @example
+   * var promise = CustomEndpoint.execute('myCustomEndpoint').then(function(data) {
+   *   ...
+   * }).catch(function(error) {
+   *   ...
+   * });
+   */
+  static async execute(endpoint, args, options = {}) {
+    options = assign({
+      client: Client.sharedInstance()
+    }, options);
+
+    if (!endpoint) {
+      throw new KinveyError('An endpoint is required.');
+    }
+
+    if (!isString(endpoint)) {
+      throw new KinveyError('The endpoint must be a string.');
+    }
+
+    const request = new NetworkRequest({
+      method: HttpMethod.POST,
+      authType: AuthType.Default,
+      url: url.format({
+        protocol: options.client.protocol,
+        host: options.client.host,
+        pathname: `/${rpcNamespace}/${options.client.appKey}/custom/${endpoint}`
+      }),
+      properties: options.properties,
+      data: args,
+      timeout: options.timeout,
+      client: options.client
+    });
+    return request.execute().then(response => response.data);
+  }
+}
---
+++ test/endpoint.spec.js
@@ -0,0 +1,83 @@
+import './setup';
+import CustomEndpoint from '../src/endpoint';
+import { KinveyError } from '../src/errors';
+import { loginUser, logoutUser } from './utils/user';
+import { randomString } from '../src/utils/string';
+import nock from 'nock';
+import chai from 'chai';
+const expect = chai.expect;
+const rpcNamespace = process.env.KINVEY_RPC_NAMESPACE || 'rpc';
+
+describe('CustomEndpoint', function () {
+  describe('execute()', function () {
+    beforeEach(function() {
+      return loginUser.call(this);
+    });
+
+    afterEach(function() {
+      return logoutUser.call(this);
+    });
+
+    it('should respond', function () {
+      expect(CustomEndpoint).itself.to.respondTo('execute');
+    });
+
+    it('should throw an error if and endpoint is not provided', function() {
+      return logoutUser.call(this).then(() => {
+        const promise = CustomEndpoint.execute();
+        return expect(promise).to.be.rejectedWith(KinveyError);
+      });
+    });
+
+    it('should throw an error if the endpoint is not a string', function() {
+      return logoutUser.call(this).then(() => {
+        const promise = CustomEndpoint.execute({});
+        return expect(promise).to.be.rejectedWith(KinveyError);
+      });
+    });
+
+    it('should return the response from the custom endpoint', function() {
+      const endpoint = 'testendpoint';
+      const reply = {
+        prop: randomString()
+      };
+      nock(this.client.baseUrl)
+        .post(`/${rpcNamespace}/${this.client.appKey}/custom/${endpoint}`, () => true)
+        .query(true)
+        .reply(200, reply, {
+          'content-type': 'application/json'
+        });
+
+      return CustomEndpoint.execute(endpoint).then(response => {
+        expect(response).to.deep.equal(reply);
+      });
+    });
+
+    it('should send args in request to custom endpoint', function() {
+      const endpoint = 'testendpoint';
+      const args = { version: 1 };
+      const reply = {
+        prop: randomString()
+      };
+      nock(this.client.baseUrl)
+        .post(`/${rpcNamespace}/${this.client.appKey}/custom/${endpoint}`, body => {
+          let allowed = true;
+          const keys = Object.keys(args);
+
+          for (const key of keys) {
+            allowed = body[key] === args[key];
+          }
+
+          return allowed;
+        })
+        .query(true)
+        .reply(200, reply, {
+          'content-type': 'application/json'
+        });
+
+      return CustomEndpoint.execute(endpoint, args).then(response => {
+        expect(response).to.deep.equal(reply);
+      });
+    });
+  });
+});
