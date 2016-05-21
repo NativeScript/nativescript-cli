2cf2e3d6a3cd898517f8419d66af5eb85a405446
Thomas Conner <thomas@kinvey.com>
2016-05-21 08:05:17 -0400

    Fix import of KinveyHttpMiddleware

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import { Kinvey } from 'kinvey-javascript-sdk-core';
+import { Kinvey as CoreKinvey } from 'kinvey-javascript-sdk-core';
 import { NetworkRack } from 'kinvey-javascript-sdk-core/es5/rack/rack';
-import { KinveyHttpMiddleware } from 'kinvey-javascript-sdk-core/es5/rack/middleware/http';
+import { HttpMiddleware as KinveyHttpMiddleware } from 'kinvey-javascript-sdk-core/es5/rack/middleware/http';
 import { HttpMiddleware } from './http';
 import { Push } from './push';
+import Device from './device';
 
 // Add Http middleware
 const networkRack = NetworkRack.sharedInstance();
 networkRack.swap(KinveyHttpMiddleware, new HttpMiddleware());
 
-const prevInit = Kinvey.init;
-Kinvey.init = (options) => {
-  // Initialize Kinvey
-  const client = prevInit(options);
+// Extend the Core Kinvey class
+class Kinvey extends CoreKinvey {
+  static init(options) {
+    // Initialize Kinvey
+    const client = super.init(options);
 
-  // Add Push module to Kinvey
-  Kinvey.Push = new Push();
+    // Add Push module to Kinvey
+    if (Device.isiOS() || Device.isAndroid()) {
+      this.Push = new Push();
+    }
 
-  // Return the client
-  return client;
-};
+    // Return the client
+    return client;
+  }
+}
 
 // Export
 module.exports = Kinvey;
