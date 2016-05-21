62289425df7c8caebcb2c572dbe8adaa0e3c9d6a
Thomas Conner <thomas@kinvey.com>
2016-05-21 08:05:27 -0400

    Fix bug with unregister in push

--- push.js
+++ push.js
@@ -1,18 +1,16 @@
 import { Device } from './device';
 import { KinveyError } from 'kinvey-javascript-sdk-core/es5/errors';
 import { EventEmitter } from 'events';
-import { DataStore, DataStoreType } from 'kinvey-javascript-sdk-core/es5/datastore';
 import { RequestMethod, AuthType } from 'kinvey-javascript-sdk-core/es5/requests/request';
 import { User } from 'kinvey-javascript-sdk-core/es5/user';
 import { NetworkRequest } from 'kinvey-javascript-sdk-core/es5/requests/network';
-import { Client } from 'kinvey-javascript-sdk-core/es5/client';
-import assign from 'lodash/assign';
+import Client from 'kinvey-javascript-sdk-core/es5/client';
 import url from 'url';
 import bind from 'lodash/bind';
 const pushNamespace = process.env.KINVEY_PUSH_NAMESPACE || 'push';
 const notificationEvent = process.env.KINVEY_NOTIFICATION_EVENT || 'notification';
-const deviceCollectionName = process.env.KINVEY_DEVICE_COLLECTION_NAME || 'kinvey_device';
-const idAttribute = process.env.KINVEY_ID_ATTRIBUTE || '_id';
+const deviceIdCollectionName = process.env.KINVEY_DEVICE_COLLECTION_NAME || 'kinvey_deviceId';
+const pushSettingsCollectionName = process.env.KINVEY_PUSH_COLLECTION_NAME || 'kinvey_pushSettings';
 const storage = global.localStorage;
 let notificationEventListener;
 
@@ -37,30 +35,22 @@ export class Push extends EventEmitter {
     }
 
     this.deviceReady = this.deviceReady.then(() => {
-      if (this.isSupported()) {
-        const pushOptions = this.client.push;
-        if (pushOptions) {
-          this.phonegapPush = global.PushNotification.init(pushOptions);
-          this.phonegapPush.on(notificationEvent, notificationEventListener);
+      try {
+        if (this.isSupported()) {
+          const pushOptions = JSON.parse(storage.getItem(pushSettingsCollectionName));
+          if (pushOptions) {
+            this.phonegapPush = global.PushNotification.init(pushOptions);
+            this.phonegapPush.on(notificationEvent, notificationEventListener);
+          }
         }
+      } catch (error) {
+        // Cactch the JSON parsing error
       }
     });
   }
 
-  get _pathname() {
+  get pathname() {
     return `/${pushNamespace}/${this.client.appKey}`;
-  }
-
-  get client() {
-    return this._client;
-  }
-
-  set client(client) {
-    if (!client) {
-      throw new KinveyError('Kinvey.Push much have a client defined.');
-    }
-
-    this._client = client;
   }
 
   isSupported() {
@@ -86,21 +76,15 @@ export class Push extends EventEmitter {
           'push notifications on iOS and Android platforms.'));
       }
 
-      options = assign({
-        force: false
-      }, options);
+      if (!global.PushNotification) {
+        throw new KinveyError('PhoneGap Push Notification Plugin is not installed.',
+          'Please refer to http://devcenter.kinvey.com/phonegap-v3.0/guides/push#ProjectSetUp for help with ' +
+          'setting up your project.');
+      }
 
+      return this.unregister();
+    }).catch(() => null).then(() => {
       const promise = new Promise((resolve, reject) => {
-        if (!global.PushNotification) {
-          return reject(new KinveyError('PhoneGap Push Notification Plugin is not installed.',
-            'Please refer to http://devcenter.kinvey.com/phonegap-v3.0/guides/push#ProjectSetUp for help with ' +
-            'setting up your project.'));
-        }
-
-        if (this.phonegapPush) {
-          this.phonegapPush.off(notificationEvent, notificationEventListener);
-        }
-
         this.phonegapPush = global.PushNotification.init(options);
         this.phonegapPush.on(notificationEvent, notificationEventListener);
 
@@ -118,43 +102,31 @@ export class Push extends EventEmitter {
           throw new KinveyError('Unable to retrieve the device id to register this device for push notifications.');
         }
 
-        return new Promise((resolve, reject) => {
-          try {
-            const entity = JSON.parse(storage.getItem(deviceCollectionName));
-            resolve(entity);
-          } catch (error) {
-            reject(error);
-          }
-        }).then(entity => {
-          if (entity && options.force !== true) {
-            return entity;
-          }
-
-          const user = User.getActiveUser(this.client);
-          const request = new NetworkRequest({
-            method: RequestMethod.POST,
-            url: url.format({
-              protocol: this.client.protocol,
-              host: this.client.host,
-              pathname: `${this._pathname}/register-device`
-            }),
-            properties: options.properties,
-            authType: user ? AuthType.Session : AuthType.Master,
-            data: {
-              platform: global.device.platform.toLowerCase(),
-              framework: 'phonegap',
-              deviceId: deviceId,
-              userId: user ? undefined : options.userId
-            },
-            timeout: options.timeout,
-            client: this.client
-          });
-          return request.execute()
-            .then(() => storage.setItem(deviceCollectionName, JSON.stringify({ _id: deviceId, registered: true })))
-            .then(() => {
-              this.client.push = options;
-            });
+        const user = User.getActiveUser(this.client);
+        const request = new NetworkRequest({
+          method: RequestMethod.POST,
+          url: url.format({
+            protocol: this.client.protocol,
+            host: this.client.host,
+            pathname: `${this.pathname}/register-device`
+          }),
+          properties: options.properties,
+          authType: user ? AuthType.Session : AuthType.Master,
+          data: {
+            platform: global.device.platform.toLowerCase(),
+            framework: 'phonegap',
+            deviceId: deviceId,
+            userId: user ? undefined : options.userId
+          },
+          timeout: options.timeout,
+          client: this.client
         });
+        return request.execute()
+          .then(response => {
+            storage.setItem(deviceIdCollectionName, deviceId);
+            storage.setItem(pushSettingsCollectionName, JSON.stringify(options));
+            return response.data;
+          });
       });
 
       return promise;
@@ -168,12 +140,9 @@ export class Push extends EventEmitter {
           'push notifications on iOS and Android platforms.'));
       }
 
-      const store = DataStore.collection(deviceCollectionName, DataStoreType.Sync);
-      store.client = this.client;
-      store.disableSync();
-
       let promise = new Promise((resolve, reject) => {
         if (this.phonegapPush) {
+          this.phonegapPush.off(notificationEvent, notificationEventListener);
           this.phonegapPush.unregister(() => {
             this.phonegapPush = null;
             resolve();
@@ -185,45 +154,39 @@ export class Push extends EventEmitter {
         resolve();
       });
 
-      promise = promise.then(() => {
-        const promise = new Promise((resolve, reject) => {
-          try {
-            const entity = JSON.parse(storage.getItem(deviceCollectionName));
-            resolve(entity);
-          } catch (error) {
-            reject(error);
+      promise = promise
+        .then(() => Promise.resolve(storage.getItem(deviceIdCollectionName)))
+        .then(deviceId => {
+          if (!deviceId) {
+            throw new KinveyError('This device has not been registered for push notifications.');
           }
-        });
-        return promise;
-      }).then(entity => {
-        if (!entity) {
-          throw new KinveyError('This device has not been registered for push notifications.');
-        }
 
-        const user = User.getActiveUser(this.client);
-        const deviceId = entity[idAttribute];
-        const request = new NetworkRequest({
-          method: RequestMethod.POST,
-          url: url.format({
-            protocol: this.client.protocol,
-            host: this.client.host,
-            pathname: `${this._pathname}/unregister-device`
-          }),
-          properties: options.properties,
-          authType: user ? AuthType.Session : AuthType.Master,
-          data: {
-            platform: global.device.platform.toLowerCase(),
-            framework: 'phonegap',
-            deviceId: deviceId,
-            userId: user ? null : options.userId
-          },
-          timeout: options.timeout,
-          client: this.client
+          const user = User.getActiveUser(this.client);
+          const request = new NetworkRequest({
+            method: RequestMethod.POST,
+            url: url.format({
+              protocol: this.client.protocol,
+              host: this.client.host,
+              pathname: `${this.pathname}/unregister-device`
+            }),
+            properties: options.properties,
+            authType: user ? AuthType.Session : AuthType.Master,
+            data: {
+              platform: global.device.platform.toLowerCase(),
+              framework: 'phonegap',
+              deviceId: deviceId,
+              userId: user ? null : options.userId
+            },
+            timeout: options.timeout,
+            client: this.client
+          });
+          return request.execute();
+        })
+        .then(response => {
+          storage.removeItem(deviceIdCollectionName);
+          storage.removeItem(pushSettingsCollectionName);
+          return response.data;
         });
-        return request.execute().then(() => storage.removeItem(deviceId));
-      }).then(() => {
-        this.client.push = null;
-      });
 
       return promise;
     });
