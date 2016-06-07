44a028d40b0c2dd85a2c86e269135e7ee5f02725
Thomas Conner <thomas@kinvey.com>
2016-06-07 09:17:13 -0400

    Check that the cordova device plugin is installed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime/runtime';
 import KinveyProvider from './provider';
+import { KinveyError } from 'kinvey-javascript-sdk-core/es5/errors';
 import { NetworkRack } from 'kinvey-javascript-sdk-core/es5/rack/rack';
 import { HttpMiddleware } from 'kinvey-javascript-sdk-core/es5/rack/middleware/http';
 import { AngularHttpMiddleware } from './http';
@@ -9,6 +10,21 @@ import { AngularPopup } from './popup';
 // Add the Http Middleware to the network rack
 const networkRack = NetworkRack.sharedInstance();
 networkRack.swap(HttpMiddleware, new AngularHttpMiddleware());
+
+// Check that the device plugin is installed
+if (AngularDevice.isPhoneGap()) {
+  const onDeviceReady = () => {
+    document.removeEventListener('deviceready', onDeviceReady);
+
+    if (typeof global.device === 'undefined') {
+      throw new KinveyError('Cordova Device Plugin is not installed.',
+        'Please refer to http://devcenter.kinvey.com/phonegap-v3.0/guides/push#ProjectSetUp for help with'
+        + ' setting up your project.');
+    }
+  };
+
+  document.addEventListener('deviceready', onDeviceReady, false);
+}
 
 // Expose globals
 global.KinveyDevice = AngularDevice;
