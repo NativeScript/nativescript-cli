65ae7da2d97857c6e0720115cd47cde3358ae57d
Thomas Conner <thomas@kinvey.com>
2016-06-13 16:10:38 -0400

    Use Html5Popup and PhoneGapPopup

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
 import { PhoneGapPopup } from 'kinvey-phonegap-sdk/es5/popup';
+import { Html5Popup } from 'kinvey-html5-sdk/es5/popup';
+import { AngularDevice } from './device';
 
 /**
  * @private
  */
-export class AngularPopup extends PhoneGapPopup {}
+export class AngularPopup {
+  constructor() {
+    // Create a popup proxy
+    if (AngularDevice.isPhoneGap()) {
+      return new PhoneGapPopup();
+    }
+
+    return new Html5Popup();
+  }
+}
