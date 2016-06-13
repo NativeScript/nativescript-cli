5efa0eecdef916c990085b0a8163764c2edc3c93
Thomas Conner <thomas@kinvey.com>
2016-06-13 11:36:36 -0400

    Always use InAppBrowser for popups

--- popup.js
+++ popup.js
@@ -33,51 +33,24 @@ export class PhoneGapPopup extends EventEmitter {
     // Wait for the deivce to be ready
     await this.deviceReady;
 
-    if (PhoneGapDevice.isPhoneGap()) {
-      // Check that the InAppBrowser plugin is installed
-      if (global.cordova && !global.cordova.InAppBrowser) {
-        throw new Error('PhoneGap InAppBrowser Plugin is not installed.',
-            'Please refer to http://http://devcenter.kinvey.com/phonegap/guides/getting-started for help with ' +
-            'setting up your project.');
-      }
+    // Check that the InAppBrowser plugin is installed
+    if (global.cordova && !global.cordova.InAppBrowser) {
+      throw new Error('PhoneGap InAppBrowser Plugin is not installed.',
+          'Please refer to http://http://devcenter.kinvey.com/phonegap/guides/getting-started for help with ' +
+          'setting up your project.');
+    }
 
-      // Open the popup
-      this.popup = global.open(url, '_blank', 'location=yes');
+    // Open the popup
+    this.popup = global.open(url, '_blank', 'location=yes');
 
-      // Listen for popup events
-      if (this.popup) {
-        this.popup.addEventListener('loadstart', this.eventListeners.loadStartCallback);
-        this.popup.addEventListener('loadstop', this.eventListeners.loadStopCallback);
-        this.popup.addEventListener('loaderror', this.eventListeners.loadErrorCallback);
-        this.popup.addEventListener('exit', this.eventListeners.exitCallback);
-      } else {
-        throw new Error('The popup was blocked.');
-      }
+    // Listen for popup events
+    if (this.popup) {
+      this.popup.addEventListener('loadstart', this.eventListeners.loadStartCallback);
+      this.popup.addEventListener('loadstop', this.eventListeners.loadStopCallback);
+      this.popup.addEventListener('loaderror', this.eventListeners.loadErrorCallback);
+      this.popup.addEventListener('exit', this.eventListeners.exitCallback);
     } else {
-      // Open the popup
-      this.popup = global.open(url, '_blank', 'toolbar=no,location=no');
-
-      if (this.popup) {
-        // Check if the popup is closed or redirect every 100ms
-        this.interval = setInterval(() => {
-          if (this.popup.closed) {
-            this.exitCallback();
-          } else {
-            try {
-              this.loadStopCallback({
-                url: this.popup.location.href
-              });
-            } catch (error) {
-              this.loadErrorCallback({
-                message: error.message,
-                url: undefined
-              });
-            }
-          }
-        }, 100);
-      } else {
-        throw new Error('The popup was blocked.');
-      }
+      throw new Error('The popup was blocked.');
     }
 
     return this;
