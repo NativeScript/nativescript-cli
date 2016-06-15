84823b5c93161a6bbc2063579df83e6e8b56d05c
Thomas Conner <thomas@kinvey.com>
2016-06-15 09:13:11 -0400

    Remove device JSON for web browser

--- device.js
+++ device.js
@@ -30,26 +30,7 @@ export class PhoneGapDevice {
       }
     };
 
-    if (PhoneGapDevice.isBrowser()) {
-      const userAgent = global.navigator.userAgent.toLowerCase();
-      const rChrome = /(chrome)\/([\w]+)/;
-      const rFirefox = /(firefox)\/([\w.]+)/;
-      const rIE = /(msie) ([\w.]+)/i;
-      const rOpera = /(opera)(?:.*version)?[ \/]([\w.]+)/;
-      const rSafari = /(safari)\/([\w.]+)/;
-      const rAppleWebkit = /(applewebkit)\/([\w.]+)/;
-      const browser = rChrome.exec(userAgent) ||
-                      rFirefox.exec(userAgent) ||
-                      rIE.exec(userAgent) ||
-                      rOpera.exec(userAgent) ||
-                      rSafari.exec(userAgent) ||
-                      rAppleWebkit.exec(userAgent) ||
-                      [];
-
-      json.device.model = global.navigator.userAgent;
-      json.os.name = browser[1];
-      json.os.version = browser[2];
-    } else if (typeof global.device !== 'undefined') {
+    if (typeof global.device !== 'undefined') {
       json.device.model = global.device.model;
       json.platform.version = global.device.cordova;
       json.os.name = global.device.platform;
