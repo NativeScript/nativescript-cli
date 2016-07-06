3a34a0c8a6aaf77d2ae75b8e3a51974c48831ff5
Thomas Conner <thomas@kinvey.com>
2016-07-06 11:16:02 -0400

    Inject angular2 http

--- http.js
+++ http.js
@@ -1,43 +1,55 @@
 import { KinveyMiddleware } from 'kinvey-javascript-sdk-core/dist/rack/middleware';
-import { Headers, Http, RequestOptions } from '@angular/http';
-import parseHeaders from 'parse-headers';
+import { provide, ReflectiveInjector } from '@angular/core';
+import { HTTP_PROVIDERS, Headers, Http, RequestOptions, XSRFStrategy } from '@angular/http';
+import { Promise } from 'es6-promise';
 import regeneratorRuntime from 'regenerator-runtime'; // eslint-disable-line no-unused-vars
 
-export class HttpMiddleware extends KinveyMiddleware {
-  // Angular2 DI
-  static get parameters() {
-    return [[Http]];
-  }
+class FakeXSRFStrategy extends XSRFStrategy {
+  configureRequest() { /* */ }
+}
+const XRSF_STRATEGY = provide(XSRFStrategy, { useValue: new FakeXSRFStrategy() });
 
-  constructor(http) {
+export class HttpMiddleware extends KinveyMiddleware {
+  constructor() {
     super('Angular2 Http Middleware');
-    this.http = http;
   }
 
   async handle(request) {
     // Call super
     await super.handle(request);
 
-    // Create the request options
-    const { url, method, rawHeaders, body } = request;
-    const headers = new Headers(rawHeaders.toJSON());
-    const options = new RequestOptions({
-      method: method,
-      headers: headers,
-      body: body
+    return new Promise((resolve, reject) => {
+      // Create the request options
+      const { url, method, headers, body } = request;
+      const ng2Headers = new Headers(headers.toJSON());
+      const options = new RequestOptions({
+        method: method,
+        headers: ng2Headers,
+        body: body
+      });
+
+      // Send the request
+      const http = ReflectiveInjector.resolveAndCreate([...HTTP_PROVIDERS, XRSF_STRATEGY]).get(Http);
+      const observable = http.request(url, options);
+      observable.subscribe(response => {
+        const headers = {};
+        const headerKeys = response.headers.keys();
+
+        for (const key of headerKeys) {
+          headers[key] = response.headers.get(key);
+        }
+
+        // Set the response on the request
+        request.response = {
+          statusCode: response.status,
+          headers: headers,
+          data: response.text()
+        };
+      }, error => {
+        reject(error);
+      }, () => {
+        resolve(request);
+      });
     });
-
-    // Send the request
-    const response = await this.http.request(url, options).toPromise();
-
-    // Set the response on the request
-    request.response = {
-      statusCode: response.status,
-      headers: parseHeaders(response.headers.toJSON()),
-      data: response.json()
-    };
-
-    // Return the request
-    return request;
   }
 }
