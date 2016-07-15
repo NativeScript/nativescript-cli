dd93c23819cb1d93df083cd564ae3a0d77a4c243
Thomas Conner <thomas@kinvey.com>
2016-07-15 16:38:24 -0400

    Return $q for Kinvey.Promise

--- kinvey.js
+++ kinvey.js
@@ -1,3 +1,17 @@
 import { Kinvey as PhoneGapKinvey } from 'kinvey-phonegap-sdk/dist/kinvey';
+import angular from 'angular'; // eslint-disable-line import/no-unresolved
+const $injector = angular.injector(['ng']);
 
-export class Kinvey extends PhoneGapKinvey {}
+export class Kinvey extends PhoneGapKinvey {
+  /**
+   * Returns the Promise class.
+   *
+   * @return {Promise} The Promise class.
+   *
+   * @example
+   * var Promise = Kinvey.Promise;
+   */
+  static get Promise() {
+    return $injector.get('$q');
+  }
+}
