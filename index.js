bb00459a2ed982674eceb146f0aa25515736fe87
Francios Delpech <R0muald@user.noreply.github.com>
2016-07-20 10:58:19 -0400

    make kinvey commonJs and AMD ready

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ networkRack.swap(CoreHttpMiddleware, new HttpMiddleware());
 Device.ready().then(() => {
   if (Device.isPhoneGap() && typeof global.device === 'undefined') {
     throw new KinveyError('Cordova Device Plugin is not installed.'
-      + ' Please refer to devcenter.kinvey.com/phonegap-v3.0/guides/getting-started for help with'
-      + ' setting up your project.');
+    + ' Please refer to devcenter.kinvey.com/phonegap-v3.0/guides/getting-started for help with'
+    + ' setting up your project.');
   }
 });
 
@@ -31,5 +31,22 @@ global.KinveyDevice = Device;
 global.KinveyPopup = Popup;
 
 // Register the SDK as a provider
-const ngKinvey = angular.module('kinvey', []);
-ngKinvey.provider('$kinvey', KinveyProvider);
+function getKinveyModule() {
+  return angular
+  .module('kinvey', [])
+  .provider('$kinvey', KinveyProvider);
+}
+
+// @see http://addyosmani.com/writing-modular-js/
+
+if (typeof window.define === 'function' && window.define.amd !== undefined) {
+  // AMD suppport
+  window.define('kinvey', [], getKinveyModule());
+} else if (typeof module !== 'undefined' && module.exports !== undefined) {
+  // CommonJS suppport
+  module.exports = getKinveyModule();
+} else {
+  // Default
+  // make kinvey available globally
+  getKinveyModule();
+}
