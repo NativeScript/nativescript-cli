d35b9c62139df12411cad46ca98381e3ace2fb9b
Thomas Conner <thomas@kinvey.com>
2016-09-13 13:41:07 -0400

    Rename provider.js to ngKinvey.js

---
+++ ngKinvey.js
@@ -0,0 +1,17 @@
+import { Kinvey } from './kinvey';
+import angular from 'angular'; // eslint-disable-line import/no-unresolved
+
+export class KinveyProvider {
+  init(options) {
+    return Kinvey.init(options);
+  }
+
+  $get() {
+    return Kinvey;
+  }
+}
+
+// Create the kinvey angular module
+const ngKinvey = angular.module('kinvey', []);
+ngKinvey.provider('$kinvey', KinveyProvider);
+export { ngKinvey };
