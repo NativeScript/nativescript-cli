ff116e87b13dfc0e6f486c7b86b50c4faad2b67e
Thomas Conner <thomas@kinvey.com>
2016-11-17 09:48:38 -0500

    Change default message for QueryError

--- src/errors/src/query.js
+++ src/errors/src/query.js
@@ -1,6 +1,6 @@
 import KinveyError from './kinvey';
 
-function QueryError(message = 'An error occurred on the query.', debug, code) {
+function QueryError(message = 'An error occurred with the query.', debug, code) {
   return KinveyError.call(this, message, debug, code);
 }
 
