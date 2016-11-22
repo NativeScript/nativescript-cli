af9f3d9383bca8186790905093ec487254d734e6
Thomas Conner <thomas@kinvey.com>
2016-11-22 10:51:18 -0500

    Make push module a singleton

--- kinvey.js
+++ kinvey.js
@@ -4,7 +4,7 @@ import Popup from './popup';
 import Push from './push';
 import assign from 'lodash/assign';
 
-export default class Kinvey extends HTML5Kinvey {
+class Kinvey extends HTML5Kinvey {
   static init(options = {}) {
     options = assign({
       deviceClass: Device,
@@ -12,12 +12,12 @@ export default class Kinvey extends HTML5Kinvey {
     }, options);
 
     // Initialize Kinvey
-    const client = super.init(options);
-
-    // // Add Push module to Kinvey
-    this.Push = new Push({ client: client });
-
-    // Return the client
-    return client;
+    return super.init(options);
   }
 }
+
+// Add Push module
+Kinvey.Push = Push;
+
+// Export
+export default Kinvey;
