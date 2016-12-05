93a9c88b18a2f052d9690acfc014c0c78311192c
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-12-06 01:39:08 +0200

    Break CLI process when Node is not supported
    As old versions of Node.js cannot be used anymore (after transpiling to ES6), we should break the process when such case happens.
    Use only var instead of let, const, as only var keyword is supported in Node.js 0.10.x

--- lib/nativescript-cli.ts
+++ lib/nativescript-cli.ts
@@ -1,7 +1,3 @@
-let node = require("../package.json").engines.node;
-// this call must be first to avoid requiring c++ dependencies
-require("./common/verify-node-version").verifyNodeVersion(node, "NativeScript", "2.5.0");
-
 require("./bootstrap");
 import * as fiber from "fibers";
 import Future = require("fibers/future");
