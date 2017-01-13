523685ab679b35e4ce35daa2f09987cea5cd3805
Tsvetan Raikov <tsvetan.raikov@telerik.com>
2017-01-13 14:10:36 +0200

    Fixed: the app doesn't exit when using --release and --justlaunch

--- lib/services/platform-service.ts
+++ lib/services/platform-service.ts
@@ -438,6 +438,9 @@ export class PlatformService implements IPlatformService {
 
 	public runPlatform(platform: string): IFuture<void> {
 		return (() => {
+			if (this.$options.release) {
+				this.$options.watch = false;
+			}
 			this.$logger.out("Starting...");
 			let action = (device: Mobile.IDevice) => {
 				return (() => {
