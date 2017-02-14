1699be577c5cb43e957eca468510b5b2817315e1
Thomas Conner <thomas@kinvey.com>
2017-02-14 10:25:38 -0500

    Fix typo on error objects

--- src/errors/src/bl.js
+++ src/errors/src/bl.js
@@ -1,7 +1,7 @@
 import BaseError from './base';
 
-export default class BLInternalError extends BaseError {
+export default class BLError extends BaseError {
   constructor(message = 'The Business Logic script did not complete. See debug message for details.', debug, code, kinveyRequestId) {
-    super('BLInternalError', message, debug, code, kinveyRequestId);
+    super('BLError', message, debug, code, kinveyRequestId);
   }
 }
