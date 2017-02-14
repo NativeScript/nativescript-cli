137cb29c64a837efca953c8c8af3b48d8105a271
Thomas Conner <thomas@kinvey.com>
2017-02-14 10:54:26 -0500

    Fix lint errors

--- src/errors/src/appProblem.js
+++ src/errors/src/appProblem.js
@@ -1,7 +1,8 @@
 import BaseError from './base';
 
 export default class AppProblemError extends BaseError {
-  constructor(message = 'There is a problem with this app backend that prevents execution of this operation. Please contact support@kinvey.com for assistance.', debug, code, kinveyRequestId) {
+  constructor(message = 'There is a problem with this app backend that prevents execution of this operation.'
+    + ' Please contact support@kinvey.com for assistance.', debug, code, kinveyRequestId) {
     super('AppProblemError', message, debug, code, kinveyRequestId);
   }
 }
--- src/errors/src/bl.js
+++ src/errors/src/bl.js
@@ -1,7 +1,8 @@
 import BaseError from './base';
 
 export default class BLError extends BaseError {
-  constructor(message = 'The Business Logic script did not complete. See debug message for details.', debug, code, kinveyRequestId) {
+  constructor(message = 'The Business Logic script did not complete.'
+    + ' See debug message for details.', debug, code, kinveyRequestId) {
     super('BLError', message, debug, code, kinveyRequestId);
   }
 }
