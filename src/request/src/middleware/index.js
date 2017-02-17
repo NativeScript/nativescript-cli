d6bfbc66a6681ab42d8aeec7e93ef242e1388194
Thomas Conner <thomas@kinvey.com>
2017-02-17 11:38:02 -0500

    Add http middleware mock

--- src/request/src/middleware/index.js
+++ src/request/src/middleware/index.js
@@ -1,5 +1,6 @@
 import CacheMiddleware from './src/cache';
 import HttpMiddleware from './src/http';
+import HttpMockMiddleware from './src/http.mock';
 import Middleware from './src/middleware';
 import ParseMiddleware from './src/parse';
 import SerializeMiddleware from './src/serialize';
@@ -8,6 +9,7 @@ import Storage, { MemoryAdapter } from './src/storage';
 export {
   CacheMiddleware,
   HttpMiddleware,
+  HttpMockMiddleware,
   MemoryAdapter,
   ParseMiddleware,
   SerializeMiddleware,
