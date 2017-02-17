aa88744c8c62a1c41d843d26e93b269286ad310d
Thomas Conner <thomas@kinvey.com>
2017-02-17 11:37:48 -0500

    Add user mock

--- src/entity/index.js
+++ src/entity/index.js
@@ -1,10 +1,12 @@
 import Acl from './src/acl';
 import Metadata from './src/metadata';
 import User from './src/user';
+import UserMock from './src/user.mock';
 
 // Export
 export {
   Acl,
   Metadata,
-  User
+  User,
+  UserMock
 };
