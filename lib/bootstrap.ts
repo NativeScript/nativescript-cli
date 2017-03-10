ef6957f0b3936f65d43d0fe504f8d20b3079e95d
Emil Tabakov <etabakov@mcsofetabakov.bedford.progress.com>
2017-03-10 16:08:24 +0200

    Remove the find plugin command. We don't see a good use case of its usage.

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -81,8 +81,6 @@ $injector.require("nodeModulesBuilder", "./tools/node-modules/node-modules-build
 $injector.require("pluginVariablesService", "./services/plugin-variables-service");
 $injector.require("pluginsService", "./services/plugins-service");
 $injector.requireCommand("plugin|*list", "./commands/plugin/list-plugins");
-$injector.requireCommand("plugin|find", "./commands/plugin/find-plugins");
-$injector.requireCommand("plugin|search", "./commands/plugin/find-plugins");
 $injector.requireCommand("plugin|add", "./commands/plugin/add-plugin");
 $injector.requireCommand("plugin|install", "./commands/plugin/add-plugin");
 $injector.requireCommand("plugin|remove", "./commands/plugin/remove-plugin");
