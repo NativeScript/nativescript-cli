55581dd2779e7f5bc1a3d671f1880582aede1d48
Thomas Conner <thomas@kinvey.com>
2017-04-14 09:58:03 -0400

    Register popup class to use

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { NetworkRack } from 'kinvey-js-sdk/dist/export';
+import { NetworkRack, User } from 'kinvey-js-sdk/dist/export';
 import Kinvey from 'kinvey-html5-sdk';
 import { HttpMiddleware } from './middleware';
+import Popup from './popup';
 import Push from './push';
 
 // Setup racks
 NetworkRack.useHttpMiddleware(new HttpMiddleware());
+
+// Setup popup
+User.usePopupClass(Popup);
 
 // Add Push module to Kinvey
 Kinvey.Push = Push;
