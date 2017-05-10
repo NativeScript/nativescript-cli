03af7ff3021dadf33b17ec93209d286eb8f433fa
vchimev <Vasil.Chimev@telerik.com>
2017-05-10 11:34:52 +0300

    Update Travis CI Scripts Set `rc` tag to the publishConfig section for the release branch.

--- .travis/add-publishConfig.js
+++ .travis/add-publishConfig.js
@@ -2,24 +2,23 @@
 
 var fsModule = require('fs');
 
-//Adds a publishConfig section to the package.json file
+// Adds a publishConfig section to the package.json file
 // and sets a tag to it
 
 var path = './package.json';
 var fileOptions = {encoding: "utf-8"};
 var content = fsModule.readFileSync(path, fileOptions);
 
-var tag = process.argv[2];
-if (!tag) {
-    console.log('Please pass the tag name as an argument!');
-    process.exit(1);
-}
-
 var packageDef = JSON.parse(content);
 if (!packageDef.publishConfig) {
     packageDef.publishConfig = {};
 }
-packageDef.publishConfig.tag = tag;
+
+if ($TRAVIS_BRANCH === 'release') {
+    packageDef.publishConfig.tag = 'rc';
+} else {
+    packageDef.publishConfig.tag = 'next';
+}
 
 var newContent = JSON.stringify(packageDef, null, '  ');
 fsModule.writeFileSync(path, newContent, fileOptions);
