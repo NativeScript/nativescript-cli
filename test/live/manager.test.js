240a996b8d9e98909296c188a520aed727b346e9
Thomas Conner <thomas@kinvey.com>
2017-05-19 11:16:28 -0400

    Add tests for live service

---
+++ test/live/manager.test.js
@@ -0,0 +1,139 @@
+import expect from 'expect';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import nock from 'nock';
+import { LiveServiceManager } from 'src/live';
+import { User } from 'src/entity';
+import { UserMock } from 'test/mocks';
+import { randomString, KinveyObservable } from 'src/utils';
+
+// Setup chai
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('LiveServiceManager', function() {
+  describe('subscribe()', function() {
+    it('should throw an error if a collection is not provided', function() {
+      const promise = LiveServiceManager.subscribe();
+      return promise.should.be.rejected;
+    });
+
+    it('should throw an error if collection is not a string', function() {
+      const promise = LiveServiceManager.subscribe({});
+      return promise.should.be.rejected;
+    });
+
+    it('should throw an error if there is no active user', function() {
+      const promise = UserMock.logout()
+        .then(() => {
+          return LiveServiceManager.subscribe('books');
+        });
+      return promise.should.be.rejected;
+    });
+
+    it('should return a stream', function() {
+      const activeUser = User.getActiveUser();
+      const collection = randomString();
+      const pubnubConfig = {
+        subscribeKey: randomString(),
+        publishKey: randomString(),
+        userChannelGroup: randomString()
+      };
+
+      nock(this.client.apiHostname)
+        .post(`/user/${this.client.appKey}/${activeUser._id}/register-realtime`, { deviceId: this.client.deviceId })
+        .reply(200, pubnubConfig);
+
+      nock(this.client.apiHostname)
+        .post(`/appdata/${this.client.appKey}/${collection}/_subscribe`, { deviceId: this.client.deviceId })
+        .reply(200);
+
+      return LiveServiceManager.subscribe(collection)
+        .then((stream) => {
+          expect(stream).toBeA(KinveyObservable);
+        });
+    });
+  });
+
+  describe('unsubscribe()', function() {
+    it('should throw an error if a collection is not provided', function() {
+      const promise = LiveServiceManager.subscribe();
+      return promise.should.be.rejected;
+    });
+
+    it('should throw an error if collection is not a string', function() {
+      const promise = LiveServiceManager.subscribe({});
+      return promise.should.be.rejected;
+    });
+
+    it('should unsubscribe all listeners', function() {
+      const activeUser = User.getActiveUser();
+      const collection = randomString();
+      const pubnubConfig = {
+        subscribeKey: randomString(),
+        publishKey: randomString(),
+        userChannelGroup: randomString()
+      };
+
+      nock(this.client.apiHostname)
+        .post(`/user/${this.client.appKey}/${activeUser._id}/register-realtime`, { deviceId: this.client.deviceId })
+        .times(2)
+        .reply(200, pubnubConfig);
+
+      nock(this.client.apiHostname)
+        .post(`/appdata/${this.client.appKey}/${collection}/_subscribe`, { deviceId: this.client.deviceId })
+        .times(2)
+        .reply(200);
+
+      return LiveServiceManager.subscribe(collection)
+        .then(() => {
+          return LiveServiceManager.subscribe(collection);
+        })
+        .then(() => {
+          nock(this.client.apiHostname)
+            .post(`/appdata/${this.client.appKey}/${collection}/_unsubscribe`, { deviceId: this.client.deviceId })
+            .times(2)
+            .reply(200);
+
+          return LiveServiceManager.unsubscribe(collection);
+        });
+    });
+
+    it('should keep subcriptions that fail to unsubscribe', function() {
+      const activeUser = User.getActiveUser();
+      const collection = randomString();
+      const pubnubConfig = {
+        subscribeKey: randomString(),
+        publishKey: randomString(),
+        userChannelGroup: randomString()
+      };
+
+      nock(this.client.apiHostname)
+        .post(`/user/${this.client.appKey}/${activeUser._id}/register-realtime`, { deviceId: this.client.deviceId })
+        .times(2)
+        .reply(200, pubnubConfig);
+
+      nock(this.client.apiHostname)
+        .post(`/appdata/${this.client.appKey}/${collection}/_subscribe`, { deviceId: this.client.deviceId })
+        .times(2)
+        .reply(200);
+
+      const promise = LiveServiceManager.subscribe(collection)
+        .then(() => {
+          return LiveServiceManager.subscribe(collection);
+        })
+        .then(() => {
+          nock(this.client.apiHostname)
+            .post(`/appdata/${this.client.appKey}/${collection}/_unsubscribe`, { deviceId: this.client.deviceId })
+            .reply(200);
+
+          nock(this.client.apiHostname)
+            .post(`/appdata/${this.client.appKey}/${collection}/_unsubscribe`, { deviceId: this.client.deviceId })
+            .reply(500);
+
+          return LiveServiceManager.unsubscribe(collection);
+        });
+      return promise.should.be.rejected;
+    });
+  });
+});
