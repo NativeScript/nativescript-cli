8d288b1eb8340076a498ec29ae5322631fcfa682
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2017-05-24 18:11:52 +0300

    Fix ios logs with logLevel INFO
    Whenever an ios device log is triggered with logLevel INFO `console.log` statements are not seen on the console for ios 8 and ios 9 devices due to a difference in their formats.
    Fix this and add tests.

--- lib/services/ios-log-filter.ts
+++ lib/services/ios-log-filter.ts
@@ -4,18 +4,18 @@ import { cache } from "../common/decorators";
 import * as iOSLogFilterBase from "../common/mobile/ios/ios-log-filter";
 
 export class IOSLogFilter extends iOSLogFilterBase.IOSLogFilter implements Mobile.IPlatformLogFilter {
-	protected infoFilterRegex = /^.*?(<Notice>:.*?((CONSOLE LOG|JS ERROR).*?)|(<Warning>:.*?)|(<Error>:.*?))$/im;
+	protected infoFilterRegex = /^.*?(<Notice>:.*?(((?:CONSOLE|JS) (?:LOG|ERROR)).*?))$/im;
 
 	private partialLine: string = null;
 
 	constructor($loggingLevels: Mobile.ILoggingLevels,
 		private $fs: IFileSystem,
 		private $projectData: IProjectData) {
-			super($loggingLevels);
-		}
+		super($loggingLevels);
+	}
 
 	public filterData(data: string, logLevel: string, pid?: string): string {
-		data  = super.filterData(data, logLevel, pid);
+		data = super.filterData(data, logLevel, pid);
 		if (pid && data && data.indexOf(`[${pid}]`) === -1) {
 			return null;
 		}
