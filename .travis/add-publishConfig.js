e182625e62b8a32cf3a83581fc1f9067bb282a10
vchimev <Vasil.Chimev@telerik.com>
2017-06-22 17:20:48 +0300

    Update Travis CI Scripts
    Fix publish to S3

--- .travis/add-publishConfig.js
+++ .travis/add-publishConfig.js
@@ -1,25 +1,21 @@
 #!/usr/bin/env node
 
-var fsModule = require("fs");
+const fsModule = require("fs");
+const path = "./package.json";
+const fileOptions = {encoding: "utf-8"};
+const content = fsModule.readFileSync(path, fileOptions);
 
-// Adds a publishConfig section to the package.json file
-// and sets a tag to it
-
-var path = "./package.json";
-var fileOptions = {encoding: "utf-8"};
-var content = fsModule.readFileSync(path, fileOptions);
-
-var packageDef = JSON.parse(content);
+const packageDef = JSON.parse(content);
 if (!packageDef.publishConfig) {
     packageDef.publishConfig = {};
 }
 
-var branch = process.argv[2];
+const branch = process.argv[2];
 if (!branch) {
     console.log("Please pass the branch name as an argument!");
     process.exit(1);
 }
 packageDef.publishConfig.tag = branch === "release" ? "rc" : "next";
 
-var newContent = JSON.stringify(packageDef, null, "  ");
+const newContent = JSON.stringify(packageDef, null, "  ");
 fsModule.writeFileSync(path, newContent, fileOptions);
