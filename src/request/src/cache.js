f06699170be8eb084fe2d35c220beede35ad4ef3
Thomas Conner <thomas@kinvey.com>
2017-07-07 16:54:38 -0400

    Don't suppress URI Malformed error

--- src/request/src/cache.js
+++ src/request/src/cache.js
@@ -70,14 +70,7 @@ export default class CacheRequest extends Request {
 
   set url(urlString) {
     super.url = urlString;
-    let pathname = url.parse(urlString).pathname;
-
-    try {
-      pathname = global.decodeURIComponent(url.parse(urlString).pathname);
-    } catch (error) {
-      // Just catch the URI Malformed error
-    }
-
+    let pathname = global.decodeURIComponent(url.parse(urlString).pathname);
     const urlParts = pathname.replace(/^\//g, '').split('/');
     // "pathname" has the following form "/namespace/appKey/collection/id"
     this.appKey = urlParts[1];
