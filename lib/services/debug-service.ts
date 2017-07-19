dea71ce1f08730170bd004780e848e7c7eada458
Rosen Vladimirov <rosen-vladimirov@users.noreply.github.com>
2017-07-19 14:22:58 +0300

    Fix debug when using CLI as library (#2984)
    When CLI is used as a library, the method for debug returns incorrect result - previously the return type of the method we are calling was `string[]`, we've changed it to `string` but forgot to remove the `_.first(result)`.
    So now the debug returns incorrect URL. Remove the `_.first` and return the result directly.

--- lib/services/debug-service.ts
+++ lib/services/debug-service.ts
@@ -64,7 +64,7 @@ export class DebugService extends EventEmitter implements IDebugService {
 			result = await debugService.debug(debugData, debugOptions);
 		}
 
-		return _.first(result);
+		return result;
 	}
 
 	public getDebugService(device: Mobile.IDevice): IPlatformDebugService {
