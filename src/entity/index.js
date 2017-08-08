95a2de340b8d3495d83535517e27f77915a52905
tejasranade <tejas.ranade@gmail.com>
2017-08-08 15:30:05 -0400

    One more activeuserhelper reference

--- src/entity/index.js
+++ src/entity/index.js
@@ -1,12 +1,10 @@
 import Acl from './src/acl';
 import Metadata from './src/metadata';
 import User from './src/user';
-import { ActiveUserHelper } from './src/activeUserHelper';
 
 // Export
 export {
   Acl,
   Metadata,
-  User,
-  ActiveUserHelper
+  User
 };
