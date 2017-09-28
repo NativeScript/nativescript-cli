e1cd8dd9bb31a69ba9e11242677e936e477a39a3
Thomas Conner <thomas@kinvey.com>
2017-09-28 16:48:50 -0400

    Import nativescript-nodeify to allow the use of node modules

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import 'nativescript-nodeify';
 import { CacheRack, NetworkRack } from 'kinvey-js-sdk/dist/request';
 import { MobileIdentityConnect } from 'kinvey-js-sdk/dist/identity';
 import { Kinvey } from './kinvey';
