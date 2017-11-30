979e725f7d8a814c23418dac68bba0e2961e1089
Georgi Prodanov <georgi.prodanov@progress.com>
2017-11-30 15:26:50 +0200

    MLIBZ-2213 Subscribe for messages sent to user's personal collections channel.

--- packages/kinvey-live/src/collection/live-collection-manager.js
+++ packages/kinvey-live/src/collection/live-collection-manager.js
@@ -23,7 +23,10 @@ class LiveCollectionManager {
     return this._makeSubscriptionRequest(name, '_subscribe')
       .then(() => {
         const channelName = this._buildChannelName(name);
-        getLiveService().subscribeToChannel(channelName, receiver);
+        const personalChannelName = this._buildPersonalChannelName(name);
+        const liveService = getLiveService();
+        liveService.subscribeToChannel(channelName, receiver);
+        liveService.subscribeToChannel(personalChannelName, receiver);
       });
   }
 
@@ -35,7 +38,10 @@ class LiveCollectionManager {
     return this._makeSubscriptionRequest(name, '_unsubscribe')
       .then(() => {
         const channelName = this._buildChannelName(name);
-        getLiveService().unsubscribeFromChannel(channelName);
+        const personalChannelName = this._buildPersonalChannelName(name);
+        const liveService = getLiveService();
+        liveService.unsubscribeFromChannel(channelName);
+        liveService.unsubscribeFromChannel(personalChannelName);
       });
   }
 
@@ -46,6 +52,12 @@ class LiveCollectionManager {
    */
   _buildChannelName(collectionName) {
     return `${this._client.appKey}.c-${collectionName}`;
+  }
+
+  _buildPersonalChannelName(collectionName) {
+    const channelName = this._buildChannelName(collectionName);
+    const userId = this._client.getActiveUser()._id;
+    return `${channelName}.u-${userId}`;
   }
 
   /**
@@ -61,6 +73,7 @@ class LiveCollectionManager {
     });
   }
 }
+
 exports.LiveCollectionManager = LiveCollectionManager;
 
 /** @type {LiveCollectionManager} */
@@ -75,4 +88,5 @@ function getLiveCollectionManager(client) {
   }
   return managerInstance;
 }
+
 exports.getLiveCollectionManager = getLiveCollectionManager;
