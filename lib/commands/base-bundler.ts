37c6e9716f29320eff8b31a6b5355b06f456f928
Dimitar Kerezov <d.kerezov@gmail.com>
2017-12-11 14:35:23 +0200

    Safeguard bundle option check (#3256)
    In case the project does not have dependencies or dev-dependencies CLI should fail with correct message when `--bundle` is passed.

--- lib/commands/base-bundler.ts
+++ lib/commands/base-bundler.ts
@@ -12,7 +12,9 @@ export abstract class BundleBase {
 	protected validateBundling(): void {
 		if (this.$options.bundle) {
 			const bundlePluginName = this.bundlersMap[this.$options.bundle];
-			if (!bundlePluginName || (!this.$projectData.devDependencies[bundlePluginName] && !this.$projectData.dependencies[bundlePluginName])) {
+			const hasBundlerPluginAsDependency = this.$projectData.dependencies && this.$projectData.dependencies[bundlePluginName];
+			const hasBundlerPluginAsDevDependency = this.$projectData.devDependencies && this.$projectData.devDependencies[bundlePluginName];
+			if (!bundlePluginName || (!hasBundlerPluginAsDependency && !hasBundlerPluginAsDevDependency)) {
 				this.$errors.fail("Passing --bundle requires a bundling plugin. No bundling plugin found or the specified bundling plugin is invalid.");
 			}
 		}
