4265f3e9883944042e91ad9c0b8e589e5b68d0a9
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-12-13 10:44:53 +0200

    Changed the require to use .Kinvey

--- packages/kinvey-nativescript-sdk/test/template/main-page.js
+++ packages/kinvey-nativescript-sdk/test/template/main-page.js
@@ -3,6 +3,6 @@ var runner = require('./testRunner.bundle');
 externalConfig = require('./config.js');
 utilities = require('./tests/utilities.js');
 Constants = require('./tests/constants.js');
-Kinvey = require('kinvey-nativescript-sdk');
+Kinvey = require('kinvey-nativescript-sdk').Kinvey;
 runner.initialize(tests, { logServerPort });
 runner.runAllTests();
