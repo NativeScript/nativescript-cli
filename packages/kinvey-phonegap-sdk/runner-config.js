de1d9882bafe88d91b21a471c35d6d68d32f25eb
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-01-04 14:26:41 +0200

    Added a runPipeline function in order to be able to execute it for iOS and android

--- packages/kinvey-phonegap-sdk/runner-config.js
+++ packages/kinvey-phonegap-sdk/runner-config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const walk = require('klaw-sync');
 const fs = require('fs-extra');
-const osType = process.argv[2] || 'android';
 
 const {
     Runner,
@@ -26,78 +25,85 @@ const commonTestsPath = path.join(rootMonoRepoPath, 'test', 'integration');
 const distPath = path.join(__dirname, 'dist');
 let logServerPort;
 
-const runner = new Runner({
-    pipeline: [
-        logServer(),
-        remove(distPath),
-        remove(appRootPath),
-        runCommand({
-            command: 'npm',
-            args: ['run', 'build'],
-            cwd: rootMonoRepoPath
-        }),
-        runCommand({
-            command: 'cordova',
-            args: ['create', appName],
-            cwd: __dirname
-        }),
-        copy(path.join(__dirname, 'test', 'template'), appPath),
-        copy(distPath, appPath),
-        copy(
-            shimTestsPath,
-            appTestsPath
-        ),
-        copy(
-            commonTestsPath,
-            appTestsPath
-        ),
-        processTemplateFile(
-            path.join(appPath, 'index.template.hbs'),
-            () => ({
-                tests: walk(path.join(appPath, 'tests'), {
-                    nodir: true
-                }).map(f => `./${path.relative(appPath, f.path)}`),
-                logServerPort
+
+function runPipeline(osName) {
+
+    const runner = new Runner({
+        pipeline: [
+            logServer(),
+            remove(distPath),
+            remove(appRootPath),
+            runCommand({
+                command: 'npm',
+                args: ['run', 'build'],
+                cwd: rootMonoRepoPath
             }),
-            path.join(appPath, 'index.html')
-        ),
-        copyTestRunner(appPath),
-        runCommand({
-            command: 'adb',
-            args: [
-                'reverse',
-                () => `tcp:${logServerPort}`,
-                () => `tcp:${logServerPort}`
-            ]
-        }),
-        runCommand({
-            command: 'cordova',
-            args: ['platform', 'add', osType],
-            cwd: appRootPath
-        }),
-        ...[
-            'https://github.com/apache/cordova-plugin-file.git',
-            'https://github.com/apache/cordova-plugin-whitelist',
-            'https://github.com/apache/cordova-plugin-file-transfer.git',
-            'cordova-sqlite-storage'
-        ].map(p => {
-            return runCommand({
+            runCommand({
                 command: 'cordova',
-                args: ['plugin', 'add', '--force', p],
+                args: ['create', appName],
+                cwd: __dirname
+            }),
+            copy(path.join(__dirname, 'test', 'template'), appPath),
+            copy(distPath, appPath),
+            copy(
+                shimTestsPath,
+                appTestsPath
+            ),
+            copy(
+                commonTestsPath,
+                appTestsPath
+            ),
+            processTemplateFile(
+                path.join(appPath, 'index.template.hbs'),
+                () => ({
+                    tests: walk(path.join(appPath, 'tests'), {
+                        nodir: true
+                    }).map(f => `./${path.relative(appPath, f.path)}`),
+                    logServerPort
+                }),
+                path.join(appPath, 'index.html')
+            ),
+            copyTestRunner(appPath),
+            runCommand({
+                command: 'adb',
+                args: [
+                    'reverse',
+                    () => `tcp:${logServerPort}`,
+                    () => `tcp:${logServerPort}`
+                ]
+            }),
+            runCommand({
+                command: 'cordova',
+                args: ['platform', 'add', osName],
                 cwd: appRootPath
-            });
-        }),
-        runCommand({
-            command: 'cordova',
-            args: ['run', osType],
-            cwd: appRootPath
-        })
-    ]
-});
+            }),
+            ...[
+                'https://github.com/apache/cordova-plugin-file.git',
+                'https://github.com/apache/cordova-plugin-whitelist',
+                'https://github.com/apache/cordova-plugin-file-transfer.git',
+                'cordova-sqlite-storage'
+            ].map(p => {
+                return runCommand({
+                    command: 'cordova',
+                    args: ['plugin', 'add', '--force', p],
+                    cwd: appRootPath
+                });
+            }),
+            runCommand({
+                command: 'cordova',
+                args: ['run', osName],
+                cwd: appRootPath
+            })
+        ]
+    });
 
-runner.on('log.start', port => (logServerPort = port));
 
-runner
-    .run()
-    .then(() => console.log('done'))
-    .catch(err => console.log(err));
+    runner.on('log.start', port => (logServerPort = port));
+
+    runner
+        .run()
+        .then(() => console.log('done'))
+        .catch(err => console.log(err));
+}
+
+module.exports = runPipeline;
