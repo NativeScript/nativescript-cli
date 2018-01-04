c273a4855a565df21928500c9f3c87ce0d12ae45
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-01-04 15:24:47 +0200

    Added a configurable execution

---
+++ test/platform-config.js
@@ -0,0 +1,19 @@
+const program = require('commander');
+
+program
+  .option('--platform [type]', 'Add Platform [html5/phonegap/nativescript]')
+  .option('--os [type]', 'Add OS [android/ios]')
+  .parse(process.argv);
+
+
+const runnerConfigFilePath = `../packages/kinvey-${program.platform}-sdk/runner-config`;
+switch (program.platform) {
+  case 'html5':
+    require(runnerConfigFilePath);
+    break;
+  case 'phonegap':
+  case 'nativescript':
+    const runPipeline = require(runnerConfigFilePath);
+    runPipeline(program.os);
+    break;
+}
