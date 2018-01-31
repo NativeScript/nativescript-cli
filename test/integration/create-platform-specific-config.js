e0ec36c3ac0e2aa5f545dbc43c3ecd14c701f783
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-01-31 17:59:05 +0200

    Moved several variables outside the createPlatformSpecificConfig function

--- test/integration/create-platform-specific-config.js
+++ test/integration/create-platform-specific-config.js
@@ -2,33 +2,30 @@ const fs = require('fs');
 const path = require('path');
 const lodash = require('lodash');
 
-const createPlatformSpecificConfig = (platform, os) => {
-  const rootPath = path.join(__dirname, '../../');
-  const integrationTestsFilePath = path.join(rootPath, 'test', 'integration');
-  const configTemplateFilePath = path.join(integrationTestsFilePath, 'configs', 'config.template');
-  const testsConfigFilePath = path.join(integrationTestsFilePath, 'configs', 'tests-config');
+const rootPath = path.join(__dirname, '../../');
+const integrationTestsFilePath = path.join(rootPath, 'test', 'integration');
+const configTemplateFilePath = path.join(integrationTestsFilePath, 'configs', 'config.template');
+const testsConfigFilePath = path.join(integrationTestsFilePath, 'configs', 'tests-config');
+const config = require(testsConfigFilePath);
+const testsConfig = config.testsConfig;
+const appCredentials = config.appCredentials;
+const crossPlatformExport = fs.readFileSync(configTemplateFilePath, 'utf8');
+const compiled = lodash.template(crossPlatformExport);
 
-  const resultConfigFilePath = path.join(rootPath, 'packages', `kinvey-${platform}-sdk`, 'test', 'config.js');
-
-
-  const config = require(testsConfigFilePath);
-  const testsConfig = config.testsConfig;
-  const appCredentials = config.appCredentials;
-
-  const getCredentialsByEnvironment = (appConfig, platform, os) => {
-    const app = appConfig[platform][os] || appConfig[platform];
-    return {
-      appKey: app.appKey,
-      appSecret: app.appSecret
-    };
+const getCredentialsByEnvironment = (appConfig, platform, os) => {
+  const app = appConfig[platform][os] || appConfig[platform];
+  return {
+    appKey: app.appKey,
+    appSecret: app.appSecret
   };
+};
 
+const createPlatformSpecificConfig = (platform, os) => {
+  const resultConfigFilePath = path.join(rootPath, 'packages', `kinvey-${platform}-sdk`, 'test', 'config.js');
   const credentialsToUse = getCredentialsByEnvironment(appCredentials, platform, os);
   Object.assign(testsConfig, credentialsToUse);
-
-  const crossPlatformExport = fs.readFileSync(configTemplateFilePath, 'utf8');
-  const compiled = lodash.template(crossPlatformExport);
   const configFileContents = compiled({ appConfig: JSON.stringify(testsConfig, null, 2) });
+  
   fs.writeFileSync(resultConfigFilePath, configFileContents, 'utf8');
 };
 
