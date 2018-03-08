bd28d75ff07e714faae26866ecaabfbf48be601c
Dimitar Kerezov <d.kerezov@gmail.com>
2018-03-08 17:05:17 +0200

    fix: pass devices for debugging correctly
    Pass `deviceDebugMap` correctly in order to fix `tns debug`

--- lib/commands/debug.ts
+++ lib/commands/debug.ts
@@ -38,7 +38,9 @@ export class DebugPlatformCommand implements ICommand {
 		await this.$devicesService.detectCurrentlyAttachedDevices({ shouldReturnImmediateResult: false, platform: this.platform });
 
 		await this.$liveSyncCommandHelper.executeLiveSyncOperation([selectedDeviceForDebug], this.platform, {
-			[selectedDeviceForDebug.deviceInfo.identifier]: true,
+			deviceDebugMap: {
+				[selectedDeviceForDebug.deviceInfo.identifier]: true
+			},
 			// This will default in the liveSyncCommandHelper
 			buildPlatform: undefined
 		});
