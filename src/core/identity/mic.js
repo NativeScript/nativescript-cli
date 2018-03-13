511b9955aa1b33cf3009f25bd3c817533c18e135
tejasranade <tejas.ranade@gmail.com>
2018-03-13 16:33:58 -0400

    Add scope parameter to mic for v3.

--- src/core/identity/mic.js
+++ src/core/identity/mic.js
@@ -133,7 +133,8 @@ export class MobileIdentityConnect extends Identity {
         query: {
           client_id: clientId,
           redirect_uri: redirectUri,
-          response_type: 'code'
+          response_type: 'code',
+          scope: 'openid'
         }
       }));
     }).then((popup) => {
@@ -204,7 +205,8 @@ export class MobileIdentityConnect extends Identity {
           username: options.username,
           password: options.password
         },
-        followRedirect: false
+        followRedirect: false,
+        scope: 'openid'
       });
       return request.execute();
     }).then((response) => {
