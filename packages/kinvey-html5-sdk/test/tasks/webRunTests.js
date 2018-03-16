bd5d518b6946bb1321c7b401ba813b2966ac2d5f
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-03-16 14:42:07 +0200

    added --user-data-dir option

--- packages/kinvey-html5-sdk/test/tasks/webRunTests.js
+++ packages/kinvey-html5-sdk/test/tasks/webRunTests.js
@@ -13,7 +13,7 @@ const webRunTests = (staticPort, runner) =>
         .then(resolve)
         .catch(reject);
     } else {
-      const chrome = spawnHeadlessChromium(args);
+      const chrome = spawnHeadlessChromium([args[0], '--user-data-dir']);
       chrome.stderr.on('data', d => reject(d.toString()));
       resolve();
     }
