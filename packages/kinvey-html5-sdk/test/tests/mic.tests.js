5c15643f5f17381b1ac1c1c517e045a7865d5399
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-03-23 16:56:06 +0200

    Added an additional verification

--- packages/kinvey-html5-sdk/test/tests/mic.tests.js
+++ packages/kinvey-html5-sdk/test/tests/mic.tests.js
@@ -157,6 +157,10 @@ function testFunc() {
       Kinvey.User.loginWithMIC(redirectUrl, Kinvey.AuthorizationGrant.AuthorizationCodeLoginPage, { micId: authServiceId })
         .then((user) => {
           validateMICUser(user, false, true);
+          return Kinvey.User.exists(user.username)
+        })
+        .then((existsOnServer) => {
+          expect(existsOnServer).to.be.true;
           return networkstore.find().toPromise()
         })
         .then((result) => {
