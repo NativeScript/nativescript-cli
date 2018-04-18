e0578e559296bc7c1f60871b04e96d36466cc32b
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-04-18 11:16:09 +0300

    Added tests for stream

--- test/integration/tests/files.tests.js
+++ test/integration/tests/files.tests.js
@@ -128,8 +128,87 @@ function testFunc() {
           .catch(done);
       });
 
-      it('should return and error if the _id does not exist', (done) => {
+      it('should return and error if the file with the supplied _id does not exist on the server', (done) => {
         Kinvey.Files.download(utilities.randomString())
+          .catch((error) => {
+            expect(error.message).to.equal('This blob not found for this app backend');
+            done();
+          })
+          .catch(done);
+      });
+    });
+
+    describe('stream', () => {
+      let uploadedFile;
+      const fileContent = utilities.randomString();
+
+      before((done) => {
+        Kinvey.Files.upload(fileContent, { 'mimeType': plainTextMimeType })
+          .then((result) => { uploadedFile = result; })
+          .then(() => done())
+          .catch(done);
+      });
+
+      it('should stream the file by https when tls is not set', (done) => {
+        Kinvey.Files.stream(uploadedFile._id)
+          .then((result) => {
+            assertFileMetadata(result, uploadedFile._id, plainTextMimeType, uploadedFile._filename);
+            expect(result._downloadURL).to.contain('https://');
+            return Kinvey.Files.downloadByUrl(result._downloadURL);
+          })
+          .then((result) => {
+            expect(result).to.exist;
+            expect(result).to.equal(fileContent);
+            done();
+          })
+          .catch(done);
+      });
+
+      it('should stream the file by https when tls = true', (done) => {
+        Kinvey.Files.stream(uploadedFile._id, { tls: true })
+          .then((result) => {
+            assertFileMetadata(result, uploadedFile._id, plainTextMimeType, uploadedFile._filename);
+            expect(result._downloadURL).to.contain('https://');
+            return Kinvey.Files.downloadByUrl(result._downloadURL);
+          })
+          .then((result) => {
+            expect(result).to.exist;
+            expect(result).to.equal(fileContent);
+            done();
+          })
+          .catch(done);
+      });
+
+      it('should stream the file by http when tls = false', (done) => {
+        Kinvey.Files.stream(uploadedFile._id, { tls: false })
+          .then((result) => {
+            assertFileMetadata(result, uploadedFile._id, plainTextMimeType, uploadedFile._filename);
+            expect(result._downloadURL).to.contain('http://');
+            done();
+          })
+          .catch(done);
+      });
+
+      it('should set correctly ttl', (done) => {
+        const ttlValue = 1;
+        // After the fix of MLIBZ-2453, the downloadByUrl assertion should be modified to check the error and moved to the error function
+        Kinvey.Files.stream(uploadedFile._id, { ttl: ttlValue })
+          .then((result) => {
+            assertFileMetadata(result, uploadedFile._id, plainTextMimeType, uploadedFile._filename);
+            setTimeout(() => {
+              return Kinvey.Files.downloadByUrl(result._downloadURL)
+                .then((result) => {
+                  expect(result).to.contain('The provided token has expired.')
+                  done();
+                })
+                .catch(done);
+            }, ttlValue + 500);
+          })
+          .catch(done);
+      });
+
+      it('should return and error if the _id does not exist', (done) => {
+        Kinvey.Files.stream(utilities.randomString())
           .catch((error) => {
             expect(error.message).to.equal('This blob not found for this app backend');
             done();
