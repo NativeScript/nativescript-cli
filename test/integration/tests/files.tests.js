1afb4eb076b6a34702048dc398edeecb704c1f77
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-04-19 17:41:26 +0300

    Removed some duplicated tests

--- test/integration/tests/files.tests.js
+++ test/integration/tests/files.tests.js
@@ -164,67 +164,6 @@ function testFunc() {
             })
             .catch(done);
         });
-
-        it('should return the metadata for all files that match the query', (done) => {
-          Kinvey.Files.find(query)
-            .then((result) => {
-              expect(result).to.be.an('array');
-              expect(result.length).to.equal(1);
-              assertFileMetadata(result[0], uploadedFile2._id, plainTextMimeType, uploadedFile2._filename);
-              done();
-            })
-            .catch(done);
-        });
-
-        it('should return the file by http if tls = false', (done) => {
-          Kinvey.Files.find(query, { tls: false })
-            .then((result) => {
-              assertFileMetadata(result[0], uploadedFile2._id, plainTextMimeType, uploadedFile2._filename, true);
-              done();
-            })
-            .catch(done);
-        });
-
-        it('should set correctly ttl', (done) => {
-          const ttlValue = 1;
-          // After the fix of MLIBZ-2453, the downloadByUrl assertion should be modified to check the error and moved to the error function
-          Kinvey.Files.find(query, { ttl: ttlValue })
-            .then((result) => {
-              assertFileMetadata(result[0], uploadedFile2._id, plainTextMimeType, uploadedFile2._filename);
-              setTimeout(() => {
-                return Kinvey.Files.downloadByUrl(result[0]._downloadURL)
-                  .then((result) => {
-                    expect(result).to.contain('The provided token has expired.')
-                    done();
-                  })
-                  .catch(done);
-              }, ttlValue + 1000);
-            })
-            .catch(done);
-        });
-
-        it('should download all files with download = true', (done) => {
-          Kinvey.Files.find(null, { download: true })
-            .then((result) => {
-              expect(result).to.be.an('array');
-              expect(result.length).to.equal(2);
-              expect(result.find(fileContent => fileContent === fileContent1)).to.exist;
-              expect(result.find(fileContent => fileContent === fileContent2)).to.exist;
-              done();
-            })
-            .catch(done);
-        });
-
-        it('should download all files which match the query with download = true', (done) => {
-          Kinvey.Files.find(query, { download: true })
-            .then((result) => {
-              expect(result).to.be.an('array');
-              expect(result.length).to.equal(1);
-              expect(result[0]).to.equal(fileContent2);
-              done();
-            })
-            .catch(done);
-        });
       });
 
       describe('download()', () => {
