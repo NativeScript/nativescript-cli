b54cbc19f4eac55eb42ac4fc6b6c373646e9c5ce
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-04-23 16:45:40 +0300

    Added more tests for Files.upload()

--- test/integration/tests/files/files-web-pg.tests.js
+++ test/integration/tests/files/files-web-pg.tests.js
@@ -5,6 +5,15 @@ function testFunc() {
   const plainTextMimeType = 'text/plain';
   const shouldNotBeCalledMessage = 'Should not be called';
 
+  const ArrayBufferFromString = (str) => {
+    const buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    const bufView = new Uint16Array(buf);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
   describe('Files', () => {
 
     before((done) => {
@@ -49,12 +58,65 @@ function testFunc() {
           .catch(done);
       });
 
-      it.skip('should upload a file by string content', (done) => {
-        debugger
+      it('should upload a Blob', (done) => {
         const stringContent = utilities.randomString();
-        Kinvey.Files.upload(stringContent, metadata)
+        const fileAsBlob = new Blob([stringContent]);
+        Kinvey.Files.upload(fileAsBlob, metadata)
           .then((result) => {
-            utilities.assertFileUploadResult(result, metadata._id, metadata.mimeType, metadata.filename, stringContent)
+            utilities.assertFileUploadResult(result, metadata._id, metadata.mimeType, metadata.filename, fileAsBlob)
+            return Kinvey.Files.find(query);
+          })
+          .then((result) => {
+            const fileMetadata = result[0];
+            utilities.assertReadFileResult(fileMetadata, metadata._id, metadata.mimeType, metadata.filename);
+            return Kinvey.Files.downloadByUrl(fileMetadata._downloadURL);
+          })
+          .then((result) => {
+            expect(result).to.exist;
+            expect(result).to.equal(stringContent);
+            done();
+          })
+          .catch(done);
+      });
+
+      it('should upload a File', (done) => {
+        const stringContent = utilities.randomString();
+        const fileAsBlob = new Blob([stringContent]);
+        const file = new File([fileAsBlob], metadata.filename, { type: 'text/plain', lastModified: Date.now() })
+        Kinvey.Files.upload(file, metadata)
+          .then((result) => {
+            utilities.assertFileUploadResult(result, metadata._id, metadata.mimeType, metadata.filename, file)
+            return Kinvey.Files.find(query);
+          })
+          .then((result) => {
+            const fileMetadata = result[0];
+            utilities.assertReadFileResult(fileMetadata, metadata._id, metadata.mimeType, metadata.filename);
+            return Kinvey.Files.downloadByUrl(fileMetadata._downloadURL);
+          })
+          .then((result) => {
+            expect(result).to.exist;
+            expect(result).to.equal(stringContent);
+            done();
+          })
+          .catch(done);
+      });
+
+      it.skip('should upload an ArrayBuffer', (done) => {
+        const stringContent = utilities.randomString();
+        const arrayBuffer = ArrayBufferFromString(stringContent);
+        Kinvey.Files.upload(arrayBuffer, metadata)
+          .then((result) => {
+            utilities.assertFileUploadResult(result, metadata._id, metadata.mimeType, metadata.filename, arrayBuffer)
+            return Kinvey.Files.find(query);
+          })
+          .then((result) => {
+            const fileMetadata = result[0];
+            utilities.assertReadFileResult(fileMetadata, metadata._id, metadata.mimeType, metadata.filename);
+            return Kinvey.Files.downloadByUrl(fileMetadata._downloadURL);
+          })
+          .then((result) => {
+            expect(result).to.exist;
+            expect(result).to.equal(stringContent);
             done();
           })
           .catch(done);
