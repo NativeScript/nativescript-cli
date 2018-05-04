b7ae6dd7f9c8791feeec9fdd37eaa56948327e74
rosen-vladimirov <rosen.vladimirov@progress.com>
2018-05-04 11:58:42 +0300

    fix: Build plugins when path to project has space
    In case the path to project has space in the name and there's Android plugin that should be build, the build operation fails. The reason is usage of exec - change it to spawn, which handles the spaces correctly.
    
    feat: Add `buildAndroidPlugin` hook, which is executed just before spawning the gradle build for specific plugin. The hook can be used to modify plugin's structure before building the plugin.

--- lib/definitions/android-plugin-migrator.d.ts
+++ lib/definitions/android-plugin-migrator.d.ts
@@ -13,3 +13,25 @@ interface IAndroidPluginBuildService {
 	buildAar(options: IBuildOptions): Promise<boolean>;
 	migrateIncludeGradle(options: IBuildOptions): boolean;
 }
+
+/**
+ * Describes data required for building plugin for Android.
+ * The data can be consumed in the buildAndroidPlugin hook.
+ */
+interface IBuildAndroidPluginData {
+	/**
+	 * Directory where the plugin will be build.
+	 * Usually this is the `<project dir>/platforms/tempPlugin/<plugin name>` dir.
+	 */
+	pluginDir: string;
+
+	/**
+	 * The name of the plugin.
+	 */
+	pluginName: string;
+
+	/**
+	 * Information about tools that will be used to build the plugin, for example compile SDK version, build tools version, etc.
+	 */
+	androidToolsInfo: IAndroidToolsInfoData;
+}
