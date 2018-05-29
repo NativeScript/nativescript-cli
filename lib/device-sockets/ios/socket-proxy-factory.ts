c45bea7e1a7346496ee85bc70dfa10ff199a4195
fatme <hfatme@gmail.com>
2018-05-29 14:32:33 +0300

    fix: fix deprecated message that is shown when debug command is executed with node10

--- lib/device-sockets/ios/socket-proxy-factory.ts
+++ lib/device-sockets/ios/socket-proxy-factory.ts
@@ -124,7 +124,7 @@ export class SocketProxyFactory extends EventEmitter implements ISocketProxyFact
 
 			webSocket.on("message", (message: string) => {
 				const length = Buffer.byteLength(message, encoding);
-				const payload = new Buffer(length + 4);
+				const payload = Buffer.allocUnsafe(length + 4);
 				payload.writeInt32BE(length, 0);
 				payload.write(message, 4, length, encoding);
 				deviceSocket.write(payload);
