67744c6a1049968b0c163973813b5809224cf9f4
Thomas Conner <thomas.conner@me.com>
2018-06-07 11:49:33 -0400

    Update configuration files

---
+++ packages/kinvey-js-sdk/lib/logger.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.log = log;
+function log(message) {
+  console.log(message);
+};
--- packages/kinvey-js-sdk/src/bundle.js
+++
@@ -1,4 +0,0 @@
-export * from '../../../src/core';
-export { MobileIdentityConnect } from '../../../src/core/identity';
-export { NetworkRack, Middleware } from '../../../src/core/request';
-export { isDefined } from '../../../src/core/utils';
