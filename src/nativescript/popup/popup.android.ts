77ecf2b91749caa184e1aca0abe4784b42cc83f7
Thomas Conner <thomas.conner@me.com>
2018-06-12 09:54:40 -0400

    MLIBZ-2366: Add fallback for Android devices that don't have Chrome installed

--- src/nativescript/popup/popup.android.ts
+++ src/nativescript/popup/popup.android.ts
@@ -4,6 +4,11 @@ import * as frameModule from 'tns-core-modules/ui/frame';
 import * as app from 'tns-core-modules/application';
 import { handleOpenURL, AppURL } from 'nativescript-urlhandler';
 
+import { Page } from 'tns-core-modules/ui/page';
+import { GridLayout } from 'tns-core-modules/ui/layouts/grid-layout';
+import { StackLayout } from 'tns-core-modules/ui/layouts/stack-layout';
+import { WebView, LoadEventData } from 'tns-core-modules/ui/web-view';
+
 declare const android: any;
 const customtabs = android.support.customtabs;
 const CustomTabsCallback = customtabs.CustomTabsCallback;
@@ -24,6 +29,81 @@ enum NavigationEvent {
   Aborted,
   TabShown,
   TabHidden
+}
+
+class OAuthPageProvider {
+  private authUrl: string;
+  private webviewIntercept: (webview, error?, url?) => boolean;
+
+  constructor(authUrl, webviewIntercept) {
+    this.authUrl = authUrl;
+    this.webviewIntercept = webviewIntercept;
+  }
+
+  createWebViewPage(): Page {
+    const webview = new WebView();
+
+    webview.on(WebView.loadFinishedEvent, (args: LoadEventData) => {
+      this.webviewIntercept(webview, args.error, args.url);
+    });
+
+    webview.on(WebView.loadStartedEvent, (args: LoadEventData) => {
+      this.webviewIntercept(webview, args.error, args.url);
+    });
+
+    const grid = new GridLayout();
+    grid.addChild(webview);
+
+    const stack = new StackLayout();
+    stack.addChild(grid);
+
+    const page = new Page();
+    page.content = stack;
+
+    webview.src = this.authUrl;
+
+    return page;
+  }
+}
+
+class LegacyPopup extends EventEmitter {
+  private _open = false;
+
+  open(url = '/') {
+    if (this._open === false) {
+      const webViewIntercept = (webview, error, url): boolean => {
+        let urlStr = '';
+
+        try {
+          if (error && error.userInfo && error.userInfo.allValues && error.userInfo.allValues.count > 0) {
+            const val0 = error.userInfo.allValues[0];
+            if (val0.absoluteString) {
+              urlStr = val0.absoluteString;
+            } else if (val0.userInfo && val0.userInfo.allValues && val0.userInfo.allValues.count > 0) {
+              urlStr = val0.userInfo.allValues[0];
+            } else {
+              urlStr = val0;
+            }
+          } else if (webview.request && webview.request.URL && webview.request.URL.absoluteString) {
+            urlStr = webview.request.URL.absoluteString;
+          } else if (url) {
+            urlStr = url;
+          }
+        } catch (ex) {
+          // Just catch the exception
+        }
+
+        (this as any).emit('loadstop', { url: urlStr });
+        return true;
+      };
+
+      const authPage = new OAuthPageProvider(url, webViewIntercept);
+      frameModule.topmost().navigate(() => authPage.createWebViewPage());
+      this._open = true;
+    }
+
+    return this;
+  }
 }
 
 export class Popup extends EventEmitter {
@@ -92,7 +172,14 @@ export class Popup extends EventEmitter {
     });
 
     // Bind to the custom tabs service
-    CustomTabsClient.bindCustomTabsService(activity, 'com.android.chrome', new serviceConnection());
+    const success = CustomTabsClient.bindCustomTabsService(activity, 'com.android.chrome', new serviceConnection());
+
+    if (!success) {
+      const legacyPopup = new LegacyPopup();
+      legacyPopup.on('loadstop', (event) => this.emit('loadstop', event));
+      legacyPopup.open(url);
+      this._open = true;
+    }
 
     // Return this
     return this;
