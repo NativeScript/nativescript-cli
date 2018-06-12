71e7dc0f90429d6a68d4e5a9c4e8f65b62789158
Thomas Conner <thomas.conner@me.com>
2018-06-12 10:20:19 -0400

    MLIBZ-2366: Add try/catch to prevent error when Chrome is not installed

--- src/nativescript/popup/popup.android.ts
+++ src/nativescript/popup/popup.android.ts
@@ -10,7 +10,7 @@ import { StackLayout } from 'tns-core-modules/ui/layouts/stack-layout';
 import { WebView, LoadEventData } from 'tns-core-modules/ui/web-view';
 
 declare const android: any;
-const customtabs = android.support.customtabs;
+const customtabs = android.support.customtabs || {};
 const CustomTabsCallback = customtabs.CustomTabsCallback;
 const CustomTabsServiceConnection = customtabs.CustomTabsServiceConnection;
 const CustomTabsIntent = customtabs.CustomTabsIntent;
@@ -112,67 +112,70 @@ export class Popup extends EventEmitter {
   open(url = '/', options: PopupOptions = {}) {
     const activity = app.android.startActivity || app.android.foregroundActivity;
     let shouldClose = false;
+    let success = false;
 
-    const callback = CustomTabsCallback.extend({
-      onNavigationEvent: (navigationEvent) => {
-        switch (navigationEvent) {
-          case NavigationEvent.Finished:
-          case NavigationEvent.Failed:
-          case NavigationEvent.Aborted:
-            if (shouldClose) {
-              setTimeout(() => {
-                this.emit('exit');
-              }, 0);
-            }
-            break;
-          case NavigationEvent.TabHidden:
-            shouldClose = true;
-            this._open = false;
-            break;
+    try {
+      const callback = CustomTabsCallback.extend({
+        onNavigationEvent: (navigationEvent) => {
+          switch (navigationEvent) {
+            case NavigationEvent.Finished:
+            case NavigationEvent.Failed:
+            case NavigationEvent.Aborted:
+              if (shouldClose) {
+                setTimeout(() => {
+                  this.emit('exit');
+                }, 0);
+              }
+              break;
+            case NavigationEvent.TabHidden:
+              shouldClose = true;
+              this._open = false;
+              break;
+          }
         }
-      }
-    });
+      });
 
-    handleOpenURL((appURL: AppURL) => {
-      this.emit('loadstop', { url: appURL.toString() });
-    });
+      handleOpenURL((appURL: AppURL) => {
+        this.emit('loadstop', { url: appURL.toString() });
+      });
 
-    const serviceConnection = CustomTabsServiceConnection.extend({
-      onCustomTabsServiceConnected: (name, client) => {
-        // Create a new session
-        const session = client.newSession(new callback());
+      const serviceConnection = CustomTabsServiceConnection.extend({
+        onCustomTabsServiceConnected: (name, client) => {
+          // Create a new session
+          const session = client.newSession(new callback());
 
-        // Create a new intent builder
-        const intentBuilder = new CustomTabsIntent.Builder(session);
+          // Create a new intent builder
+          const intentBuilder = new CustomTabsIntent.Builder(session);
 
-        // Toolbar color
-        if (options.toolbarColor) {
-          intentBuilder.setToolbarColor(new Color(options.toolbarColor).android);
+          // Toolbar color
+          if (options.toolbarColor) {
+            intentBuilder.setToolbarColor(new Color(options.toolbarColor).android);
+          }
+
+          // Show title
+          if (options.showTitle) {
+            intentBuilder.setShowTitle(options.showTitle);
+          }
+
+          intentBuilder.addDefaultShareMenuItem(); // Adds a default share item to the menu.
+          intentBuilder.enableUrlBarHiding(); // Enables the url bar to hide as the user scrolls down on the page.
+
+          // Launch the url
+          let intent = intentBuilder.build();
+          intent.launchUrl(activity, Uri.parse(url));
+
+          // Set open to true
+          this._open = true;
+        },
+
+        onServiceDisconnected(name) {
+          // TODO: Do nothing for now. Should this change?
         }
+      });
 
-        // Show title
-        if (options.showTitle) {
-          intentBuilder.setShowTitle(options.showTitle);
-        }
-
-        intentBuilder.addDefaultShareMenuItem(); // Adds a default share item to the menu.
-        intentBuilder.enableUrlBarHiding(); // Enables the url bar to hide as the user scrolls down on the page.
-
-        // Launch the url
-        let intent = intentBuilder.build();
-        intent.launchUrl(activity, Uri.parse(url));
-
-        // Set open to true
-        this._open = true;
-      },
-
-      onServiceDisconnected(name) {
-        // TODO: Do nothing for now. Should this change?
-      }
-    });
-
-    // Bind to the custom tabs service
-    const success = CustomTabsClient.bindCustomTabsService(activity, 'com.android.chrome', new serviceConnection());
+      // Bind to the custom tabs service
+      success = CustomTabsClient.bindCustomTabsService(activity, 'com.android.chrome', new serviceConnection());
+    } catch (error) {}
 
     if (!success) {
       const legacyPopup = new LegacyPopup();
