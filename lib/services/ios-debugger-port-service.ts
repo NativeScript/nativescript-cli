4f4bbd7315f0ada5326d4b6b87c953e9a914bc9c
rosen-vladimirov <rosen.vladimirov@progress.com>
2018-06-15 13:58:24 +0300

    fix: Unable to run app on iOS in Sidekick
    When trying to run application on iOS in Sidekick for the first time (when the iOS platform is not added yet to the project), the run fails with error "Cannot read property version of undefined".
    The problem is in the call to `platformLiveSyncService.prepareForLiveSync` which tries to start parsing device logs before starting the liveSync action. However, at this point, the application does not have the platform added yet and the code that tries to get the version of `tns-ios` from project's package.json fails. In order to resolve this, ensure the code for getting framework version from package.json will return undefined instead of failing.
    When undefined is returned, we know CLI will add the latest compatible iOS runtime version. We know that this version will print the port for debugging in the logs, so we can safely start parsing the logs.

--- lib/services/ios-debugger-port-service.ts
+++ lib/services/ios-debugger-port-service.ts
@@ -51,7 +51,7 @@ export class IOSDebuggerPortService implements IIOSDebuggerPortService {
 	private canStartLookingForDebuggerPort(data: IProjectDir): boolean {
 		const projectData = this.$projectDataService.getProjectData(data && data.projectDir);
 		const frameworkVersion = this.$iOSProjectService.getFrameworkVersion(projectData);
-		return semver.gt(frameworkVersion, IOSDebuggerPortService.MIN_REQUIRED_FRAMEWORK_VERSION);
+		return !frameworkVersion || semver.gt(frameworkVersion, IOSDebuggerPortService.MIN_REQUIRED_FRAMEWORK_VERSION);
 	}
 
 	@cache()
