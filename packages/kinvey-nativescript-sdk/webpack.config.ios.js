74a3cdc894535db954961770578eae49926d77e0
Thomas Conner <thomas.conner@me.com>
2018-06-26 10:06:22 -0400

    MLIBZ-2570: Update webpack configs

--- packages/kinvey-nativescript-sdk/webpack.config.ios.js
+++ packages/kinvey-nativescript-sdk/webpack.config.ios.js
@@ -117,15 +117,22 @@ function getPlugins(env, platform) {
         from: 'package.json',
         transform: (content) => {
           const pkg = JSON.parse(content.toString('utf8'));
+          const postinstallScript = pkg.scripts.postinstall;
+          const preuninstallScript = pkg.scripts.preuninstall;
           delete pkg.private;
           delete pkg.devDependencies;
           delete pkg.scripts;
+          pkg.scripts = {
+            postinstall: postinstallScript,
+            preuninstall: preuninstallScript
+          };
           return new Buffer(JSON.stringify(pkg, null, 2));
         }
       },
       { from: '.travis.yml' },
       { from: path.join(__dirname, '../../src/kinvey.d.ts') },
       { from: 'platforms/ios/**/*' },
+      { from: 'lib/**/*' },
       { from: 'LICENSE' },
       { from: 'README.md' },
     ]),
