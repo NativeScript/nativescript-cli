dd4610efff326bb9f69e1fb73e7989e351ba400d
Thomas Conner <thomas.conner@me.com>
2018-06-26 15:27:46 -0400

    Replace the redirectUrlScheme properly

--- packages/kinvey-nativescript-sdk/lib/before-prepare.js
+++ packages/kinvey-nativescript-sdk/lib/before-prepare.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const url = require('url');
 const pkg = require('../package.json');
 
 module.exports = function (hookArgs) {
@@ -11,17 +12,19 @@ module.exports = function (hookArgs) {
 
   const packageJsonContent = JSON.parse(fs.readFileSync(path.join(appDirectoryPath, 'package.json')));
   const kinveyData = packageJsonContent.pluginsData && packageJsonContent.pluginsData[pkg.name];
-  const redirectUri = (kinveyData && kinveyData.config && kinveyData.config.redirectUri) || 'enterpriseauth';
+  const redirectUri = (kinveyData && kinveyData.config && kinveyData.config.redirectUri) || 'enterpriseauth://';
+  const parsedRedirectUri = url.parse(redirectUri);
+  const redirectUriScheme = parsedRedirectUri.protocol && parsedRedirectUri.protocol.substring(0, parsedRedirectUri.protocol.indexOf(':'));
 
   if (hookArgs.platform.toLowerCase() === 'android') {
     const androidManifestPath = path.join(__dirname, '..', 'platforms', 'android', 'AndroidManifest.xml');
     let content = fs.readFileSync(androidManifestPath).toString();
-    content = content.replace(/{redirectUri}/i, redirectUri);
+    content = content.replace(/{redirectUriScheme}/i, redirectUriScheme);
     fs.writeFileSync(androidManifestPath, content);
   } else if (hookArgs.platform.toLowerCase() === 'ios') {
     const infoPlistPath = path.join(__dirname, '..', 'platforms', 'ios', 'Info.plist');
     let content = fs.readFileSync(infoPlistPath).toString();
-    content = content.replace(/{redirectUri}/i, redirectUri);
+    content = content.replace(/{redirectUriScheme}/i, redirectUriScheme);
     fs.writeFileSync(infoPlistPath, content);
   }
 };
