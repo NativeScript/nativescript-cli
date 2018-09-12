aaee1765e8e4c53218217fcaab9fcef84d86a886
fatme <hfatme@gmail.com>
2018-09-12 10:28:37 +0300

    Fix canExecuteFastSync for ios devices

--- lib/services/livesync/ios-device-livesync-service.ts
+++ lib/services/livesync/ios-device-livesync-service.ts
@@ -61,7 +61,7 @@ export class IOSDeviceLiveSyncService extends DeviceLiveSyncServiceBase implemen
 		constants.LIVESYNC_EXCLUDED_FILE_PATTERNS.forEach(pattern => scriptRelatedFiles = _.concat(scriptRelatedFiles, localToDevicePaths.filter(file => minimatch(file.getDevicePath(), pattern, { nocase: true }))));
 
 		const otherFiles = _.difference(localToDevicePaths, _.concat(scriptFiles, scriptRelatedFiles));
-		const canExecuteFastSync = this.canExecuteFastSyncForPaths(liveSyncInfo, otherFiles, projectData, deviceAppData.platform);
+		const canExecuteFastSync = this.canExecuteFastSyncForPaths(liveSyncInfo, localToDevicePaths, projectData, deviceAppData.platform);
 
 		if (!canExecuteFastSync) {
 			await this.restartApplication(deviceAppData, projectData.projectName);
