11c2263709fdd88b3c7834fc098305bdb90b4a00
fatme <hfatme@gmail.com>
2018-09-17 12:51:38 +0300

    chore: persist warnings per preview app's version

--- lib/services/livesync/playground/preview-app-plugins-service.ts
+++ lib/services/livesync/playground/preview-app-plugins-service.ts
@@ -5,35 +5,27 @@ import { Device } from "nativescript-preview-sdk";
 import { PluginComparisonMessages } from "./preview-app-constants";
 
 export class PreviewAppPluginsService implements IPreviewAppPluginsService {
+	private previewAppVersionWarnings: IDictionary<string[]> = {};
+
 	constructor(private $fs: IFileSystem,
 		private $logger: ILogger,
 		private $projectData: IProjectData) { }
 
 	public async comparePluginsOnDevice(device: Device): Promise<void> {
-		const devicePlugins = this.getDevicePlugins(device);
-		const localPlugins = this.getLocalPlugins();
+		if (!this.previewAppVersionWarnings[device.previewAppVersion]) {
+			const devicePlugins = this.getDevicePlugins(device);
+			const localPlugins = this.getLocalPlugins();
+			const warnings = _.keys(localPlugins)
+				.map(localPlugin => {
+					const localPluginVersion = localPlugins[localPlugin];
+					const devicePluginVersion = devicePlugins[localPlugin];
+					return this.getWarningForPlugin(localPlugin, localPluginVersion, devicePluginVersion, device.id);
+				})
+				.filter(item => !!item);
+			this.previewAppVersionWarnings[device.previewAppVersion] = warnings;
+		}
 
-		_.keys(localPlugins).forEach(localPlugin => {
-			const localPluginVersion = localPlugins[localPlugin];
-			const devicePluginVersion = devicePlugins[localPlugin];
-
-			this.$logger.trace(`Comparing plugin ${localPlugin} with localPluginVersion ${localPluginVersion} and devicePluginVersion ${devicePluginVersion}`);
-
-			if (devicePluginVersion) {
-				const localPluginVersionData = semver.coerce(localPluginVersion);
-				const devicePluginVersionData = semver.coerce(devicePluginVersion);
-
-				if (localPluginVersionData.major !== devicePluginVersionData.major) {
-					this.$logger.warn(util.format(PluginComparisonMessages.LOCAL_PLUGIN_WITH_DIFFERENCE_IN_MAJOR_VERSION, localPlugin, localPluginVersion, devicePluginVersion));
-				}
-
-				if (localPluginVersionData.major === devicePluginVersionData.major && localPluginVersionData.minor > devicePluginVersionData.minor) {
-					this.$logger.warn(util.format(PluginComparisonMessages.LOCAL_PLUGIN_WITH_GREATHER_MINOR_VERSION, localPlugin, localPluginVersion, devicePluginVersion));
-				}
-			} else {
-				this.$logger.warn(util.format(PluginComparisonMessages.PLUGIN_NOT_INCLUDED_IN_PREVIEW_APP, localPlugin, device.id));
-			}
-		});
+		this.previewAppVersionWarnings[device.previewAppVersion].map(warning => this.$logger.warn(warning));
 	}
 
 	private getDevicePlugins(device: Device): IStringDictionary {
@@ -53,6 +45,25 @@ export class PreviewAppPluginsService implements IPreviewAppPluginsService {
 			this.$logger.trace(`Error while parsing ${projectFilePath}. Error is ${err.message}`);
 			return {};
 		}
+	}
+
+	private getWarningForPlugin(localPlugin: string, localPluginVersion: string, devicePluginVersion: string, deviceId: string): string {
+		this.$logger.trace(`Comparing plugin ${localPlugin} with localPluginVersion ${localPluginVersion} and devicePluginVersion ${devicePluginVersion}`);
+
+		if (devicePluginVersion) {
+			const localPluginVersionData = semver.coerce(localPluginVersion);
+			const devicePluginVersionData = semver.coerce(devicePluginVersion);
+
+			if (localPluginVersionData.major !== devicePluginVersionData.major) {
+				return util.format(PluginComparisonMessages.LOCAL_PLUGIN_WITH_DIFFERENCE_IN_MAJOR_VERSION, localPlugin, localPluginVersion, devicePluginVersion);
+			} else if (localPluginVersionData.minor > devicePluginVersionData.minor) {
+				return util.format(PluginComparisonMessages.LOCAL_PLUGIN_WITH_GREATHER_MINOR_VERSION, localPlugin, localPluginVersion, devicePluginVersion);
+			}
+
+			return null;
+		}
+
+		return util.format(PluginComparisonMessages.PLUGIN_NOT_INCLUDED_IN_PREVIEW_APP, localPlugin, deviceId);
 	}
 }
 $injector.register("previewAppPluginsService", PreviewAppPluginsService);
