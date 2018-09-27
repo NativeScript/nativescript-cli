a6673ebe2975fb7f1697b47fc47eaa7f5b090f24
fatme <hfatme@gmail.com>
2018-09-27 15:15:54 +0300

    refactor: Make pluginsService.isNativeScriptPlugin to work with full path

--- lib/services/plugins-service.ts
+++ lib/services/plugins-service.ts
@@ -2,7 +2,6 @@ import * as path from "path";
 import * as shelljs from "shelljs";
 import * as semver from "semver";
 import * as constants from "../constants";
-import { NODE_MODULES_DIR_NAME } from "../common/constants";
 
 export class PluginsService implements IPluginsService {
 	private static INSTALL_COMMAND_NAME = "install";
@@ -208,8 +207,7 @@ export class PluginsService implements IPluginsService {
 		};
 	}
 
-	public isNativeScriptPlugin(pluginName: string, projectData: IProjectData): boolean {
-		const pluginPackageJsonPath = path.join(projectData.projectDir, NODE_MODULES_DIR_NAME, pluginName, "package.json");
+	public isNativeScriptPlugin(pluginPackageJsonPath: string): boolean {
 		const pluginPackageJsonContent = this.$fs.readJson(pluginPackageJsonPath);
 		return pluginPackageJsonContent && pluginPackageJsonContent.nativescript;
 	}
