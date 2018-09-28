fc399500572f57b1dd763e320ce0babd71c34540
rosen-vladimirov <rosen.vladimirov@progress.com>
2018-09-28 17:40:19 +0300

    fix: tns preview warns incorrectly for exceeding file size limit
    The check in `tns preview` relies on the zipped content instead of checking the actual file content. This way, based on what is the zipped content, we may end up with 30000 bytes length for 15 MB input files or for 100KBs input.
    The check is not correct, it should rely on the input file size, as when extracted on device, their size is the actual limitation.

--- lib/services/livesync/playground/preview-sdk-service.ts
+++ lib/services/livesync/playground/preview-sdk-service.ts
@@ -3,7 +3,7 @@ import { PubnubKeys } from "./preview-app-constants";
 const pako = require("pako");
 
 export class PreviewSdkService implements IPreviewSdkService {
-	private static MAX_FILES_UPLOAD_BYTE_LENGTH = 30000;
+	private static MAX_FILES_UPLOAD_BYTE_LENGTH = 15 * 1024 * 1024; // In MBs
 	private messagingService: MessagingService = null;
 	private instanceId: string = null;
 	public connectedDevices: Device[] = [];
@@ -76,7 +76,8 @@ export class PreviewSdkService implements IPreviewSdkService {
 			onSendingChange: (sending: boolean) => ({ }),
 			onBiggerFilesUpload: async (filesContent, callback) => {
 				const gzippedContent = Buffer.from(pako.gzip(filesContent));
-				const byteLength = gzippedContent.byteLength;
+				const byteLength = filesContent.length;
+
 				if (byteLength > PreviewSdkService.MAX_FILES_UPLOAD_BYTE_LENGTH) {
 					this.$logger.warn("The files to upload exceed the maximum allowed size of 15MB. Your app might not work as expected.");
 				}
