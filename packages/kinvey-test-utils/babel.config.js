5a59b1ff5028d164abd84bbc919873eeb88e399d
Thomas Conner <thomas.conner@me.com>
2018-10-08 09:33:42 -0400

    Rename tests to integration-tests. Create kinvey-log

--- packages/kinvey-test-utils/babel.config.js
+++ packages/kinvey-test-utils/babel.config.js
@@ -3,6 +3,12 @@ module.exports = (api) => {
 
   const presets = [
     ['@babel/env', {
+      targets: {
+        edge: '17',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1'
+      },
       useBuiltIns: 'usage'
     }]
   ];
