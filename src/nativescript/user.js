df330af68745c36cdc717155cc3dddedfb4e832d
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2018-10-15 14:24:20 +0300

    Implement preview app check

--- src/nativescript/user.js
+++ src/nativescript/user.js
@@ -1,8 +1,10 @@
 import { User as CoreUser } from '../core/user';
 import { getDataFromPackageJson } from './utils';
+import * as app from "application";
 
 export class User extends CoreUser {
   loginWithMIC(redirectUri, authorizationGrant, options) {
+    redirectUri = isInsidePreviewApp() ? "nsplay://" : redirectUri;
     const config = getDataFromPackageJson();
     return super.loginWithMIC(redirectUri || config.redirectUri, authorizationGrant, options);
   }
@@ -10,5 +12,17 @@ export class User extends CoreUser {
   static loginWithMIC(redirectUri, authorizationGrant, options) {
     const user = new User();
     return user.loginWithMIC(redirectUri, authorizationGrant, options);
+  }
+}
+
+function isInsidePreviewApp() {
+  return getAppIdentifier() === "org.nativescript.preview";
+}
+
+function getAppIdentifier() {
+  if (app.android) {
+    return app.android.packageName;
+  } else if (app.ios) {
+    return NSBundle.mainBundle.bundleIdentifier;
   }
 }
