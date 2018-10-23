8550e1f02dca1dcbdb0c9a92af74eaf58c8050d4
Martin Hristov <mhristov@mcsofmhristov.bedford.progress.com>
2018-10-23 15:53:55 +0300

    reworked aggregation, app, files, mic, kmd, query tests

--- packages/kinvey-kmd/src/kmd.spec.js
+++ packages/kinvey-kmd/src/kmd.spec.js
@@ -1,124 +1,124 @@
 import expect from 'expect';
-import { Metadata } from './kmd';
+import { Kmd } from './kmd';
 import { randomString } from 'kinvey-test-utils';
 import { KinveyError } from '../../errors';
 
-describe('Metadata', () => {
+describe('kmd', () => {
   describe('constructor', () => {
     it('should throw an error if an entity is not provided', () => {
       expect(() => {
-        const metadata = new Metadata();
-        return metadata;
+        const kmd = new Kmd();
+        return kmd;
       }).toThrow(KinveyError, /entity argument must be an object/);
     });
 
-    it('should create an empty metadata when the entity does not contain an _kmd property', () => {
+    it('should create an empty kmd when the entity does not contain an _kmd property', () => {
       const entity = {};
-      const metadata = new Metadata(entity);
-      expect(metadata.toPlainObject()).toEqual({});
+      const kmd = new Kmd(entity);
+      expect(kmd.toPlainObject()).toEqual({});
       expect(entity._kmd).toEqual({});
     });
 
     it('should use the _kmd property on the entity', () => {
       const kmdProp = { lmt: randomString() };
       const entity = { _kmd: kmdProp };
-      const metadata = new Metadata(entity);
-      expect(metadata.toPlainObject()).toEqual(kmdProp);
+      const kmd = new Kmd(entity);
+      expect(kmd.toPlainObject()).toEqual(kmdProp);
       expect(entity._kmd).toEqual(kmdProp);
     });
   });
 
   describe('createdAt', () => {
     it('should return undefined for entity create time', () => {
-      const metadata = new Metadata({ _kmd: {} });
-      expect(metadata.createdAt).toEqual(undefined);
+      const kmd = new Kmd({ _kmd: {} });
+      expect(kmd.createdAt).toEqual(undefined);
     });
 
     it('should return a Date for entity create time', () => {
       const ect = new Date().toISOString();
-      const metadata = new Metadata({ _kmd: { ect: ect } });
-      expect(metadata.createdAt).toBeA(Date);
-      expect(metadata.createdAt).toEqual(new Date(ect));
+      const kmd = new Kmd({ _kmd: { ect: ect } });
+      expect(kmd.createdAt).toBeA(Date);
+      expect(kmd.createdAt).toEqual(new Date(ect));
     });
   });
 
   describe('ect', () => {
     it('should return undefined for entity create time', () => {
-      const metadata = new Metadata({ _kmd: {} });
-      expect(metadata.ect).toEqual(undefined);
+      const kmd = new Kmd({ _kmd: {} });
+      expect(kmd.ect).toEqual(undefined);
     });
 
     it('should return a Date for entity create time', () => {
       const ect = new Date().toISOString();
-      const metadata = new Metadata({ _kmd: { ect: ect } });
-      expect(metadata.ect).toBeA(Date);
-      expect(metadata.ect).toEqual(new Date(ect));
+      const kmd = new Kmd({ _kmd: { ect: ect } });
+      expect(kmd.ect).toBeA(Date);
+      expect(kmd.ect).toEqual(new Date(ect));
     });
   });
 
   describe('emailVerification', () => {
     it('should return undefined', () => {
-      const metadata = new Metadata({ _kmd: {} });
-      expect(metadata.emailVerification).toEqual(undefined);
+      const kmd = new Kmd({ _kmd: {} });
+      expect(kmd.emailVerification).toEqual(undefined);
     });
 
     it('should return emailVerification.status value', () => {
       const emailVerificationStatus = 'Verified';
-      const metadata = new Metadata({ _kmd: { emailVerification: { status: emailVerificationStatus } } });
-      expect(metadata.emailVerification).toEqual(emailVerificationStatus);
+      const kmd = new Kmd({ _kmd: { emailVerification: { status: emailVerificationStatus } } });
+      expect(kmd.emailVerification).toEqual(emailVerificationStatus);
     });
   });
 
   describe('lastModified', () => {
     it('should return undefined for entity create time', () => {
-      const metadata = new Metadata({ _kmd: {} });
-      expect(metadata.lastModified).toEqual(undefined);
+      const kmd = new Kmd({ _kmd: {} });
+      expect(kmd.lastModified).toEqual(undefined);
     });
 
     it('should return a Date for entity create time', () => {
       const lmt = new Date().toISOString();
-      const metadata = new Metadata({ _kmd: { lmt: lmt } });
-      expect(metadata.lastModified).toBeA(Date);
-      expect(metadata.lastModified).toEqual(new Date(lmt));
+      const kmd = new Kmd({ _kmd: { lmt: lmt } });
+      expect(kmd.lastModified).toBeA(Date);
+      expect(kmd.lastModified).toEqual(new Date(lmt));
     });
   });
 
   describe('lmt', () => {
     it('should return undefined for entity create time', () => {
-      const metadata = new Metadata({ _kmd: {} });
-      expect(metadata.lmt).toEqual(undefined);
+      const kmd = new Kmd({ _kmd: {} });
+      expect(kmd.lmt).toEqual(undefined);
     });
 
     it('should return a Date for entity create time', () => {
       const lmt = new Date().toISOString();
-      const metadata = new Metadata({ _kmd: { lmt: lmt } });
-      expect(metadata.lmt).toBeA(Date);
-      expect(metadata.lmt).toEqual(new Date(lmt));
+      const kmd = new Kmd({ _kmd: { lmt: lmt } });
+      expect(kmd.lmt).toBeA(Date);
+      expect(kmd.lmt).toEqual(new Date(lmt));
     });
   });
 
   describe('authtoken', () => {
     it('should return undefined', () => {
-      const metadata = new Metadata({ _kmd: {} });
-      expect(metadata.authtoken).toEqual(undefined);
+      const kmd = new Kmd({ _kmd: {} });
+      expect(kmd.authtoken).toEqual(undefined);
     });
 
     it('should return authtoken value', () => {
       const authtoken = randomString();
-      const metadata = new Metadata({ _kmd: { authtoken: authtoken } });
-      expect(metadata.authtoken).toEqual(authtoken);
+      const kmd = new Kmd({ _kmd: { authtoken: authtoken } });
+      expect(kmd.authtoken).toEqual(authtoken);
     });
   });
 
   describe('isLocal()', () => {
     it('should return false', () => {
-      const metadata = new Metadata({ _kmd: { local: false } });
-      expect(metadata.isLocal()).toEqual(false);
+      const kmd = new Kmd({ _kmd: { local: false } });
+      expect(kmd.isLocal()).toEqual(false);
     });
 
     it('should return true', () => {
-      const metadata = new Metadata({ _kmd: { local: true } });
-      expect(metadata.isLocal()).toEqual(true);
+      const kmd = new Kmd({ _kmd: { local: true } });
+      expect(kmd.isLocal()).toEqual(true);
     });
   });
 
@@ -131,8 +131,8 @@ describe('Metadata', () => {
           status: 'Verified'
         }
       };
-      const metadata = new Metadata({ _kmd: kmd });
-      expect(metadata.toPlainObject()).toEqual(kmd);
+      const kmd_metadata = new Kmd({ _kmd: kmd });
+      expect(kmd_metadata.toPlainObject()).toEqual(kmd);
     });
   });
 });
