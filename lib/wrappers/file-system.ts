3142fd5579c0d19ff6f6c1f9a0fa8a177af3b990
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2018-10-30 16:00:40 -0300

    Implemented needed code to extract zip archives into a directory

--- lib/wrappers/file-system.ts
+++ lib/wrappers/file-system.ts
@@ -1,5 +1,10 @@
 import * as fs from "fs";
-import { Extract } from "unzip";
+import * as path from "path";
+import * as yauzl from "yauzl";
+import * as util from "util";
+
+const access = util.promisify(fs.access);
+const mkdir = util.promisify(fs.mkdir);
 
 export class FileSystem {
 	public exists(path: string): boolean {
@@ -7,11 +12,39 @@ export class FileSystem {
 	}
 
 	public extractZip(pathToZip: string, outputDir: string): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			const stream = fs.createReadStream(pathToZip).pipe(Extract({ path: outputDir }));
-			stream.on("close", resolve);
-			stream.on("error", reject);
-		});
+		return new Promise((resolve, reject) => {
+			yauzl.open(pathToZip, { autoClose: true, lazyEntries: true }, (e, zipFile) => {
+				if (e) return reject(e);
+
+				zipFile.on('entry', entry => {
+					const fn = <string>entry.fileName;
+					if (/\/$/.test(fn)) {
+						return zipFile.readEntry();
+					}
+
+					zipFile.openReadStream(entry, (err, stream) => {
+						if(err) return reject(err);
+
+						const filePath = `${outputDir}/${fn}`;
+
+						return createParentDirsIfNeeded(filePath)
+							.catch(e => reject(e))
+							.then(() => {
+								const outfile = createOutfile(filePath);
+								stream.once('end', () => {
+									zipFile.readEntry();
+									outfile.close();
+								});
+								stream.pipe(outfile);
+							});
+					});
+				});
+
+				zipFile.once('end', () => resolve());
+
+				zipFile.readEntry();
+			});
+		})
 	}
 
 	public readDirectory(path: string): string[] {
@@ -22,4 +55,19 @@ export class FileSystem {
 		const content = fs.readFileSync(path, options);
 		return JSON.parse(content.toString());
 	}
+}
+
+function createParentDirsIfNeeded(filePath: string) {
+	const dirs = path.dirname(filePath).split(path.sep);
+	return dirs.reduce((p, dir, index) => p.then(parent => {
+		const current = `${parent}${path.sep}${dir}`;
+
+		return access(current)
+			.catch(e => mkdir(current))
+			.then(() => current);
+	}), Promise.resolve(''));
+}
+
+function createOutfile(path: string): fs.WriteStream {
+	return fs.createWriteStream(path);
 }
