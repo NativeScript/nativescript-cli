26b4b950f9f45585e25fa4a4cafd0a64cc39a462
Thomas Conner <thomas.conner@me.com>
2019-01-24 16:20:36 -0500

    Update nativescript build script

--- scripts/build/build.nativescript.js
+++ scripts/build/build.nativescript.js
@@ -4,6 +4,6 @@ const { PACKAGES_DIR, ROOT_DIR } = require('../utils');
 
 const SRC_DIR = path.resolve(ROOT_DIR, 'src');
 const BUILD_DIR = path.resolve(PACKAGES_DIR, 'kinvey-nativescript-sdk', 'src');
-const EXTENSIONS = ['.nativescript'];
+const EXTENSIONS = ['.nativescript', '.angular'];
 
 build(SRC_DIR, BUILD_DIR, EXTENSIONS);
