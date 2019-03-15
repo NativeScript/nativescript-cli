74f12a58f189b546a52c2c08907e53aa50267412
Thomas Conner <thomas.conner@me.com>
2019-03-15 10:00:07 -0400

    Remove push files for Node and HTML5

--- src/push/index.html5.ts
+++
@@ -1,9 +0,0 @@
-import KinveyError from '../errors/kinvey';
-
-export async function register() {
-  throw new KinveyError('Push notifications are currently not supported on the platform.');
-}
-
-export async function unregister(options: any = {}) {
-  throw new KinveyError('Push notifications are currently not supported on the platform.');
-}
