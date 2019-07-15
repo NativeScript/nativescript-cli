c575d50f6f49a0cbacb6eacaf96a680456373b38
fatme <hfatme@gmail.com>
2019-07-15 14:09:44 +0300

    chore: fix additional whitespace

--- lib/services/livesync-process-data-service.ts
+++ lib/services/livesync-process-data-service.ts
@@ -6,7 +6,7 @@ export class LiveSyncProcessDataService implements ILiveSyncProcessDataService {
 		this.processes[projectDir].actionsChain = this.processes[projectDir].actionsChain || Promise.resolve();
 		this.processes[projectDir].currentSyncAction = this.processes[projectDir].actionsChain;
 		this.processes[projectDir].isStopped = false;
-		this.processes[projectDir].platforms =  platforms;
+		this.processes[projectDir].platforms = platforms;
 
 		const currentDeviceDescriptors = this.getDeviceDescriptors(projectDir);
 		this.processes[projectDir].deviceDescriptors = _.uniqBy(currentDeviceDescriptors.concat(deviceDescriptors), "identifier");
