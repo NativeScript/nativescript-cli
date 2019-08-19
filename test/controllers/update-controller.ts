f24ab5c2028d785573e6828dd7774da8f23930e3
rosen-vladimirov <rosen.vladimirov@progress.com>
2019-08-19 18:46:15 +0300

    feat: add tracking for React and Svelte projecs
    Add tracking in CLI analytics for React and Svelte project types. As we do not have templates for those two project types, when CLI detects a project from this type and the `tns update` command is executed, get the dependencies for update from the default hellow world JS template.

--- test/controllers/update-controller.ts
+++ test/controllers/update-controller.ts
@@ -2,7 +2,7 @@ import * as stubs from "./../stubs";
 import * as yok from "../../lib/common/yok";
 import { UpdateController } from "../../lib/controllers/update-controller";
 import { assert } from "chai";
-import * as sinon from 'sinon';
+import * as sinon from "sinon";
 import * as path from "path";
 import { Options } from "../../lib/options";
 import { StaticConfig } from "../../lib/config";
@@ -21,7 +21,7 @@ function createTestInjector(
 		checkConsent: async (): Promise<void> => undefined,
 		trackFeature: async (): Promise<void> => undefined
 	});
-	testInjector.register('errors', stubs.ErrorsStub);
+	testInjector.register("errors", stubs.ErrorsStub);
 	testInjector.register("staticConfig", StaticConfig);
 	testInjector.register("projectData", {
 		projectDir,
@@ -32,29 +32,29 @@ function createTestInjector(
 	testInjector.register("migrateController", {
 		shouldMigrate: () => { return false; },
 	});
-	testInjector.register('fs', stubs.FileSystemStub);
-	testInjector.register('platformCommandHelper', {
+	testInjector.register("fs", stubs.FileSystemStub);
+	testInjector.register("platformCommandHelper", {
 		getCurrentPlatformVersion: () => {
 			return "5.2.0";
 		}
 	});
 
-	testInjector.register('packageManager', {
+	testInjector.register("packageManager", {
 		getTagVersion: () => {
 			return "2.3.0";
 		}
 	});
 	testInjector.register("addPlatformService", {
-		setPlatformVersion: () => {/**/}
+		setPlatformVersion: () => {/**/ }
 	});
 	testInjector.register("pluginsService", {
-		addToPackageJson: () => {/**/}
+		addToPackageJson: () => {/**/ }
 	});
-	testInjector.register('devicePlatformsConstants', DevicePlatformsConstants);
-	testInjector.register('packageInstallationManager', stubs.PackageInstallationManagerStub);
-	testInjector.register('platformsDataService', stubs.NativeProjectDataStub);
+	testInjector.register("devicePlatformsConstants", DevicePlatformsConstants);
+	testInjector.register("packageInstallationManager", stubs.PackageInstallationManagerStub);
+	testInjector.register("platformsDataService", stubs.NativeProjectDataStub);
 	testInjector.register("pacoteService", stubs.PacoteServiceStub);
-	testInjector.register('projectDataService', stubs.ProjectDataService);
+	testInjector.register("projectDataService", stubs.ProjectDataService);
 	testInjector.register("updateController", UpdateController);
 
 	return testInjector;
@@ -78,7 +78,7 @@ describe("update controller method tests", () => {
 		sandbox.stub(fs, "copyFile").throws();
 		const updateController = testInjector.resolve("updateController");
 
-		await updateController.update({projectDir: projectFolder, version: "3.3.0"});
+		await updateController.update({ projectDir: projectFolder, version: "3.3.0" });
 
 		assert.isTrue(deleteDirectory.calledWith(path.join(projectFolder, UpdateController.backupFolder)));
 		assert.isFalse(deleteDirectory.calledWith(path.join(projectFolder, "platforms")));
@@ -90,7 +90,7 @@ describe("update controller method tests", () => {
 		const copyFileStub = sandbox.stub(fs, "copyFile");
 		const updateController = testInjector.resolve("updateController");
 
-		await updateController.update({projectDir: projectFolder, version: "3.3.0"});
+		await updateController.update({ projectDir: projectFolder, version: "3.3.0" });
 
 		assert.isTrue(copyFileStub.calledWith(path.join(projectFolder, "package.json")));
 		for (const folder of UpdateController.folders) {
@@ -108,11 +108,94 @@ describe("update controller method tests", () => {
 		const updateController = testInjector.resolve("updateController");
 		const tempDir = path.join(projectFolder, UpdateController.backupFolder);
 
-		await updateController.update({projectDir: projectFolder, version: "3.3.0"});
+		await updateController.update({ projectDir: projectFolder, version: "3.3.0" });
 
 		assert.isTrue(copyFileStub.calledWith(path.join(tempDir, "package.json"), projectFolder));
 		for (const folder of UpdateController.folders) {
 			assert.isTrue(copyFileStub.calledWith(path.join(tempDir, folder), projectFolder));
 		}
 	});
+
+	for (const projectType of ["Angular", "React"]) {
+		it(`should update dependencies from project type: ${projectType}`, async () => {
+			const testInjector = createTestInjector();
+			testInjector.resolve("platformCommandHelper").removePlatforms = () => {
+				throw new Error();
+			};
+
+			const fs = testInjector.resolve("fs");
+			const copyFileStub = sandbox.stub(fs, "copyFile");
+			const updateController = testInjector.resolve("updateController");
+			const tempDir = path.join(projectFolder, UpdateController.backupFolder);
+
+			const projectDataService = testInjector.resolve<IProjectDataService>("projectDataService");
+			projectDataService.getProjectData = (projectDir: string) => {
+				return <any>{
+					projectDir,
+					projectType,
+					dependencies: {
+						"tns-core-modules": "0.1.0",
+					},
+					devDependencies: {
+						"nativescript-dev-webpack": "1.1.3"
+					}
+				};
+			};
+
+			const packageInstallationManager = testInjector.resolve<IPackageInstallationManager>("packageInstallationManager");
+			const latestCompatibleVersion = "1.1.1";
+			packageInstallationManager.getLatestCompatibleVersionSafe = async (packageName: string, referenceVersion?: string): Promise<string> => {
+				assert.isString(packageName);
+				assert.isFalse(_.isEmpty(packageName));
+				return latestCompatibleVersion;
+			};
+
+			const pacoteService = testInjector.resolve<IPacoteService>("pacoteService");
+			pacoteService.manifest = async (packageName: string, options?: IPacoteManifestOptions): Promise<any> => {
+				assert.isString(packageName);
+				assert.isFalse(_.isEmpty(packageName));
+
+				return {
+					dependencies: {
+						"tns-core-modules": "1.0.0",
+						"dep2": "1.1.0"
+					},
+					devDependencies: {
+						"devDep1": "1.2.0",
+						"nativescript-dev-webpack": "1.3.0"
+					},
+					name: "template1"
+				};
+			};
+
+			const pluginsService = testInjector.resolve<IPluginsService>("pluginsService");
+			const dataAddedToPackageJson: IDictionary<any> = {
+				dependencies: {},
+				devDependencies: {}
+			};
+			pluginsService.addToPackageJson = (plugin: string, version: string, isDev: boolean, projectDir: string): void => {
+				if (isDev) {
+					dataAddedToPackageJson.devDependencies[plugin] = version;
+				} else {
+					dataAddedToPackageJson.dependencies[plugin] = version;
+				}
+			};
+
+			await updateController.update({ projectDir: projectFolder });
+
+			assert.isTrue(copyFileStub.calledWith(path.join(tempDir, "package.json"), projectFolder));
+			for (const folder of UpdateController.folders) {
+				assert.isTrue(copyFileStub.calledWith(path.join(tempDir, folder), projectFolder));
+			}
+
+			assert.deepEqual(dataAddedToPackageJson, {
+				dependencies: {
+					"tns-core-modules": "1.0.0",
+				},
+				devDependencies: {
+					"nativescript-dev-webpack": "1.3.0"
+				}
+			});
+		});
+	}
 });
