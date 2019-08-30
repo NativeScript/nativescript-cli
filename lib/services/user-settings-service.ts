5478891444cc60d87449809b9e022ed3fd2554a2
rosen-vladimirov <rosen.vladimirov@progress.com>
2019-08-30 09:01:05 +0300

    fix: save and remove of user-settings are not working
    Due to recent changes, trying to save a setting in the user-settings.json or to remove it from there is not working. This breaks all new users and the execution of commands like `tns usage-reporting enable/disable`, `tns error-reporting enable/disable`.
    The problem is an infinite recursion in the userSettingsService, which should just call jsonFileSettingsService, but instead it calls itself.
    Fix the recursion and add unit tests for the userSettingsService to ensure it acts as a proxy to jsonFileSettingsService.

--- lib/services/user-settings-service.ts
+++ lib/services/user-settings-service.ts
@@ -5,6 +5,7 @@ export class UserSettingsService implements IUserSettingsService {
 		const userSettingsFilePath = path.join(this.$settingsService.getProfileDir(), "user-settings.json");
 		return this.$injector.resolve("jsonFileSettingsService", { jsonFileSettingsPath: userSettingsFilePath });
 	}
+
 	constructor(private $injector: IInjector,
 		private $settingsService: ISettingsService) {
 	}
@@ -14,7 +15,7 @@ export class UserSettingsService implements IUserSettingsService {
 	}
 
 	public saveSetting<T>(key: string, value: T, cacheOpts?: IUseCacheOpts): Promise<void> {
-		return this.saveSetting<T>(key, value, cacheOpts);
+		return this.$jsonFileSettingsService.saveSetting<T>(key, value, cacheOpts);
 	}
 
 	public saveSettings(data: IDictionary<{}>, cacheOpts?: IUseCacheOpts): Promise<void> {
@@ -22,7 +23,7 @@ export class UserSettingsService implements IUserSettingsService {
 	}
 
 	public removeSetting(key: string): Promise<void> {
-		return this.removeSetting(key);
+		return this.$jsonFileSettingsService.removeSetting(key);
 	}
 
 	public loadUserSettingsFile(): Promise<void> {
---
+++ test/services/user-settings-service.ts
@@ -0,0 +1,82 @@
+import { Yok } from "../../lib/common/yok";
+import { UserSettingsService } from "../../lib/services/user-settings-service";
+import { assert } from "chai";
+import * as path from "path";
+
+class JsonFileSettingsServiceMock {
+	constructor(public jsonFileSettingsPath: string) { }
+}
+
+describe("userSettingsService", () => {
+	const profileDir = "my-profile-dir";
+	const expectedJsonFileSettingsFilePath = path.join(profileDir, "user-settings.json");
+
+	const createTestInjector = (): IInjector => {
+		const testInjector = new Yok();
+		testInjector.register("settingsService", {
+			getProfileDir: () => profileDir
+		});
+
+		testInjector.register("jsonFileSettingsService", JsonFileSettingsServiceMock);
+		testInjector.register("userSettingsService", UserSettingsService);
+		return testInjector;
+	};
+
+	const testCases = [
+		{
+			methodName: "getSettingValue",
+			input: ["settingName"],
+			expectedArgs: [
+				"settingName",
+				undefined
+			]
+		},
+		{
+			methodName: "saveSetting",
+			input: ["settingName", "settingValue"],
+			expectedArgs: [
+				"settingName",
+				"settingValue",
+				undefined
+			]
+		},
+		{
+			methodName: "saveSettings",
+			input: [{ value: { subValue: 1 } }],
+			expectedArgs: [
+				{ value: { subValue: 1 } },
+				undefined
+			]
+		},
+		{
+			methodName: "removeSetting",
+			input: ["settingName"],
+			expectedArgs: [
+				"settingName"
+			]
+		},
+		{
+			methodName: "loadUserSettingsFile",
+			input: [],
+			expectedArgs: []
+		}
+	];
+
+	for (const testCase of testCases) {
+		it(`calls ${testCase.methodName} method of jsonFileSettingsService with correct args`, async () => {
+			const testInjector = createTestInjector();
+			const dataPassedToJsonFileSettingsService: any[] = [];
+			const userSettingsService = testInjector.resolve("userSettingsService");
+			const jsonFileSettingsService = userSettingsService.$jsonFileSettingsService;
+
+			jsonFileSettingsService[testCase.methodName] = async (...args: any[]): Promise<void> => {
+				dataPassedToJsonFileSettingsService.push(...args);
+			};
+
+			await userSettingsService[testCase.methodName](...testCase.input);
+
+			assert.deepEqual(dataPassedToJsonFileSettingsService, testCase.expectedArgs);
+			assert.equal(jsonFileSettingsService.jsonFileSettingsPath, expectedJsonFileSettingsFilePath);
+		});
+	}
+});
