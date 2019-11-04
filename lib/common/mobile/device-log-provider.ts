b27e3331f9f644e867310514917836ff52140aa2
Kristian Dimitrov <kddimitrov@progress.com>
2019-11-04 11:10:10 +0200

    spike for device colorization in logs

--- lib/common/mobile/device-log-provider.ts
+++ lib/common/mobile/device-log-provider.ts
@@ -1,6 +1,7 @@
 import { DeviceLogProviderBase } from "./device-log-provider-base";
 import { DEVICE_LOG_EVENT_NAME } from "../constants";
 import { LoggerConfigData } from "../../constants";
+import colors = require("colors");
 
 export class DeviceLogProvider extends DeviceLogProviderBase {
 	constructor(protected $logFilter: Mobile.ILogFilter,
@@ -8,13 +9,27 @@ export class DeviceLogProvider extends DeviceLogProviderBase {
 		protected $logSourceMapService: Mobile.ILogSourceMapService) {
 		super($logFilter, $logger, $logSourceMapService);
 	}
+	private colors = [
+		"yellow",
+		"blue",
+		"magenta",
+		"cyan",
+		"gray"
+	]
+	private deviceColors: IDictionary<string> = {};
 
 	public logData(lineText: string, platform: string, deviceIdentifier: string): void {
 		const loggingOptions = this.getDeviceLogOptionsForDevice(deviceIdentifier);
 		let data = this.$logFilter.filterData(platform, lineText, loggingOptions);
 		data = this.$logSourceMapService.replaceWithOriginalFileLocations(platform, data, loggingOptions);
 		if (data) {
-			this.logDataCore(data);
+			let color = this.deviceColors[deviceIdentifier];
+			if(!color) {
+				color = this. colors[Math.floor(Math.random() * Math.floor(4))];
+				this.deviceColors[deviceIdentifier] = color;
+			}
+			//const device = this.$devicesService.getDeviceByIdentifier(deviceIdentifier);
+			this.logDataCore(`[${colors[color](deviceIdentifier)}] ${data}`);
 			this.emit(DEVICE_LOG_EVENT_NAME, lineText, deviceIdentifier, platform);
 		}
 	}
