fcb71750a0eaa68b1c73b1d843d24c595146f6bf
Igor Randjelovic <rigor789@gmail.com>
2020-07-22 00:06:13 +0200

    fix: hide "already installed" message when invoking inside a project

--- lib/services/nativescript-cloud-extension-service.ts
+++ lib/services/nativescript-cloud-extension-service.ts
@@ -4,11 +4,18 @@ import * as semver from "semver";
 export class NativeScriptCloudExtensionService implements INativeScriptCloudExtensionService {
 	constructor(private $extensibilityService: IExtensibilityService,
 		private $logger: ILogger,
-		private $packageInstallationManager: IPackageInstallationManager) { }
+		private $packageInstallationManager: IPackageInstallationManager,
+		private $projectService: IProjectService) { }
 
 	public install(): Promise<IExtensionData> {
 		if (!this.isInstalled()) {
 			return this.$extensibilityService.installExtension(constants.NATIVESCRIPT_CLOUD_EXTENSION_NAME);
+		}
+
+		if (this.$projectService.isValidNativeScriptProject()) {
+			// if we are in a valid NS project, we want to set up the current project, so no need to print the
+			// info message about the cloud extension already installed.
+			return;
 		}
 
 		this.$logger.info(`Extension ${constants.NATIVESCRIPT_CLOUD_EXTENSION_NAME} is already installed.`);
@@ -32,4 +39,5 @@ export class NativeScriptCloudExtensionService implements INativeScriptCloudExte
 		return _.find(this.$extensibilityService.getInstalledExtensionsData(), extensionData => extensionData.extensionName === constants.NATIVESCRIPT_CLOUD_EXTENSION_NAME);
 	}
 }
+
 $injector.register("nativeScriptCloudExtensionService", NativeScriptCloudExtensionService);
