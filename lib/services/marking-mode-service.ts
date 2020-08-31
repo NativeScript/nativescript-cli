eb4cc7f8e9d6f2787ee9942a7c762fae5de70b40
Igor Randjelovic <rigor789@gmail.com>
2020-08-31 22:47:22 +0200

    refactor: marking mode service

--- lib/services/marking-mode-service.ts
+++ lib/services/marking-mode-service.ts
@@ -1,4 +1,3 @@
-import * as helpers from "../common/helpers";
 import { EOL } from "os";
 import { LoggerConfigData } from "../constants";
 import { IProjectConfigService } from "../definitions/project";
@@ -10,44 +9,26 @@ const enum MarkingMode {
 }
 
 const MARKING_MODE_PROP = "markingMode";
-const MARKING_MODE_FULL_DEPRECATION_MSG = `With the upcoming NativeScript 7.0 the "${MARKING_MODE_PROP}:${MarkingMode.None}" will become the only marking mode supported by the Android Runtime.`;
-const MARKING_MODE_NONE_CONFIRM_MSG = `Do you want to switch your app to the recommended "${MARKING_MODE_PROP}:${MarkingMode.None}"?
-More info about the reasons for this change can be found in the link below:
-https://www.nativescript.org/blog/markingmode-none-is-official-boost-android-performance-while-avoiding-memory-issues`;
+const MARKING_MODE_FULL_DEPRECATION_MSG = `In NativeScript 7.0 "${MARKING_MODE_PROP}:${MarkingMode.Full}" is no longer supported.`;
 
 export class MarkingModeService implements IMarkingModeService {
 	constructor(
 		private $logger: ILogger,
-		private $projectConfigService: IProjectConfigService,
-		private $prompter: IPrompter
+		private $projectConfigService: IProjectConfigService
 	) {}
 
 	public async handleMarkingModeFullDeprecation(
 		options: IMarkingModeFullDeprecationOptions
 	): Promise<void> {
-		let markingModeValue = this.$projectConfigService.getValue(
+		const markingModeValue = this.$projectConfigService.getValue(
 			"android.markingMode"
 		);
+
 		const { skipWarnings, forceSwitch } = options;
 
 		if (forceSwitch) {
 			this.setMarkingMode(MarkingMode.None);
 			return;
-		}
-
-		if (!markingModeValue && helpers.isInteractive()) {
-			this.$logger.info();
-			this.$logger.printMarkdown(`
-__Improve your app by switching to "${MARKING_MODE_PROP}:${MarkingMode.None}".__
-
-\`${MARKING_MODE_FULL_DEPRECATION_MSG}\``);
-			const hasSwitched = await this.$prompter.confirm(
-				MARKING_MODE_NONE_CONFIRM_MSG,
-				() => true
-			);
-
-			markingModeValue = hasSwitched ? MarkingMode.None : MarkingMode.Full;
-			this.setMarkingMode(markingModeValue);
 		}
 
 		if (!skipWarnings && markingModeValue.toLowerCase() !== MarkingMode.None) {
