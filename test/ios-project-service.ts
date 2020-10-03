03ecac9dcde06f361f59ae990139906dc96a45f0
Nathan Walker <walkerrunpdx@gmail.com>
2020-10-03 14:15:40 -0700

    fix(ios): remove VALID_ARCHS from cocoapods for xcode 12 (#5407)
    * fix(ios): remove VALID_ARCHS from cocoapods for xcode 12
    
    * fix: tests

--- test/ios-project-service.ts
+++ test/ios-project-service.ts
@@ -268,6 +268,7 @@ describe("Cocoapods support", () => {
 				`# Begin Podfile - ${projectPath}/platforms/ios/Podfile-exclusions`,
 				`def post_installNativeScript_CLI_Architecture_Exclusions_0 (installer)`,
 				`  installer.pods_project.build_configurations.each do |config|`,
+				`    config.build_settings.delete "VALID_ARCHS"`,
 				`    config.build_settings["EXCLUDED_ARCHS_x86_64"] = "arm64 arm64e"`,
 				`    config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "i386 armv6 armv7 armv7s armv8 $(EXCLUDED_ARCHS_$(NATIVE_ARCH_64_BIT))"`,
 				`    config.build_settings["EXCLUDED_ARCHS[sdk=iphoneos*]"] = "i386 armv6 armv7 armv7s armv8 x86_64"`,
