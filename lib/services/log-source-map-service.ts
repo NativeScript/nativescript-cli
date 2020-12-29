2ff3041cd4558c37c74de7852bfc7f8a3dfe78ce
Igor Randjelovic <rigor789@gmail.com>
2020-12-29 22:04:39 +0100

    test: add external source-map tests

--- lib/services/log-source-map-service.ts
+++ lib/services/log-source-map-service.ts
@@ -67,14 +67,17 @@ export class LogSourceMapService implements Mobile.ILogSourceMapService {
 	public async setSourceMapConsumerForFile(filePath: string): Promise<void> {
 		try {
 			if (!this.$fs.getFsStats(filePath).isDirectory()) {
-				const mapFile = filePath + '.map';
-                    		let sourceMapRaw;
-                    		const source = this.$fs.readText(filePath);
-                    		if (this.$fs.exists(mapFile)) {
-                        		sourceMapRaw = sourceMapConverter.fromMapFileSource(source, path.dirname(filePath));
-                    		} else {
-                        		sourceMapRaw = sourceMapConverter.fromSource(source);
-                    		}
+				const mapFile = filePath + ".map";
+				let sourceMapRaw;
+				const source = this.$fs.readText(filePath);
+				if (this.$fs.exists(mapFile)) {
+					sourceMapRaw = sourceMapConverter.fromMapFileSource(
+						source,
+						path.dirname(filePath)
+					);
+				} else {
+					sourceMapRaw = sourceMapConverter.fromSource(source);
+				}
 				let smc: any = null;
 				if (sourceMapRaw && sourceMapRaw.sourcemap) {
 					const sourceMap = sourceMapRaw.sourcemap;
---
+++ test/files/sourceMapBundle/ios/app/external.js
@@ -0,0 +1,19 @@
+module.exports =
+  (global["webpackJsonp"] = global["webpackJsonp"] || []).push([["external"],{
+
+    /***/ "./external-test.js":
+    /***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+      "use strict";
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onTap", function() { return onTap; });
+      function onTap() {
+        console.log("Test.");
+        console.trace("Test");
+        throw new Error("Test");
+      }
+
+      /***/ })
+
+  },[["./external-test.js","runtime"]]]);
+//# sourceMappingURL=external.js.map
