dea3a027b0b69748324a2bc53f3ce2d8d5eb9542
Janos Hrubos <33330538+janoshrubos@users.noreply.github.com>
2021-01-26 20:48:14 +0100

    fix: negate type-o fix

--- lib/project-data.ts
+++ lib/project-data.ts
@@ -117,7 +117,7 @@ export class ProjectData implements IProjectData {
 	}
 
 	public initializeProjectData(projectDir?: string): void {
-		if (!this.initialized) {
+		if (this.initialized) {
 			return;
 		}
 		projectDir = projectDir || this.$projectHelper.projectDir;
