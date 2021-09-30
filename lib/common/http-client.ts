542040787adeed163d68db95e112fec1fb328461
Igor Randjelovic <rigor789@gmail.com>
2021-09-30 19:05:43 +0200

    Update lib/common/http-client.ts

--- lib/common/http-client.ts
+++ lib/common/http-client.ts
@@ -113,7 +113,7 @@ export class HttpClient implements Server.IHttpClient {
 			method: options.method,
 			proxy: false,
 			httpAgent: agent,
-			data: data,
+			data: options.body,
 		}).catch((err) => {
 			this.$logger.trace("An error occurred while sending the request:", err);
 			if (err.response) {
