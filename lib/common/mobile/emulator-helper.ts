a57813f0fdf4b3fd2d337bc7b34732c0f98e986f
Igor Randjelovic <rigor789@gmail.com>
2022-07-25 20:19:16 +0200

    chore: update api-level to version map

--- lib/common/mobile/emulator-helper.ts
+++ lib/common/mobile/emulator-helper.ts
@@ -5,6 +5,8 @@ import { injector } from "../yok";
 export class EmulatorHelper implements Mobile.IEmulatorHelper {
 	// https://developer.android.com/guide/topics/manifest/uses-sdk-element
 	public mapAndroidApiLevelToVersion = {
+		"android-33": "13.0.0",
+		"android-32": "12.0.0",
 		"android-31": "12.0.0",
 		"android-30": "11.0.0",
 		"android-29": "10.0.0",
