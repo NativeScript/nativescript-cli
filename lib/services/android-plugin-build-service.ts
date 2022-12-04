798c9a955d97d1aa25297097089170152a149837
Martin Guillon <dev@akylas.fr>
2022-12-04 15:50:27 +0100

    chore: yarn2 fixes

--- lib/services/android-plugin-build-service.ts
+++ lib/services/android-plugin-build-service.ts
@@ -430,12 +430,13 @@ export class AndroidPluginBuildService implements IAndroidPluginBuildService {
 		let runtimeVersion: string = null;
 
 		try {
-			const result = await this.$packageManager.view(
+			let result = await this.$packageManager.view(
 				SCOPED_ANDROID_RUNTIME_NAME,
 				{
 					"dist-tags": true,
 				}
 			);
+			result = result['dist-tags'] || result;
 			runtimeVersion = result.latest;
 		} catch (err) {
 			this.$logger.trace(
@@ -529,6 +530,7 @@ export class AndroidPluginBuildService implements IAndroidPluginBuildService {
 				`${SCOPED_ANDROID_RUNTIME_NAME}@${runtimeVersion}`,
 				{ version_info: true }
 			);
+			output = output['version_info'] || output;
 
 			if (!output) {
 				/**
@@ -543,6 +545,7 @@ export class AndroidPluginBuildService implements IAndroidPluginBuildService {
 					`${SCOPED_ANDROID_RUNTIME_NAME}@${runtimeVersion}`,
 					{ gradle: true }
 				);
+				output = output['gradle'] || output;
 
 				const { version, android } = output;
 
