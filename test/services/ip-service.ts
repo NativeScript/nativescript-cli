839b90cec5906ff6a221b7b7f33922d1b2869cd5
Igor Randjelovic <rigor789@gmail.com>
2023-03-21 14:55:15 +0100

    chore: remove play whoami tests

--- test/services/ip-service.ts
+++ test/services/ip-service.ts
@@ -7,8 +7,6 @@ import { IInjector } from "../../lib/common/definitions/yok";
 
 describe("ipService", () => {
 	const ip = "8.8.8.8";
-	const whoamiDefaultEndpoint = "https://who.am.i/api/whoami";
-	const errorMsgForDefaultEndpoint = `Unable to get data from ${whoamiDefaultEndpoint}`;
 	const errMsgForMyipCom = "Unable to get data from myip.com";
 	const errMsgForIpifyOrg = "Unable to get data from ipify.org";
 
@@ -23,14 +21,11 @@ describe("ipService", () => {
 
 		testInjector.register("logger", LoggerStub);
 		testInjector.register("ipService", IPService);
-		testInjector.register("config", {
-			WHOAMI_URL_ENDPOINT: whoamiDefaultEndpoint,
-		});
 		return testInjector;
 	};
 
 	describe("getCurrentIPv4Address", () => {
-		it("returns result from default service (play.nativescript.org) when it succeeds", async () => {
+		it("returns result from default service (myip.com) when it succeeds", async () => {
 			const testInjector = createTestInjector();
 			const httpClient = testInjector.resolve<Server.IHttpClient>("httpClient");
 			const httpRequestPassedOptions: any[] = [];
@@ -47,42 +42,11 @@ describe("ipService", () => {
 
 			assert.equal(ipAddress, ip);
 			assert.deepStrictEqual(httpRequestPassedOptions, [
-				{ method: "GET", url: whoamiDefaultEndpoint, timeout: 1000 },
-			]);
-		});
-
-		it("returns result from myip.com when the default endpoint fails", async () => {
-			const testInjector = createTestInjector();
-			const httpClient = testInjector.resolve<Server.IHttpClient>("httpClient");
-			const httpRequestPassedOptions: any[] = [];
-			httpClient.httpRequest = async (
-				options: any,
-				proxySettings?: IProxySettings
-			): Promise<Server.IResponse> => {
-				httpRequestPassedOptions.push(options);
-				if (options.url === whoamiDefaultEndpoint) {
-					throw new Error(errorMsgForDefaultEndpoint);
-				}
-				return <any>{ body: JSON.stringify({ ip }) };
-			};
-
-			const ipService = testInjector.resolve<IIPService>("ipService");
-			const ipAddress = await ipService.getCurrentIPv4Address();
-
-			assert.equal(ipAddress, ip);
-			assert.deepStrictEqual(httpRequestPassedOptions, [
-				{ method: "GET", url: whoamiDefaultEndpoint, timeout: 1000 },
 				{ method: "GET", url: "https://api.myip.com", timeout: 1000 },
 			]);
-
-			const logger = testInjector.resolve<LoggerStub>("logger");
-			assert.isTrue(
-				logger.traceOutput.indexOf(errorMsgForDefaultEndpoint) !== -1,
-				`Trace output\n'${logger.traceOutput}'\ndoes not contain expected message:\n${errorMsgForDefaultEndpoint}`
-			);
 		});
 
-		it("returns result from ipify.com when it default endpoint and myip.comm both fail", async () => {
+		it("returns result from ipify.com when the default endpoint (myip.com) fails", async () => {
 			const testInjector = createTestInjector();
 			const httpClient = testInjector.resolve<Server.IHttpClient>("httpClient");
 			const httpRequestPassedOptions: any[] = [];
@@ -91,10 +55,6 @@ describe("ipService", () => {
 				proxySettings?: IProxySettings
 			): Promise<Server.IResponse> => {
 				httpRequestPassedOptions.push(options);
-				if (options.url === whoamiDefaultEndpoint) {
-					throw new Error(errorMsgForDefaultEndpoint);
-				}
-
 				if (options.url === "https://api.myip.com") {
 					throw new Error(errMsgForMyipCom);
 				}
@@ -107,16 +67,11 @@ describe("ipService", () => {
 
 			assert.equal(ipAddress, ip);
 			assert.deepStrictEqual(httpRequestPassedOptions, [
-				{ method: "GET", url: whoamiDefaultEndpoint, timeout: 1000 },
 				{ method: "GET", url: "https://api.myip.com", timeout: 1000 },
 				{ method: "GET", url: "https://api.ipify.org", timeout: 1000 },
 			]);
 
 			const logger = testInjector.resolve<LoggerStub>("logger");
-			assert.isTrue(
-				logger.traceOutput.indexOf(errorMsgForDefaultEndpoint) !== -1,
-				`Trace output\n'${logger.traceOutput}'\ndoes not contain expected message:\n${errorMsgForDefaultEndpoint}`
-			);
 			assert.isTrue(
 				logger.traceOutput.indexOf(errMsgForMyipCom) !== -1,
 				`Trace output\n'${logger.traceOutput}'\ndoes not contain expected message:\n${errMsgForMyipCom}`
@@ -132,10 +87,6 @@ describe("ipService", () => {
 				proxySettings?: IProxySettings
 			): Promise<Server.IResponse> => {
 				httpRequestPassedOptions.push(options);
-				if (options.url === whoamiDefaultEndpoint) {
-					throw new Error(errorMsgForDefaultEndpoint);
-				}
-
 				if (options.url === "https://api.myip.com") {
 					throw new Error(errMsgForMyipCom);
 				}
@@ -152,16 +103,11 @@ describe("ipService", () => {
 
 			assert.isNull(ipAddress);
 			assert.deepStrictEqual(httpRequestPassedOptions, [
-				{ method: "GET", url: whoamiDefaultEndpoint, timeout: 1000 },
 				{ method: "GET", url: "https://api.myip.com", timeout: 1000 },
 				{ method: "GET", url: "https://api.ipify.org", timeout: 1000 },
 			]);
 
 			const logger = testInjector.resolve<LoggerStub>("logger");
-			assert.isTrue(
-				logger.traceOutput.indexOf(errorMsgForDefaultEndpoint) !== -1,
-				`Trace output\n'${logger.traceOutput}'\ndoes not contain expected message:\n${errorMsgForDefaultEndpoint}`
-			);
 			assert.isTrue(
 				logger.traceOutput.indexOf(errMsgForMyipCom) !== -1,
 				`Trace output\n'${logger.traceOutput}'\ndoes not contain expected message:\n${errMsgForMyipCom}`
