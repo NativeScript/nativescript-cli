68d9373aec1427a71e1f6236fff4f8084c33a421
Jason Cassidy <47318351+jcassidyav@users.noreply.github.com>
2023-04-12 15:32:06 +0100

    fix: migrate - safeguard possibly null values (#5730)

--- lib/services/project-cleanup-service.ts
+++ lib/services/project-cleanup-service.ts
@@ -27,10 +27,10 @@ export class ProjectCleanupService implements IProjectCleanupService {
 		options?: IProjectCleanupOptions
 	): Promise<IProjectCleanupResult> {
 		this.spinner = this.$terminalSpinnerService.createSpinner({
-			isSilent: options.silent,
+			isSilent: options?.silent,
 		});
 
-		let stats = options.stats ? new Map<string, number>() : false;
+		let stats = options?.stats ? new Map<string, number>() : false;
 
 		let success = true;
 		for (const pathToClean of pathsToClean) {
